4086e3bca06ea65c5eacf6a2213b3f2228cc3b36
David Chan <davidchan125@hotmail.co.uk>
2022-03-09 15:08:54 +0000

    Fully implement multi-checkbox ticking

--- search/static/search/search.js
+++ search/static/search/search.js
@@ -1,26 +1,39 @@
-const ontologyTreeLiElems = document.querySelectorAll("li");
-const ontologyTreeCheckboxesL1 = document.querySelectorAll("input[data-list-level='1']");
-function updateParentCheckboxes(nodeId) {
-    
+const ontologyParentNodeCheckboxes = document.querySelectorAll("input[data-is-parent-node='true']");
+const ontologyChildNodeCheckboxes = document.querySelectorAll("input:not([data-parent-node-in-ontology=''])");
+
+
+function updateParentNodeCheckboxes(childNodeCheckbox) {
+    const siblingNodeCheckboxes = document.querySelectorAll(`input[data-parent-node-in-ontology='${childNodeCheckbox.dataset.parentNodeInOntology}']`);
+    const siblingNodeCheckboxesChecked = document.querySelectorAll(`input[data-parent-node-in-ontology='${childNodeCheckbox.dataset.parentNodeInOntology}']:checked`);
+    const parentNodeCheckbox = document.getElementById(childNodeCheckbox.dataset.parentNodeInOntology);
+    parentNodeCheckbox.checked = siblingNodeCheckboxes.length === siblingNodeCheckboxesChecked.length;
+    if (parentNodeCheckbox.dataset.parentNodeInOntology !== "") {
+        updateParentNodeCheckboxes(parentNodeCheckbox);
+    }
 }
 
-function updateChildCheckboxes(nodeId) {
-    const parentNodeCheckbox = document.getElementById(nodeId);
-    const childNodeCheckboxes = document.querySelectorAll(`input[data-parent-node-in-tree='${nodeId}']`);
+function updateChildNodeCheckboxes(parentNodeCheckbox) {
+    const childNodeCheckboxes = document.querySelectorAll(`input[data-parent-node-in-ontology='${parentNodeCheckbox.id}']`);
     childNodeCheckboxes.forEach(checkbox => {
         checkbox.checked = parentNodeCheckbox.checked;
-        const childNodeCheckboxesOfChildNodeCheckbox = document.querySelectorAll(`input[data-parent-node-in-tree='${checkbox.id}']`);
+        const childNodeCheckboxesOfChildNodeCheckbox = document.querySelectorAll(`input[data-parent-node-in-ontology='${checkbox.id}']`);
         if (childNodeCheckboxesOfChildNodeCheckbox.length > 0) {
-            updateChildCheckboxes(checkbox.id);
+            updateChildNodeCheckboxes(checkbox);
         }
     })
 }
 
-ontologyTreeCheckboxesL1.forEach(checkbox => {
+ontologyParentNodeCheckboxes.forEach(checkbox => {
     checkbox.addEventListener("change", event => {
-        const childNodeCheckboxes = document.querySelectorAll(`input[data-parent-node-in-tree='${checkbox.id}']`);
+        const childNodeCheckboxes = document.querySelectorAll(`input[data-parent-node-in-ontology='${checkbox.id}']`);
         if (childNodeCheckboxes.length > 0) {
-            updateChildCheckboxes(checkbox.id);
+            updateChildNodeCheckboxes(checkbox);
         }
+    });
+});
+
+ontologyChildNodeCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener("change", event => {
+        updateParentNodeCheckboxes(checkbox);
     });
 });
