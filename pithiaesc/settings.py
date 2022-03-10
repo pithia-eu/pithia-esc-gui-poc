9e79e3202c3bc8829d55090979096e7d91efe712
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-10 16:49:07 +0000

    Add RegisterConfig to INSTALLED_APPS

--- pithiaesc/settings.py
+++ pithiaesc/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'search.apps.SearchConfig',
+    'register.apps.RegisterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
