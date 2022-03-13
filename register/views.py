e23945a0f734ca969c38bd53a48c139289132142
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-13 00:19:11 +0000

    Update register app views.py

--- register/views.py
+++ register/views.py
@@ -1,5 +1,17 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import UploadFileForm
+from .file_upload import handle_uploaded_file
 
 # Create your views here.
 def index(request):
-    return render(request, 'register/index.html', {})
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/register/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'register/index.html', {
+        'form': form.as_p()
+    })
