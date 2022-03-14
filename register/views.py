4840f020aebf833331924383f7e9623c57f58d50
David Chan <davidchan125@hotmail.co.uk>
2022-03-14 21:40:28 +0000

    Update POST register index view handling

--- register/views.py
+++ register/views.py
@@ -4,7 +4,7 @@ from register.forms import UploadFileForm
 from .helpers import handle_uploaded_file
 import json
 import xmltodict
-from pymongo import MongoClient
+from utils import db
 
 # Create your views here.
 def index(request):
@@ -12,13 +12,15 @@ def index(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             with request.FILES['file'] as file:
+                observation_collections = db['observation_collections']
                 xml = file.read()
                 dict = xmltodict.parse(xml)
                 json_dict = json.dumps(dict)
                 json_dict = json_dict.replace('\\n', '')
                 json_dict = ' '.join(json_dict.split())
                 json_dict = json.loads(json_dict)
-                print(json.dumps(json_dict, indent=4, sort_keys=True))
+                print(json.dumps(json_dict['ObservationCollection'], indent=2, sort_keys=True))
+                observation_collections.insert_one(json_dict['ObservationCollection'])
 
             return HttpResponseRedirect('/register/')
     else:
