e1617e4fdea024e4a4f2dcd5ad07fe247790212a
David Chan <davidchan125@hotmail.co.uk>
2022-03-14 13:13:39 +0000

    Add POST request handling to register index.html

--- register/views.py
+++ register/views.py
@@ -1,5 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from register.forms import UploadFileForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'register/index.html', {})
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/register/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'register/index.html', {
+        'form': form
+    })
