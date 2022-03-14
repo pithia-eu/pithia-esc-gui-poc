0ae2e183d2ba89047d8dd9ba0465f3ece1c3e02c
David Chan <davidchan125@hotmail.co.uk>
2022-03-14 20:06:24 +0000

    Update register app forms.py

--- register/forms.py
+++ register/forms.py
@@ -1,4 +1,10 @@
+from cProfile import label
 from django.forms import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(label='Model or Dataset metadata file')
+
+    def __init__(self, *args, **kwargs):
+        super(UploadFileForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
