cfcc226f55a10f69e5cb4caee26b9b3548b308ce
David Chan <davidchan125@hotmail.co.uk>
2022-03-15 16:27:41 +0000

    Update search & register apps forms.py

--- search/forms.py
+++ search/forms.py
@@ -1,9 +1,9 @@
 from django.forms import forms
 
-class UploadFileForm(forms.Form):
+class SearchForm(forms.Form):
     file = forms.FileField(label='Model or Dataset metadata file')
 
     def __init__(self, *args, **kwargs):
-        super(UploadFileForm, self).__init__(*args, **kwargs)
+        super(SearchForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
