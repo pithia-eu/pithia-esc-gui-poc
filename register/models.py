80a3473acb2ad2202494732c267bdbb35191d4f9
David Chan <davidchan125@hotmail.co.uk>
2022-03-15 10:09:54 +0000

    Update register app models.py

--- register/models.py
+++ register/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class PithiaResourceIdentifier(models.Model):
+    localID = models.CharField(max_length=200)
+    namespace = models.CharField(max_length=200)
+    version = models.IntegerField()
+    creationDate = models.DateTimeField()
+    lastModificationDate = models.DateTimeField()
+
+class ResourceIdentifier(models.Model):
+    pithiaIdentifier = models.ForeignKey(PithiaResourceIdentifier, on_delete=models.CASCADE)
+
 class Resource(models.Model):
     name = models.CharField(max_length=200)
     upload_date = models.DateTimeField('date uploaded')
