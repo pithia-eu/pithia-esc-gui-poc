879d605ff75703c7c34566769a4614ee6d6d7e9c
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-15 22:59:51 +0000

    Remove print MONGODB_CONNECTION_STRING

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ from pymongo import MongoClient
 import environ
 
 env = environ.Env()
-print(env('MONGODB_CONNECTION_STRING'))
 
 client = MongoClient(env('MONGODB_CONNECTION_STRING'))
 db = client[env('DB_NAME')]
