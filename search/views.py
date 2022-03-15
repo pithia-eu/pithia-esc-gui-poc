e75d20acdfffa9122a8a89be572ff2add7195072
David Chan <davidchan125@hotmail.co.uk>
2022-03-15 16:27:50 +0000

    Update search app views.py

--- search/views.py
+++ search/views.py
@@ -1,10 +1,17 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .ontology_helpers import nested_list_from_ontology_component
+from utils import db
 
 
 def index(request):
     if request.method == 'POST':
-        weg = ''
+        observed_properties = request.POST.getlist('observed_properties')
+        measurands = request.POST.getlist('measurands')
+        qualifiers = request.POST.getlist('qualifiers')
+        phenomenons = request.POST.getlist('phenomenons')
+        db.
+        return HttpResponseRedirect('/search/')
     else:
         observed_properties = nested_list_from_ontology_component('observedProperty')
         qualifiers = nested_list_from_ontology_component('qualifier')
