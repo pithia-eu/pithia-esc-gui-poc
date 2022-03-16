5e52760b3a75a0b57a725f7d943e219a0759cd65
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-16 14:04:53 +0000

    Update register app views.py - Update index view to use generic FormView - Adjust index view to handle multiple file upload

--- register/views.py
+++ register/views.py
@@ -1,31 +1,49 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from register.forms import UploadFileForm
-from .helpers import handle_uploaded_file
+from django.views.generic.edit import FormView
+from .forms import UploadFileForm
+from utils import db
 import json
 import xmltodict
-from utils import db
 
 # Create your views here.
-def index(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            with request.FILES['file'] as file:
-                observation_collections = db['observation_collections']
-                xml = file.read()
-                dict = xmltodict.parse(xml)
-                json_dict = json.dumps(dict)
-                json_dict = json_dict.replace('\\n', '')
-                json_dict = ' '.join(json_dict.split())
-                json_dict = json.loads(json_dict)
-                print(json.dumps(json_dict['ObservationCollection'], indent=2, sort_keys=True))
-                observation_collections.insert_one(json_dict['ObservationCollection'])
+class RegisterView(FormView):
+    form_class = UploadFileForm
+    template_name = 'register/index.html'
+    success_url = '/register/'
 
-            return HttpResponseRedirect('/register/')
-    else:
-        form = UploadFileForm()
-    return render(request, 'register/index.html', {
-        'title': 'Register Models/Datasets',
-        'form': form
-    })
+    def post(self, request, *args, **kwargs):
+        form_class = self.form_class
+        form = self.get_form(form_class)
+        files = request.FILES.getlist('file')
+        if form.is_valid():
+            xmls_as_dicts = []
+            for f in files:
+                observation_collections = db['observation_collections']
+                xml = f.read()
+                # Convert XML to a dictionary to be able to convert it JSON
+                xml_as_dict = xmltodict.parse(xml)
+                # Convert the dictionary to JSON
+                xml_as_json = json.dumps(xml_as_dict)
+                # Some formatting to get rid of '\n' characters and extra
+                # whitespace within strings
+                xml_as_json = xml_as_json.replace('\\n', '')
+                xml_as_json = ' '.join(xml_as_json.split())
+                # pymongo takes dictionaries when inserting new documents,
+                # so convert the JSON back to a dictionary
+                xml_as_dict = json.loads(xml_as_dict)
+                # DEBUG: Print out the resulting dictionary
+                print(json.dumps(xml_as_dict['ObservationCollection'], indent=2, sort_keys=True))
+                # Add the dictionary to the list of dictionaries to be inserted
+                # into the database.
+                xmls_as_dicts.append(xml_as_dict['ObservationCollection'])
+            # Insert the dictionaries into the 'observation_collections' collection
+            observation_collections.insert_many(xmls_as_dicts)
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Register Models/Datasets'
+        return context
