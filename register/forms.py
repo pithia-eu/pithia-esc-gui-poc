961fde328515dcd865949dbcaa6c22caedbf4552
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-16 22:52:43 +0000

    Update UploadFileForm

--- register/forms.py
+++ register/forms.py
@@ -1,9 +1,7 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField(label='Select the metadata files you would like to upload:', widget=forms.ClearableFileInput(attrs={'multiple': True}))
-
-    def __init__(self, *args, **kwargs):
-        super(UploadFileForm, self).__init__(*args, **kwargs)
-        for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'form-control'
+    files = forms.FileField(label='Select the metadata files you would like to register:', widget=forms.ClearableFileInput(attrs={
+                'multiple': True,
+                'class': 'form-control'
+            }))
