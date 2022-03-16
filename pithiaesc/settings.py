6cba43621e16a20374688f12bf8f1af6bc0a7cef
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-16 10:30:51 +0000

    Update templates DIRS in settings.py

--- pithiaesc/settings.py
+++ pithiaesc/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'pithiaesc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
