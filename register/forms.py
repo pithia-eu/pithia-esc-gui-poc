41151e45a10c442966718ad29c7c70299c3ad891
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-16 14:03:49 +0000

    Register app forms.py changes - Update bad import of forms from django library - Update UploadFileForm to support multiple file upload

--- register/forms.py
+++ register/forms.py
@@ -1,7 +1,7 @@
-from django.forms import forms
+from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField(label='Model or Dataset metadata file')
+    file = forms.FileField(label='Select the metadata files you would like to upload:', widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
     def __init__(self, *args, **kwargs):
         super(UploadFileForm, self).__init__(*args, **kwargs)
