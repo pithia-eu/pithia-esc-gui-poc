dab120f6f2cba54c39d8c6bcc682345bf14fa35f
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-16 22:53:19 +0000

    Move xml processing to helpers.py

--- register/views.py
+++ register/views.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.edit import FormView
 from .forms import UploadFileForm
-from utils import db
-import json
-import xmltodict
+from .helpers import handle_uploaded_metadata
 
 # Create your views here.
 class RegisterView(FormView):
@@ -15,30 +12,9 @@ class RegisterView(FormView):
     def post(self, request, *args, **kwargs):
         form_class = self.form_class
         form = self.get_form(form_class)
-        files = request.FILES.getlist('file')
+        files = request.FILES.getlist('files')
         if form.is_valid():
-            xmls_as_dicts = []
-            for f in files:
-                observation_collections = db['observation_collections']
-                xml = f.read()
-                # Convert XML to a dictionary to be able to convert it JSON
-                xml_as_dict = xmltodict.parse(xml)
-                # Convert the dictionary to JSON
-                xml_as_json = json.dumps(xml_as_dict)
-                # Some formatting to get rid of '\n' characters and extra
-                # whitespace within strings
-                xml_as_json = xml_as_json.replace('\\n', '')
-                xml_as_json = ' '.join(xml_as_json.split())
-                # pymongo takes dictionaries when inserting new documents,
-                # so convert the JSON back to a dictionary
-                xml_as_dict = json.loads(xml_as_dict)
-                # DEBUG: Print out the resulting dictionary
-                print(json.dumps(xml_as_dict['ObservationCollection'], indent=2, sort_keys=True))
-                # Add the dictionary to the list of dictionaries to be inserted
-                # into the database.
-                xmls_as_dicts.append(xml_as_dict['ObservationCollection'])
-            # Insert the dictionaries into the 'observation_collections' collection
-            observation_collections.insert_many(xmls_as_dicts)
+            handle_uploaded_metadata(files)
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
