80e3b7c02a12deba9f61775289dc89ececfa85df
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-16 10:31:19 +0000

    Add view for base url

--- pithiaesc/urls.py
+++ pithiaesc/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='home'),
     path('search/', include('search.urls')),
     path('register/', include('register.urls')),
     path('admin/', admin.site.urls),
