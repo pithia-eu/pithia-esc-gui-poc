7afe69f3deb6ee2521227cfe30fa820a9a00dfd8
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-18 03:34:45 +0000

    Load search form components async - Search app index.html changes - search.js changes

--- search/static/search/search.js
+++ search/static/search/search.js
@@ -1,7 +1,3 @@
-const ontologyParentNodeCheckboxes = document.querySelectorAll(".tree input[data-is-parent-node='true']");
-const ontologyChildNodeCheckboxes = document.querySelectorAll(".tree input:not([data-parent-node-in-ontology=''])");
-
-
 function getEnclosingLiNode(checkbox) {
     let currentParentNode = checkbox;
     let currentChildNode = checkbox;
@@ -38,17 +34,56 @@ function updateChildNodeCheckboxes(parentNodeCheckbox) {
     })
 }
 
-ontologyParentNodeCheckboxes.forEach(checkbox => {
-    checkbox.addEventListener("change", event => {
-        const childNodeCheckboxes = document.querySelectorAll(`input[data-parent-node-in-ontology='${checkbox.id}']`);
-        if (childNodeCheckboxes.length > 0) {
-            updateChildNodeCheckboxes(checkbox);
-        }
+async function loadSearchFormComponents() {
+    const fetchParams = { method: "GET" };
+    const measurandsResponse = fetch("/search/measurand/", fetchParams);
+    const observedPropertiesResponse = fetch("/search/observedProperty/", fetchParams);
+    const phenomenonsResponse = fetch("/search/phenomenon/", fetchParams);
+    const qualifiersResponse = fetch("/search/qualifier/", fetchParams);
+    const responses = await Promise.all([
+        measurandsResponse,
+        observedPropertiesResponse,
+        phenomenonsResponse,
+        qualifiersResponse,
+    ]);
+    
+    setTimeout(async () => {
+        const responsesParsed = await Promise.all(responses.map(response => response.text()));
+        const measurandsFormComponent = responsesParsed[0];
+        const observedPropertiesFormComponent = responsesParsed[1];
+        const phenomenonsFormComponent = responsesParsed[2];
+        const qualifiersFormComponent = responsesParsed[3];
+        document.getElementById("measurands-tree-container").innerHTML = measurandsFormComponent;
+        document.getElementById("observed-properties-tree-container").innerHTML = observedPropertiesFormComponent;
+        document.getElementById("phenomenons-tree-container").innerHTML = phenomenonsFormComponent;
+        document.getElementById("qualifiers-tree-container").innerHTML = qualifiersFormComponent;
+        treeContainers.forEach(tc => {
+            tc.style.opacity = 1;
+        });
+        const ontologyParentNodeCheckboxes = document.querySelectorAll(".tree input[data-is-parent-node='true']");
+        const ontologyChildNodeCheckboxes = document.querySelectorAll(".tree input:not([data-parent-node-in-ontology=''])");
+    
+        ontologyParentNodeCheckboxes.forEach(checkbox => {
+            checkbox.addEventListener("change", event => {
+                const childNodeCheckboxes = document.querySelectorAll(`input[data-parent-node-in-ontology='${checkbox.id}']`);
+                if (childNodeCheckboxes.length > 0) {
+                    updateChildNodeCheckboxes(checkbox);
+                }
+            });
+        });
+    
+        ontologyChildNodeCheckboxes.forEach(checkbox => {
+            checkbox.addEventListener("change", event => {
+                updateParentNodeCheckboxes(checkbox);
+            });
+        });
+    }, 300);
+    const treeContainers = document.querySelectorAll(".tree-container");
+    treeContainers.forEach(tc => {
+        tc.style.opacity = 0;
     });
-});
+}
 
-ontologyChildNodeCheckboxes.forEach(checkbox => {
-    checkbox.addEventListener("change", event => {
-        updateParentNodeCheckboxes(checkbox);
-    });
+document.getElementById("search-script").addEventListener("load", async event => {
+    await loadSearchFormComponents();
 });
