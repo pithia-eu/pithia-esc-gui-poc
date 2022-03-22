de323ffb6f33640e800f5c4c68ebc10c296e3f14
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-22 22:21:30 +0000

    Update resources urls.py

--- resources/urls.py
+++ resources/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'resources'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<namespace>/', views.list_by_namespace, name='list_by_namespace'),
     path('<namespace>/<localID>/', views.detail, name='detail'),
 ]
