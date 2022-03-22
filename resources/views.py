656212511faf054c19ef7fc61a53441a98ecbd88
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-22 22:21:23 +0000

    Update resources views.py

--- resources/views.py
+++ resources/views.py
@@ -3,7 +3,18 @@ from mongodb import db
 
 # Create your views here.
 def index(request):
-    return render(request, 'resources/index.html', {})
+    namespaces = list(db['observation_collections'].distinct('identifier.pithia:Identifier.namespace'))
+    return render(request, 'resources/index.html', {
+        'namespaces': namespaces
+    })
+
+def list_by_namespace(request, namespace):
+    resources = list(db['observation_collections'].find({
+        'identifier.pithia:Identifier.namespace': namespace
+    }))
+    return render(request, 'resources/list_by_namespace.html', {
+        'resources': resources
+    })
 
 def detail(request, namespace, localID):
     resource = db['observation_collections'].find_one({
