5ee554105966c5494ad14785435d9ec4f326a879
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-03-23 03:11:51 +0000

    Update UploadFileForm

--- register/forms.py
+++ register/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    files = forms.FileField(label='Model/Data Collection XML metadata files', widget=forms.ClearableFileInput(attrs={
+    files = forms.FileField(label='Model and/or Data Collection XML metadata files', widget=forms.ClearableFileInput(attrs={
                 'multiple': True,
                 'class': 'form-control'
             }))
