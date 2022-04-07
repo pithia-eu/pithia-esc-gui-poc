d0304e9c4f66481d6a12a428695d28a3d7bfd532
David Chan <davidchan125@hotmail.co.uk>
2022-04-07 17:57:11 +0100

    Add HTML datasets for filtering ob_prop checkboxes

--- search/ontology_helpers.py
+++ search/ontology_helpers.py
@@ -1,12 +1,32 @@
 from requests import get
-from rdflib import Graph
+from rdflib import Graph, Namespace, URIRef
 from rdflib.namespace import _SKOS
+from pprint import pprint
 
-ESPAS = 'https://ontology.espas-fp7.eu/'
+ESPAS_ONTOLOGY_URL = 'https://ontology.espas-fp7.eu/'
+ESPAS = Namespace('http://ontology.espas-fp7.eu/espasdefinitions#')
+
+def get_object_names_from_triple(triple):
+    s, p, o = triple
+    if 'phenomenon' in o:
+        return o.replace(f'{ESPAS_ONTOLOGY_URL}phenomenon/', 'phenomenon')
+    if 'measurand' in o:
+        return o.replace(f'{ESPAS_ONTOLOGY_URL}measurand/', 'measurand')
+    if 'qualifier' in o:
+        return o.replace(f'{ESPAS_ONTOLOGY_URL}qualifier/', 'qualifier')
+
+def add_phenomenons_qualifiers_and_measurands_to_op(op_uri, op_dict, g):
+    op_phenomenons = list(map(get_object_names_from_triple, g.triples((op_uri, ESPAS.phenomenon, None))))
+    op_measurands = list(map(get_object_names_from_triple, g.triples((op_uri, ESPAS.measurand, None))))
+    op_qualifiers = list(map(get_object_names_from_triple, g.triples((op_uri, ESPAS.qualifier, None))))
+    op_dict['phenomenons'] = op_phenomenons
+    op_dict['measurands'] = op_measurands
+    op_dict['qualifiers'] = op_qualifiers
+    return op_dict
 
 def nested_list_from_ontology_component(ontology_component):
     # Fetch Observed Property component of ESPAS ontology text
-    ontology_component_url = f'{ESPAS}{ontology_component}/'
+    ontology_component_url = f'{ESPAS_ONTOLOGY_URL}{ontology_component}/'
     r = get(ontology_component_url)
     r_text = r.text
 
@@ -43,8 +63,11 @@ def nested_list_from_ontology_component(ontology_component):
                 'value': o_value,
                 'pref_label': pref_label_mappings[o_value],
                 'alt_label': alt_label_mappings[o_value] if o_value in alt_label_mappings else None,
-                'narrowers': {}
+                'narrowers': {},
             }
+            if ontology_component == 'observedProperty':
+                nested_list[o_value] = add_phenomenons_qualifiers_and_measurands_to_op(o, nested_list[o_value], g)
+
         # Add to the broader value's dictionary by putting in a narrower
         # value as a key and setting its value to '1'.
         nested_list[o_value]['narrowers'][s_value] = {
@@ -52,8 +75,10 @@ def nested_list_from_ontology_component(ontology_component):
             'value': s_value,
             'pref_label': pref_label_mappings[s_value],
             'alt_label': alt_label_mappings[s_value] if s_value in alt_label_mappings else None,
-            'narrowers': {}
+            'narrowers': {},
         }
+        if ontology_component == 'observedProperty':
+            nested_list[o_value]['narrowers'][s_value] = add_phenomenons_qualifiers_and_measurands_to_op(s, nested_list[o_value]['narrowers'][s_value], g)
 
     keys_to_remove_at_top_level = []
     for key in nested_list:
