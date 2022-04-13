a409ed4852d18c78c9b94feaff0b4b67bc80aa2f
David Chan <davidchan125@hotmail.co.uk>
2022-04-13 13:15:52 +0100

    Update register page file input label

--- register/forms.py
+++ register/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    files = forms.FileField(label='Model and/or Data Collection XML metadata files', widget=forms.ClearableFileInput(attrs={
+    files = forms.FileField(label='XML metadata files', widget=forms.ClearableFileInput(attrs={
                 'accept': 'application/xml',
                 'multiple': True,
                 'class': 'form-control'
