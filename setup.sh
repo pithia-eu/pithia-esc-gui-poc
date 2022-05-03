9161b2d7711131d62d3be06780d55788507a6a36
David Chan <davidchan125@hotmail.co.uk>
2022-05-03 15:05:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 python3 -m venv venv
-echo "Created Python virtual environment"
+printf "\nCreated Python virtual environment"
 
 source venv/bin/activate
-echo "Activated Python virtual environment"
+printf "\nActivated Python virtual environment"
 
 pip3 install -r requirements.txt
-echo ""
-echo "Finished installing packages from requirements.txt"
+printf "\nFinished installing packages from requirements.txt"
+
+# Setup default .env file
+envfile=".env"
+secret_key=$(python3 -c "import secrets; print(secrets.token_urlsafe())")
+echo "SECRET_KEY=$secret_key"
+echo "MONGODB_CONNECTION_STRING=mongodb://localhost:27017"
+echo "DB_NAME=pithia-esc"
+cat $envfile
