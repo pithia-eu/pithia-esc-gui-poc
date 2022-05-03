95759f0f2f56806a636027c94f398424185ff6c7
David Chan <davidchan125@hotmail.co.uk>
2022-05-03 15:13:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,10 +10,10 @@ printf "\nFinished installing packages from requirements.txt\n"
 
 # Setup default .env file
 printf "\nSetting up default .env\n"
-envfile="./.env"
+envfile="./pithiaesc/.env"
 secret_key=$(python3 -c "import secrets; print(secrets.token_urlsafe())")
-echo "SECRET_KEY=$secret_key" > $file
-echo "MONGODB_CONNECTION_STRING=mongodb://localhost:27017" >> $file
-echo "DB_NAME=pithia-esc" >> $file
+echo "SECRET_KEY=$secret_key" > $envfile
+echo "MONGODB_CONNECTION_STRING=mongodb://localhost:27017" >> $envfile
+echo "DB_NAME=pithia-esc" >> $envfile
 cat $envfile
 printf "\nFinished setting up default .env\n"
