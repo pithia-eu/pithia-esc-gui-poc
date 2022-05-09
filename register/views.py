bc66fbda5e34b666e277021b14244563a7f873e6
David Chan <davidchan125@hotmail.co.uk>
2022-05-09 14:04:57 +0100

    Update register app views.py - Update index view - Move index view code to metadata_upload view - Update http redirects after upload form submission - Test xsd validation

--- register/views.py
+++ register/views.py
@@ -2,45 +2,20 @@ from pyexpat import ExpatError
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+
+from register.validation import get_xml_schema_by_type, validate_xml_files_by_type
 from .forms import UploadFileForm
 from .metadata_helpers import handle_uploaded_metadata
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        files = request.FILES.getlist('files')
-        if form.is_valid():
-            try:
-                uploaded_file_stats = handle_uploaded_metadata(files, request.POST)
-            except ExpatError as err:
-                print("There was an error whilst parsing the XML: {0}".format(err))
-                return HttpResponseRedirect(reverse('register:index') + '?error=ExpatError')
-            except BaseException as err:
-                print("An unexpected error occurred: {0}".format(err))
-                return HttpResponseRedirect(reverse('register:index') + '?error=500')
-
-            query_string = '?'
-            if uploaded_file_stats['acq_files_uploaded'] > 0:
-                query_string += f'acq_files_uploaded={uploaded_file_stats["acq_files_uploaded"]}'
-            if uploaded_file_stats['comp_files_uploaded'] > 0:
-                query_string += f'comp_files_uploaded={uploaded_file_stats["comp_files_uploaded"]}'
-            if uploaded_file_stats['op_files_uploaded'] > 0:
-                query_string += f'op_files_uploaded={uploaded_file_stats["op_files_uploaded"]}'
-            if uploaded_file_stats['proc_files_uploaded'] > 0:
-                query_string += f'proc_files_uploaded={uploaded_file_stats["proc_files_uploaded"]}'
-            print(query_string)
-            return HttpResponseRedirect(reverse('register:index') + query_string)
-        else:
-            return HttpResponseRedirect(reverse('register:index'))
-    else:
-        form = UploadFileForm()
     return render(request, 'register/index.html', {
         'title': 'Register Models & Measurements',
-        'form': form
     })
 
 def metadata_upload(request, metadata_upload_type):
+    # There's probably a DRY-er way of handling
+    # 'valid metadata upload types'
     valid_metadata_upload_types = [
         'organisation',
         'individual',
@@ -55,7 +30,37 @@ def metadata_upload(request, metadata_upload_type):
     ]
     if metadata_upload_type not in valid_metadata_upload_types:
         raise Http404
-    form = UploadFileForm()
+    if request.method == 'POST':
+        # Form validation
+        form = UploadFileForm(request.POST, request.FILES)
+        files = request.FILES.getlist('files')
+        if form.is_valid():
+            # XML Schema validation
+            valid_xmls, invalid_xmls = validate_xml_files_by_type(files, metadata_upload_type)
+            try:
+                uploaded_file_stats = handle_uploaded_metadata(files, request.POST)
+            except ExpatError as err:
+                print("There was an error whilst parsing the XML: {0}".format(err))
+                return HttpResponseRedirect(reverse('register:metadata_upload', args=[metadata_upload_type]) + '?error=ExpatError')
+            except BaseException as err:
+                print("An unexpected error occurred: {0}".format(err))
+                return HttpResponseRedirect(reverse('register:metadata_upload', args=[metadata_upload_type]) + '?error=500')
+
+            query_string = '?'
+            if uploaded_file_stats['acq_files_uploaded'] > 0:
+                query_string += f'acq_files_uploaded={uploaded_file_stats["acq_files_uploaded"]}'
+            if uploaded_file_stats['comp_files_uploaded'] > 0:
+                query_string += f'comp_files_uploaded={uploaded_file_stats["comp_files_uploaded"]}'
+            if uploaded_file_stats['op_files_uploaded'] > 0:
+                query_string += f'op_files_uploaded={uploaded_file_stats["op_files_uploaded"]}'
+            if uploaded_file_stats['proc_files_uploaded'] > 0:
+                query_string += f'proc_files_uploaded={uploaded_file_stats["proc_files_uploaded"]}'
+            print(query_string)
+            return HttpResponseRedirect(reverse('register:metadata_upload', args=[metadata_upload_type]) + query_string)
+        else:
+            return HttpResponseRedirect(reverse('register:metadata_upload', args=[metadata_upload_type]))
+    else:
+        form = UploadFileForm()
     return render(request, 'register/metadata_upload.html', {
         'metadata_upload_type': metadata_upload_type,
         'form': form
