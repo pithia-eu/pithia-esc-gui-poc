85a24a84ea00522c34835610306d6dde08088b94
David Chan <davidchan125@hotmail.co.uk>
2022-05-12 16:41:46 +0100

    Update validation.py - Use real xml schemas instead of test schema - Validate xmls one at a time instead of a list of xmls

--- register/validation.py
+++ register/validation.py
@@ -1,13 +1,10 @@
 import os
-import xmlschema
 from lxml import etree
 
 current_schema_version = '2.2'
 
 def get_xml_schema_file_path_by_type(type):
     current_dir = os.path.dirname(__file__)
-    test_schemas_path = os.path.join(current_dir, 'schemas', 'testing', 'testschema.xsd')
-    return test_schemas_path
     schemas_path = os.path.join(current_dir, 'schemas', current_schema_version)
     if type == 'organisation':
         return os.path.join(schemas_path, 'utilities.xsd')
@@ -15,25 +12,10 @@ def get_xml_schema_file_path_by_type(type):
         return os.path.join(schemas_path, 'procedure.xsd')
     return 'unknown';
 
-def validate_xml_files_by_type(xml_files, type):
-    valid_xmls = []
-    invalid_xmls = []
-    schema_file_path = get_xml_schema_file_path_by_type(type)
-    with open(schema_file_path, 'rb') as schema_file:
-        schema_doc = etree.parse(schema_file)
-        schema = etree.XMLSchema(schema_doc)
-        for xml_file in xml_files:
-            xml_file_parsed = etree.parse(xml_file)
-            print(etree.tostring(xml_file_parsed))
-            print(schema)
-            is_xml_file_valid = schema.validate(xml_file_parsed)
-            if not is_xml_file_valid:
-                print(f'{xml_file.name} is not valid', is_xml_file_valid)
-                log = schema.error_log
-                error = log.last_error
-                print(error)
-                invalid_xmls.append(xml_file)
-            else:
-                print(f'{xml_file.name} is valid', is_xml_file_valid)
-                valid_xmls.append(xml_file)
-    return valid_xmls, invalid_xmls
+def validate_xml_file_against_schema_by_type(xml_file, type):
+    file_path_of_schema_for_type = get_xml_schema_file_path_by_type(type)
+    with open(file_path_of_schema_for_type, 'rb') as schema_file:
+        schema_file_parsed = etree.parse(schema_file)
+        schema = etree.XMLSchema(schema_file_parsed)
+        xml_file_parsed = etree.parse(xml_file)
+        return schema.validate(xml_file_parsed)
