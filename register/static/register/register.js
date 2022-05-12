3dafcfa70497a7088a51691f7281939aed82d0e5
David Chan <davidchan125@hotmail.co.uk>
2022-05-12 16:43:42 +0100

    Update metadata registration page - Files in file input start validation process automatically - Refactor register.js

--- register/static/register/register.js
+++ register/static/register/register.js
@@ -6,13 +6,10 @@ function htmlToElement(html) {
     return template.content.firstChild;
 }
 
-const filesInput = document.getElementById("id_files");
-function loadUploadedFilesList() {
-    const files = Array.from(filesInput.files);
-    const uploadedFilesList = document.querySelector(".list-uploaded-files");
-    uploadedFilesList.innerHTML = "";
+function listUploadedFiles(files, listElem) {
+    listElem.innerHTML = "";
     files.forEach((file, i) => {
-        uploadedFilesList.append(htmlToElement(
+        listElem.append(htmlToElement(
             `<li class="list-group-item">
                 <div class="row g-lg-4 g-sm-2 py-2">
                     <div class="col-lg-8">
@@ -23,7 +20,8 @@ function loadUploadedFilesList() {
                         </div>
                     </div>
                     <div class="col-lg-4">
-                        Validation progress indicator will appear here
+                        <div class="file-validation-status">
+                        </div>
                     </div>
                 </div>
             </li>`
@@ -31,17 +29,80 @@ function loadUploadedFilesList() {
     });
 }
 
-const collapseGuidance = document.getElementById('collapse-guidance')
+async function validateXmlFileAgainstSchema(file) {
+    const formData = new FormData();
+    const csrfMiddlewareToken = document.querySelector("input[name='csrfmiddlewaretoken']").value;
+    formData.append("csrfmiddlewaretoken", csrfMiddlewareToken);
+    formData.append("file", file);
+    const response = await fetch(`${window.location.protocol}//${window.location.host}${window.location.pathname}validation/schema`, {
+        method: "POST",
+        body: formData
+    });
+    if (!response.ok) {
+        return "error";
+    }
+    const isFileValid = await response.json();
+    return isFileValid.result;
+}
+
+function updateXMLFileValidationStatus(state, statusElem) {
+    const statusElemContent = htmlToElement(`
+    <div class="d-flex">
+    </div>
+    `);
+    if (state === "validating") {
+        statusElemContent.innerHTML = `
+        <div class="spinner-grow-container me-2">
+            <div class="spinner-grow" style="width: 1rem; height: 1rem;" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+        Validating...
+        `;
+        return statusElem.append(statusElemContent);
+    } else if (state === "valid") {
+        return statusElem.innerHTML = `Valid`;
+    } else if (state === "invalid") {
+        return statusElem.innerHTML = `Invalid`;
+    } else if (state === "error") {
+        return statusElem.innerHTML = "Encountered an error during validation.";
+    }
+    return statusElem.innerHTML = "Unknown";
+}
+
+async function handleFileUpload(fileInput, listElem) {
+    const files = Array.from(fileInput.files);
+    listUploadedFiles(files, listElem);
+    const validationStatusElem = document.querySelector(".file-validation-status");
+    
+    for (const file of files) {
+        updateXMLFileValidationStatus("validating", validationStatusElem);
+        const isFileValid = await validateXmlFileAgainstSchema(file);
+        if (isFileValid === "error") {
+            updateXMLFileValidationStatus("error", validationStatusElem);
+            continue;
+        }
+        updateXMLFileValidationStatus((isFileValid) ? "valid" : "invalid", validationStatusElem);
+    }
+}
+
+const collapseGuidance = document.getElementById("collapse-guidance")
 const bsCollapseGuidance = new bootstrap.Collapse(collapseGuidance, {
     toggle: false
 });
-filesInput.addEventListener("change", event => {
-    loadUploadedFilesList();
+const fileInput = document.getElementById("id_files");
+const uploadedFilesList = document.querySelector(".list-uploaded-files");
+
+fileInput.addEventListener("change", async event => {
+    await handleFileUpload(fileInput, uploadedFilesList);
     bsCollapseGuidance.show();
 });
 
-document.getElementById("register-script").addEventListener("load", event => {
-    if (filesInput.value !== "") {
-        loadUploadedFilesList();
+document.getElementById("register-script").addEventListener("load", async event => {
+    if (fileInput.value !== "") {
+        // In case files have been entered into the file input
+        // and the user refreshes the page.
+        await handleFileUpload(fileInput, uploadedFilesList);
+        bsCollapseGuidance.show();
     }
 });
