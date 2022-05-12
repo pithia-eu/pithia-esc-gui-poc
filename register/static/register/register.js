f9197e566a8e5235f84f72d1a2ec9083e02b6ec4
David Chan <davidchan125@hotmail.co.uk>
2022-05-12 09:56:39 +0100

    Remove auto-gen'd form controls from register page - Replace with placeholder for validation progress indicator

--- register/static/register/register.js
+++ register/static/register/register.js
@@ -15,7 +15,7 @@ function loadUploadedFilesList() {
         uploadedFilesList.append(htmlToElement(
             `<li class="list-group-item">
                 <div class="row g-lg-4 g-sm-2 py-2">
-                    <div class="col-lg-6">
+                    <div class="col-lg-8">
                         <div class="d-flex align-items-center">
                             <img src="/static/register/file.svg" alt="file" class="me-3">
                             <input type="hidden" name="file${i}-name" value="${file.name}">
@@ -23,22 +23,7 @@ function loadUploadedFilesList() {
                         </div>
                     </div>
                     <div class="col-lg-4">
-                        <div class="d-flex flex-wrap justify-content-xl-center">
-                            <div class="d-flex align-items-center me-3">
-                                <input type="radio" class="me-1" id="radio-is-file${i}-model" name="file${i}-metadata-type" value="model" required>
-                                <label for="radio-is-file${i}-model" class="mb-0">Model</label>
-                            </div>
-                            <div class="d-flex align-items-center">
-                                <input type="radio" class="me-1" id="radio-is-file${i}-dataset" name="file${i}-metadata-type" value="dataset" required>
-                                <label for="radio-is-file${i}-dataset" class="mb-0">Measurement</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2">
-                        <div class="d-flex align-items-center justify-content-lg-end">
-                            <input type="checkbox" class="me-1" id="checkbox-is-file${i}-executable" name="is-file${i}-executable">
-                            <label for="checkbox-is-file${i}-executable" class="mb-0">Executable</label>
-                        </div>
+                        Validation progress indicator will appear here
                     </div>
                 </div>
             </li>`
