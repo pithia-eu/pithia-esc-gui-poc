40f3f1c073f3f06cffa0a4b94295a9efa2ec9be2
David Chan <davidchan125@hotmail.co.uk>
2022-05-13 11:15:07 +0100

    Improve registration page validation messages - Improve styling and message detail of xml validation messages - Update registration page styling

--- register/static/register/register.js
+++ register/static/register/register.js
@@ -14,7 +14,7 @@ function listUploadedFiles(files, listElem) {
                 <div class="row g-lg-4 g-sm-2 py-2">
                     <div class="col-lg-8">
                         <div class="d-flex align-items-center">
-                            <img src="/static/register/file.svg" alt="file" class="me-3">
+                            <img src="/static/register/file.svg" alt="file" class="file-icon me-3">
                             <input type="hidden" name="file${i}-name" value="${file.name}">
                             <span class="file-name" title=${file.name}>${file.name}</span>
                         </div>
@@ -29,7 +29,15 @@ function listUploadedFiles(files, listElem) {
     });
 }
 
-async function validateXmlFileAgainstSchema(file) {
+const xmlValidationStates =  {
+    VALIDATING: "validating",
+    VALID: "valid",
+    INVALID_SEMANTICS: "invalid",
+    INVALID_SYNTAX: "XMLSyntaxError",
+    INTERNAL_SERVER_ERROR: "InternalServerError",
+}
+
+async function validateXmlFile(file) {
     const formData = new FormData();
     const csrfMiddlewareToken = document.querySelector("input[name='csrfmiddlewaretoken']").value;
     formData.append("csrfmiddlewaretoken", csrfMiddlewareToken);
@@ -38,36 +46,56 @@ async function validateXmlFileAgainstSchema(file) {
         method: "POST",
         body: formData
     });
-    if (!response.ok) {
-        return "error";
+    console.log(response.status);
+    if (response.status === 422) {
+        return xmlValidationStates.INVALID_SYNTAX;
     }
-    const isFileValid = await response.json();
-    return isFileValid.result;
+    if (!response.ok) {
+        return xmlValidationStates.INTERNAL_SERVER_ERROR;
+    }
+    const responseContent = await response.json();
+    const isFileValid = (responseContent.result) ? xmlValidationStates.VALID : xmlValidationStates.INVALID_SEMANTICS;
+    return isFileValid;
 }
 
 function updateXMLFileValidationStatus(state, statusElem) {
     const statusElemContent = htmlToElement(`
-    <div class="d-flex">
+    <div class="d-flex align-items-center">
     </div>
     `);
     if (state === "validating") {
         statusElemContent.innerHTML = `
-        <div class="spinner-grow-container me-2">
-            <div class="spinner-grow" style="width: 1rem; height: 1rem;" role="status">
-                <span class="visually-hidden">Loading...</span>
+            <div class="spinner-grow-container text-muted me-2">
+                <div class="spinner-grow" style="width: 1rem; height: 1rem;" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
             </div>
-        </div>
-        Validating...
+            <span class="text-muted">Validating...</span>
         `;
-        return statusElem.append(statusElemContent);
-    } else if (state === "valid") {
-        return statusElem.innerHTML = `Valid`;
-    } else if (state === "invalid") {
-        return statusElem.innerHTML = `Invalid`;
-    } else if (state === "error") {
-        return statusElem.innerHTML = "Encountered an error during validation.";
+    } else if (state === xmlValidationStates.VALID) {
+        statusElemContent.innerHTML = `
+            <img src="/static/register/tick.svg" alt="tick" class="me-2"><span class="text-success">Valid</span>
+        `;
+    } else if (state === xmlValidationStates.INVALID_SEMANTICS) {
+        statusElemContent.innerHTML = `
+            <img src="/static/register/cross.svg" alt="cross" class="me-2"><span class="text-danger">Semantically incorrect.</span>
+        `;
+    } else if (state === xmlValidationStates.INVALID_SYNTAX) {
+        statusElemContent.innerHTML = `
+            <img src="/static/register/cross.svg" alt="cross" class="me-2"><span class="text-danger">Invalid syntax.</span>
+        `;
+    } else if (state === xmlValidationStates.INTERNAL_SERVER_ERROR) {
+        statusElemContent.innerHTML = `
+            <img src="/static/register/cross.svg" alt="cross" class="me-2"><span class="text-danger">Encountered an unknown error during validation.</span>
+        `;
+    } else {
+        statusElemContent.innerHTML = `
+            <img src="/static/register/cross.svg" alt="cross" class="me-2"><span class="text-danger">Unknown state.</span>
+        `;
     }
-    return statusElem.innerHTML = "Unknown";
+    
+    statusElem.innerHTML = "";
+    return statusElem.append(statusElemContent);
 }
 
 async function handleFileUpload(fileInput, listElem) {
@@ -76,13 +104,9 @@ async function handleFileUpload(fileInput, listElem) {
     const validationStatusElem = document.querySelector(".file-validation-status");
     
     for (const file of files) {
-        updateXMLFileValidationStatus("validating", validationStatusElem);
-        const isFileValid = await validateXmlFileAgainstSchema(file);
-        if (isFileValid === "error") {
-            updateXMLFileValidationStatus("error", validationStatusElem);
-            continue;
-        }
-        updateXMLFileValidationStatus((isFileValid) ? "valid" : "invalid", validationStatusElem);
+        updateXMLFileValidationStatus(xmlValidationStates.VALIDATING, validationStatusElem);
+        const validationResults = await validateXmlFile(file);
+        updateXMLFileValidationStatus(validationResults, validationStatusElem);
     }
 }
 
