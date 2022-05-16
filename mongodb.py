2c7745f19178c96d63860997ae602c8dcbc5e2e9
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-05-16 17:23:15 +0100

    Fix SSL handshake issue with mongodb

--- mongodb.py
+++ mongodb.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 import environ
+import certifi
 
 env = environ.Env()
 
-client = MongoClient(env('MONGODB_CONNECTION_STRING'))
+client = MongoClient(env('MONGODB_CONNECTION_STRING'), tlsCAFile=certifi.where())
 db = client[env('DB_NAME')]
