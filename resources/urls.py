b77e10070b6df870d313afff6d7239dfc784c5cc
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-05-17 01:01:40 +0100

    Update resources url path variable - Rename 'resources' to 'resource_collection_name'

--- resources/urls.py
+++ resources/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'resources'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<resources>/', views.list_resource_namespaces, name='list_resource_namespaces'),
-    path('<resources>/<namespace>/', views.list_resources_in_namespace, name='list_resources_in_namespace'),
-    path('<resources>/<namespace>/<local_id>/', views.detail, name='detail'),
+    path('<resource_collection_name>/', views.list_resource_namespaces, name='list_resource_namespaces'),
+    path('<resource_collection_name>/<namespace>/', views.list_resources_in_namespace, name='list_resources_in_namespace'),
+    path('<resource_collection_name>/<namespace>/<local_id>/', views.detail, name='detail'),
 ]
--- resources/views.py
+++ resources/views.py
@@ -5,29 +5,29 @@ from mongodb import db
 def index(request):
     return render(request, 'resources/index.html', {})
 
-def list_resource_namespaces(request, resources):
-    namespaces = list(db[resources].find({}).distinct('identifier.pithia:Identifier.namespace'))
+def list_resource_namespaces(request, resource_collection_name):
+    namespaces = list(db[resource_collection_name].find({}).distinct('identifier.pithia:Identifier.namespace'))
     return render(request, 'resources/list_resource_namespaces.html', {
-        'resources': resources,
+        'resource_collection_name': resource_collection_name,
         'namespaces': namespaces,
     })
 
-def list_resources_in_namespace(request, resources, namespace):
-    resources_list = list(db[resources].find({
+def list_resources_in_namespace(request, resource_collection_name, namespace):
+    resources_list = list(db[resource_collection_name].find({
         'identifier.pithia:Identifier.namespace': namespace
     }))
     return render(request, 'resources/list_resources_in_namespace.html', {
         'namespace': namespace,
-        'resources': resources,
+        'resource_collection_name': resource_collection_name,
         'resources_list': resources_list
     })
 
-def detail(request, resources, namespace, local_id):
-    resource = db[resources].find_one({
+def detail(request, resource_collection_name, namespace, local_id):
+    resource = db[resource_collection_name].find_one({
         'identifier.pithia:Identifier.localID': local_id,
         'identifier.pithia:Identifier.namespace': namespace,
     })
     return render(request, 'resources/detail.html', {
-        'resources': resources,
+        'resource_collection_name': resource_collection_name,
         'resource': resource
     })
