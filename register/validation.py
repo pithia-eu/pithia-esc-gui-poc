ff4ab857f4bd0956fb5a1938ea06869e94c82043
David Chan <davidchan125@hotmail.co.uk>
2022-05-18 17:11:20 +0100

    Update validate_xml_xlinks_by_type method - Add get_ontology_term_from_xlink_href_and_type

--- register/validation.py
+++ register/validation.py
@@ -17,8 +17,6 @@ def get_xml_schema_file_path_by_type(type):
         return os.path.join(schemas_path, 'project.xsd')
     elif type == 'platform':
         return os.path.join(schemas_path, 'utilities.xsd')
-    elif type == 'instrument':
-        return os.path.join(schemas_path, 'utilities.xsd')
     elif type == 'operation':
         return os.path.join(schemas_path, 'utilities.xsd')
     elif type == 'instrument':
@@ -31,7 +29,30 @@ def get_xml_schema_file_path_by_type(type):
         return os.path.join(schemas_path, 'process.xsd')
     elif type == 'data-collection':
         return os.path.join(schemas_path, 'observationCollection.xsd')
-    return 'unknown';
+    return 'unknown'
+
+def get_collection_name_from_component(component):
+    if type == 'organisation':
+        return 'organisations'
+    elif type == 'individual':
+        return 'individuals'
+    elif type == 'project':
+        return 'projects'
+    elif type == 'platform':
+        return 'platforms'
+    elif type == 'operation':
+        return 'operations'
+    elif type == 'instrument':
+        return 'instruments'
+    elif type == 'acquisition':
+        return 'acquisitions'
+    elif type == 'computation':
+        return 'computations'
+    elif type == 'process':
+        return 'processes'
+    elif type == 'data-collection':
+        return 'data-collections'
+    return 'unknown'
 
 def parse_xml_file(xml_file):
     # Returns an ElementTree
@@ -43,23 +64,31 @@ def validate_xml_against_schema(xml_file_parsed, schema_file_path):
         schema = etree.XMLSchema(schema_file_parsed)
         return schema.validate(xml_file_parsed)
 
-def get_entities_from_xlink_href_and_type(url, type):
-    ESPAS_RESOURCES_URL = 'http://resources.espas-fp7.eu/'
+def get_ontology_term_from_xlink_href_and_type(href, type):
+    ESPAS_ONTOLOGY_URL = 'http://ontology.espas-fp7.eu/' # e.g. http://ontology.espas-fp7.eu/relatedPartyRole/Operator
+    return
+
+def get_entities_from_xlink_href_and_type(href):
+    print('yo')
+    ESPAS_RESOURCES_URL = 'http://resources.espas-fp7.eu/' # e.g. # https://resources.pithia.eu/2.2/pithia/organisation/uml/UML/1
     PITHIA_RESOURCES_URL = 'https://resources.pithia.eu/2.2/pithia/'
-    url = url.replace(ESPAS_RESOURCES_URL, '')
-    url_components = url.split('/')
-    namespace = url_components[1]
-    localID = url_components[2]
-    if len(url_components) > 3:
-        version = url_components[3]
+    href = href.replace(ESPAS_RESOURCES_URL, '')
+    href_components = href.split('/')
+    component_referenced = href_components[0]
+    collection_for_component = get_collection_name_from_component(component_referenced)
+    namespace = href_components[1]
+    localID = href_components[2]
+    if len(href_components) > 3:
+        version = href_components[3]
     find_dictionary = {
         'identifier.ESPAS_Identifier.localID': localID,
         'identifier.ESPAS_Identifier.namespace': namespace,
     }
     if version:
         find_dictionary['identifier.ESPAS_Identifier.version'] = version
-    # https://resources.pithia.eu/2.2/pithia/organisation/uml/UML/1
-    return db['organisations'].find_one(find_dictionary)
+    return db[collection_for_component].find_one(find_dictionary)
+
+
 
 def validate_xml_xlinks_by_type(xml_file_parsed, type):
     missing_xlinks = []
@@ -68,8 +97,9 @@ def validate_xml_xlinks_by_type(xml_file_parsed, type):
     parent = xml_file_parsed.getroot()
     xlinks = parent.xpath("//@*[local-name()='href' and namespace-uri()='http://www.w3.org/1999/xlink']")
     if (len(xlinks) > 0):
-        for xlink in xlinks:
-            db_query = get_entities_from_xlink_href_and_type(xlink, type)
-        print('organisations', list(db_query))
+        print(xlinks)
+        print(list(xlinks))
+        for xlink in list(xlinks):
+            xlink_reference = get_entities_from_xlink_href_and_type(xlink)
     
     return missing_xlinks
