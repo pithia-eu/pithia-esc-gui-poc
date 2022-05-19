7072bdf6e803412d26e04f99c797f0829ba85f14
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-05-19 17:57:18 +0100

    Remove forms.py for search app - forms.py was unused.

--- search/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import forms
-
-class SearchForm(forms.Form):
-    file = forms.FileField(label='Model or Dataset metadata file')
-
-    def __init__(self, *args, **kwargs):
-        super(SearchForm, self).__init__(*args, **kwargs)
-        for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'form-control'
