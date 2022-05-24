50a8bb8e107884314afafd20abaaa63168361681
David Chan <davidchan125@hotmail.co.uk>
2022-05-24 10:36:25 +0100

    Add properties table to resource detail page

--- resources/views.py
+++ resources/views.py
@@ -22,12 +22,29 @@ def list_resources_in_namespace(request, resource_collection_name, namespace):
         'resources_list': resources_list
     })
 
+def flatten(d):
+    out = {}
+    for key, value in d.items():
+        if isinstance(value, dict):
+            value = [value]
+        if isinstance(value, list):
+            for subdict in value:
+                deeper = flatten(subdict).items()
+                out.update({
+                    key + '.' + key2: value2 for key2, value2 in deeper
+                })
+        else:
+            out[key] = value
+    return out
+
 def detail(request, resource_collection_name, namespace, local_id):
     resource = db[resource_collection_name].find_one({
         'identifier.pithia:Identifier.localID': local_id,
         'identifier.pithia:Identifier.namespace': namespace,
     })
+    resource_flattened = flatten(resource)
     return render(request, 'resources/detail.html', {
         'resource_collection_name': resource_collection_name,
-        'resource': resource
+        'resource': resource,
+        'resource_flattened': resource_flattened
     })
