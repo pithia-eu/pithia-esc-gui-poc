cc77fdfd99e104df4c7a01f828e1e1dba9d2e11f
David Chan <davidchan125@hotmail.co.uk>
2022-05-24 09:57:40 +0100

    Update metadata registration page

--- register/forms.py
+++ register/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField(label='Select a metadata file to upload:', widget=forms.ClearableFileInput(attrs={
+    file = forms.FileField(label='File Input', widget=forms.ClearableFileInput(attrs={
                 'accept': 'application/xml',
                 'class': 'form-control'
             }))
