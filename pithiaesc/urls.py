eba61b0a13a7291c085b58c0964eb87b2b26ab91
David Chan <davidchan125@hotmail.co.uk>
2022-06-10 22:49:11 +0100

    Update root url for resources app

--- pithiaesc/urls.py
+++ pithiaesc/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('resources/', include('resources.urls')),
+    path('', include('resources.urls')),
     path('search/', include('search.urls')),
     path('register/', include('register.urls')),
     path('admin/', admin.site.urls),
