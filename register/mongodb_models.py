daefa610f58e725e6309889ad506befe48657865
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-06-16 00:13:48 +0100

    Fix issues with mongodb_models.py

--- register/mongodb_models.py
+++ register/mongodb_models.py
@@ -1,61 +1,23 @@
 from mongodb import db
 
-def CurrentOrganisation():
-    return db['current-organisations']
+CurrentOrganisation = db['current-organisations']
+CurrentIndividual = db['current-individuals']
+CurrentProject = db['current-projects']
+CurrentPlatform = db['current-platforms']
+CurrentInstrument = db['current-instruments']
+CurrentOperation = db['current-operations']
+CurrentAcquisition = db['current-acquisitions']
+CurrentComputation = db['current-computations']
+CurrentProcess = db['current-processes']
+CurrentDataCollection = db['current-data-collections']
 
-def CurrentIndividual():
-    return db['current-individuals']
-
-def CurrentProject():
-    return db['current-projects']
-
-def CurrentPlatform():
-    return db['current-platforms']
-
-def CurrentInstrument():
-    return db['current-instruments']
-
-def CurrentOperation():
-    return db['current-operations']
-
-def CurrentAcquisition():
-    return db['current-acquisitions']
-
-def CurrentComputation():
-    return db['current-computations']
-
-def CurrentProcess():
-    return db['current-processes']
-
-def CurrentDataCollection():
-    return db['current-data-collections']
-
-def OrganisationRevision():
-    return db['organisation-revisions']
-
-def IndividualRevision():
-    return db['individual-revisions']
-
-def ProjectRevision():
-    return db['project-revisions']
-
-def PlatformRevision():
-    return db['platform-revisions']
-
-def InstrumentRevision():
-    return db['instrument-revisions']
-
-def OperationRevision():
-    return db['operation-revisions']
-
-def AcquisitionRevision():
-    return db['acquisition-revisions']
-
-def ComputationRevision():
-    return db['computation-revisions']
-
-def ProcessRevision():
-    return db['processe-revisions']
-
-def DataCollectionRevision():
-    return db['data-collection-revisions']
+OrganisationRevision = db['organisation-revisions']
+IndividualRevision = db['individual-revisions']
+ProjectRevision = db['project-revisions']
+PlatformRevision = db['platform-revisions']
+InstrumentRevision = db['instrument-revisions']
+OperationRevision = db['operation-revisions']
+AcquisitionRevision = db['acquisition-revisions']
+ComputationRevision = db['computation-revisions']
+ProcessRevision = db['processe-revisions']
+DataCollectionRevision = db['data-collection-revisions']
