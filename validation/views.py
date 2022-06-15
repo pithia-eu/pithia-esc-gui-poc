9cc441d156202d3a0ff35c58aa190aa885353b51
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-06-15 22:49:49 +0100

    Rename method in validation app views.py

--- validation/views.py
+++ validation/views.py
@@ -1,13 +1,12 @@
 import json
 from django.http import HttpResponse, HttpResponseServerError
-from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from lxml import etree
 
 from validation.validation import validate_acquisition_metadata_xml_file, validate_computation_metadata_xml_file, validate_data_collection_metadata_xml_file, validate_individual_metadata_xml_file, validate_instrument_metadata_xml_file, validate_operation_metadata_xml_file, validate_organisation_metadata_xml_file, validate_platform_metadata_xml_file, validate_process_metadata_xml_file, validate_project_metadata_xml_file
 
 # Create your views here.
-def handle_validation_results(request, validation_results):
+def process_validation_results_and_send_httpresponse(request, validation_results):
     if 'error' not in validation_results:
         return HttpResponse(json.dumps({
             'result': 'valid'
@@ -20,58 +19,58 @@ def handle_validation_results(request, validation_results):
 def organisation(request):
     xml_file = request.FILES['file']
     xml_file_validation_results = validate_organisation_metadata_xml_file(xml_file)
-    return handle_validation_results(request, xml_file_validation_results)
+    return process_validation_results_and_send_httpresponse(request, xml_file_validation_results)
     
 @require_POST
 def individual(request):
     xml_file = request.FILES['file']
     xml_file_validation_results = validate_individual_metadata_xml_file(xml_file)
-    return handle_validation_results(request, xml_file_validation_results)
+    return process_validation_results_and_send_httpresponse(request, xml_file_validation_results)
 
 @require_POST
 def project(request):
     xml_file = request.FILES['file']
     xml_file_validation_results = validate_project_metadata_xml_file(xml_file)
-    return handle_validation_results(request, xml_file_validation_results)
+    return process_validation_results_and_send_httpresponse(request, xml_file_validation_results)
 
 @require_POST
 def platform(request):
     xml_file = request.FILES['file']
     xml_file_validation_results = validate_platform_metadata_xml_file(xml_file)
-    return handle_validation_results(request, xml_file_validation_results)
+    return process_validation_results_and_send_httpresponse(request, xml_file_validation_results)
 
 @require_POST
 def instrument(request):
     xml_file = request.FILES['file']
     xml_file_validation_results = validate_instrument_metadata_xml_file(xml_file)
-    return handle_validation_results(request, xml_file_validation_results)
+    return process_validation_results_and_send_httpresponse(request, xml_file_validation_results)
 
 @require_POST
 def operation(request):
     xml_file = request.FILES['file']
     xml_file_validation_results = validate_operation_metadata_xml_file(xml_file)
-    return handle_validation_results(request, xml_file_validation_results)
+    return process_validation_results_and_send_httpresponse(request, xml_file_validation_results)
 
 @require_POST
 def acquisition(request):
     xml_file = request.FILES['file']
     xml_file_validation_results = validate_acquisition_metadata_xml_file(xml_file)
-    return handle_validation_results(request, xml_file_validation_results)
+    return process_validation_results_and_send_httpresponse(request, xml_file_validation_results)
 
 @require_POST
 def computation(request):
     xml_file = request.FILES['file']
     xml_file_validation_results = validate_computation_metadata_xml_file(xml_file)
-    return handle_validation_results(request, xml_file_validation_results)
+    return process_validation_results_and_send_httpresponse(request, xml_file_validation_results)
 
 @require_POST
 def process(request):
     xml_file = request.FILES['file']
     xml_file_validation_results = validate_process_metadata_xml_file(xml_file)
-    return handle_validation_results(request, xml_file_validation_results)
+    return process_validation_results_and_send_httpresponse(request, xml_file_validation_results)
 
 @require_POST
 def data_collection(request):
     xml_file = request.FILES['file']
     xml_file_validation_results = validate_data_collection_metadata_xml_file(xml_file)
-    return handle_validation_results(request, xml_file_validation_results)
+    return process_validation_results_and_send_httpresponse(request, xml_file_validation_results)
