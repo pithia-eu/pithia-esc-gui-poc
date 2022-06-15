bb0fd34a556f4dd769c153e0d43f3472d967b55d
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-06-16 00:14:13 +0100

    Add urls for register app

--- register/urls.py
+++ register/urls.py
@@ -5,5 +5,14 @@ from . import views
 app_name = 'register'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<resource_type>/', views.resource_metadata_upload, name='resource_metadata_upload'),
+    path('organisation/', views.organisation, name='organisation'),
+    path('individual/', views.individual, name='individual'),
+    path('project/', views.project, name='project'),
+    path('platform/', views.platform, name='platform'),
+    path('instrument/', views.instrument, name='instrument'),
+    path('operation/', views.operation, name='operation'),
+    path('acquisition/', views.acquisition, name='acquisition'),
+    path('computation/', views.computation, name='computation'),
+    path('process/', views.process, name='process'),
+    path('data-collection/', views.data_collection, name='data_collection'),
 ]
