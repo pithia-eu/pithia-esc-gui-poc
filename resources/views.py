60ad56a18c9a0e18bac94d8fffc4ea73cc3d9dfe
David Chan <davidchan125@hotmail.co.uk>
2022-06-16 19:06:59 +0100

    Update register app views and templates - Refactor views.py - Refactor and update content within templates - Update h1 headings for templates

--- resources/views.py
+++ resources/views.py
@@ -1,61 +1,84 @@
+from register import mongodb_models
 from django.shortcuts import render
-from mongodb import db
-from register.upload import ACQUISITION, COMPUTATION, DATA_COLLECTION, INDIVIDUAL, INSTRUMENT, OPERATION, ORGANISATION, PLATFORM, PROCESS, PROJECT, current_resource_version_collection_names
 
-def get_resource_type_by_url_namespace(url_namespace):
-    if ORGANISATION in url_namespace:
-        return ORGANISATION
-    if INDIVIDUAL in url_namespace:
-        return INDIVIDUAL
-    if PROJECT in url_namespace:
-        return PROJECT
-    if PLATFORM in url_namespace:
-        return PLATFORM
-    if INSTRUMENT in url_namespace:
-        return INSTRUMENT
-    if OPERATION in url_namespace:
-        return OPERATION
-    if ACQUISITION in url_namespace:
-        return ACQUISITION
-    if COMPUTATION in url_namespace:
-        return COMPUTATION
-    if PROCESS in url_namespace:
-        return PROCESS
-    if DATA_COLLECTION in url_namespace:
-        return DATA_COLLECTION
-    return 'Unknown'
+def _get_view_helper_variables_by_url_namespace(url_namespace):
+    mongodb_model = None
+    resource_type = ''
+    resource_type_plural = ''
+    if 'organisation' in url_namespace:
+        mongodb_model = mongodb_models.CurrentOrganisation
+        resource_type = 'Organisation'
+        resource_type_plural = 'Organisations'
+    if 'individual' in url_namespace:
+        mongodb_model = mongodb_models.CurrentIndividual
+        resource_type = 'Individual'
+        resource_type_plural = 'Individuals'
+    if 'project' in url_namespace:
+        mongodb_model = mongodb_models.CurrentProject
+        resource_type = 'Project'
+        resource_type_plural = 'Projects'
+    if 'platform' in url_namespace:
+        mongodb_model = mongodb_models.CurrentPlatform
+        resource_type = 'Platform'
+        resource_type_plural = 'Platforms'
+    if 'instrument' in url_namespace:
+        mongodb_model = mongodb_models.CurrentInstrument
+        resource_type = 'Instrument'
+        resource_type_plural = 'Instruments'
+    if 'operation' in url_namespace:
+        mongodb_model = mongodb_models.CurrentOperation
+        resource_type = 'Operation'
+        resource_type_plural = 'Operations'
+    if 'acquisition' in url_namespace:
+        mongodb_model = mongodb_models.CurrentAcquisition
+        resource_type = 'Acquisition'
+        resource_type_plural = 'Acquisitions'
+    if 'computation' in url_namespace:
+        mongodb_model = mongodb_models.CurrentComputation
+        resource_type = 'Computation'
+        resource_type_plural = 'Computations'
+    if 'process' in url_namespace:
+        mongodb_model = mongodb_models.CurrentProcess
+        resource_type = 'Process'
+        resource_type_plural = 'Processes'
+    if 'data-collection' in url_namespace:
+        mongodb_model = mongodb_models.CurrentDataCollection
+        resource_type = 'Data Collection'
+        resource_type_plural = 'Data Collections'
+    return {
+        'mongodb_model': mongodb_model,
+        'resource_type': resource_type,
+        'resource_type_plural': resource_type_plural,
+    }
 
 # Create your views here.
 def index(request):
     return render(request, 'resources/index.html', {
-        'title': 'Resources'
+        'title': 'Browse'
     })
 
 def list_resource_namespaces(request):
     url_namespace = request.resolver_match.namespace
-    resource_type = get_resource_type_by_url_namespace(url_namespace)
-    current_resource_version_collection_name = current_resource_version_collection_names.get(resource_type, None)
-    namespaces = list(db[current_resource_version_collection_name].find({}).distinct('identifier.pithia:Identifier.namespace'))
-    title = f'{resource_type.capitalize()} Namespaces'
-    if resource_type.lower() == 'data-collection':
-        title = 'Data Collection Namespaces'
+    view_helper_vars = _get_view_helper_variables_by_url_namespace(url_namespace)
+    namespaces = list(view_helper_vars['mongodb_model'].find({}).distinct('identifier.pithia:Identifier.namespace'))
     return render(request, 'resources/list_resource_namespaces.html', {
-        'title': title,
-        'resource_type': resource_type,
+        'title': f'{view_helper_vars["resource_type"]} Namespaces',
+        'resource_type_plural': view_helper_vars['resource_type_plural'],
         'url_namespace': url_namespace,
         'namespaces': namespaces,
     })
 
 def list_resources_in_namespace(request, namespace):
     url_namespace = request.resolver_match.namespace
-    resource_type = get_resource_type_by_url_namespace(url_namespace)
-    current_resource_version_collection_name = current_resource_version_collection_names.get(resource_type, None)
-    resources_list = list(db[current_resource_version_collection_name].find({
+    view_helper_vars = _get_view_helper_variables_by_url_namespace(url_namespace)
+    resources_list = list(view_helper_vars['mongodb_model'].find({
         'identifier.pithia:Identifier.namespace': namespace
     }))
     return render(request, 'resources/list_resources_in_namespace.html', {
+        'title': f'{view_helper_vars["resource_type_plural"]} in the {namespace} namespace',
+        'breadcrumb_item_list_resource_namespaces_text': f'{view_helper_vars["resource_type"]} Namespaces',
+        'resource_type_plural': view_helper_vars['resource_type_plural'],
         'namespace': namespace,
-        'resource_type': resource_type,
         'url_namespace': url_namespace,
         'resources_list': resources_list
     })
@@ -77,15 +100,14 @@ def flatten(d):
 
 def detail(request, namespace, local_id):
     url_namespace = request.resolver_match.namespace
-    resource_type = get_resource_type_by_url_namespace(url_namespace)
-    current_resource_version_collection_name = current_resource_version_collection_names.get(resource_type, None)
-    resource = db[current_resource_version_collection_name].find_one({
+    view_helper_vars = _get_view_helper_variables_by_url_namespace(url_namespace)
+    resource = view_helper_vars['mongodb_model'].find_one({
         'identifier.pithia:Identifier.localID': local_id,
         'identifier.pithia:Identifier.namespace': namespace,
     })
     resource_flattened = flatten(resource)
     return render(request, 'resources/detail.html', {
-        'resource_type': resource_type,
+        'breadcrumb_item_list_resource_namespaces_text': f'{view_helper_vars["resource_type"]} Namespaces',
         'url_namespace': url_namespace,
         'resource': resource,
         'resource_flattened': resource_flattened
