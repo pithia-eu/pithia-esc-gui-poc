4ee1692531aeb201cdfa0f2963d2b7296aa9b071
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-07-12 14:41:24 +0200

    Rename list resources of type view name

--- browse/namespace_urls.py
+++ browse/namespace_urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'browse'
 urlpatterns = [
-    path('', views.list_resources_of_type, name='list_resource_namespaces'),
+    path('', views.list_resources_of_type, name='list_resources_of_type'),
     path('<resource_id>/', views.detail, name='detail'),
 ]
