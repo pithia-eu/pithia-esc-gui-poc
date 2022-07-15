12b99341e25daa200734b0397f2d8eb553ac6f6d
Dimitris Kagialis <kagialisd@gmail.com>
2022-07-15 15:22:21 +0300

    Present app - name of the app will change

--- present/static/present/_temp_/swagger-initializer.js
+++ present/static/present/_temp_/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://vm2.pithia.eu:8080/openapi.json",
+    url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
