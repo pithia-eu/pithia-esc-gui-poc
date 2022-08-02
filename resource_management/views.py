051c4d8a579908542619dd4e87045211fcc029a2
David Chan <davidchan125@hotmail.co.uk>
2022-08-02 18:52:41 +0100

    Update res_man views.py

--- resource_management/views.py
+++ resource_management/views.py
@@ -14,7 +14,7 @@ def index(request):
 def list_resources_of_type(request):
     url_namespace = request.resolver_match.namespace
     view_helper_vars = get_view_helper_variables_by_url_namespace(url_namespace)
-    resources_list = list(view_helper_vars['mongodb_model'].find({}))
+    resources_list = list(view_helper_vars['current_version_mongodb_model'].find({}))
     resources_list = list(map(remove_underscore_from_id_attribute, resources_list))
     return render(request, 'resource_management/list_resources_of_type.html', {
         'title': view_helper_vars["resource_type_plural"],
@@ -26,16 +26,31 @@ def list_resources_of_type(request):
 
 @require_http_methods(["GET", "POST"])
 def update(request, resource_id):
+    url_namespace = request.resolver_match.namespace
+    view_helper_vars = get_view_helper_variables_by_url_namespace(url_namespace)
+    resource_to_update = view_helper_vars['current_version_mongodb_model'].find_one({
+        '_id': ObjectId(resource_id)
+    })
+    resource_name = resource_to_update['identifier']['pithia:Identifier']['localID']
+    if 'name' in resource_to_update:
+        resource_name = resource_to_update['name']
     return render(request, 'resource_management/update.html', {
-        'title': 'Upload new resource metadata'
+        'title': f'Upload New Resource Metadata for {resource_name}',
+        'url_namespace': url_namespace
     })
 
 @require_POST
 def delete(request, resource_id):
     url_namespace = request.resolver_match.namespace
-    print(url_namespace)
     view_helper_vars = get_view_helper_variables_by_url_namespace(url_namespace)
-    view_helper_vars['mongodb_model'].delete_one({
+    resource_to_delete = view_helper_vars['current_version_mongodb_model'].find_one({
         '_id': ObjectId(resource_id)
+    })
+    view_helper_vars['current_version_mongodb_model'].delete_one({
+        '_id': ObjectId(resource_id)
+    })
+    view_helper_vars['resource_revision_mongodb_model'].delete_many({
+        'identifier.PITHIA_Identifier.localID': resource_to_delete['identifier']['PITHIA_Identifier']['localID'],
+        'identifier.PITHIA_Identifier.namespace': resource_to_delete['identifier']['PITHIA_Identifier']['namespace'],
     })
     return HttpResponseRedirect(reverse(f'{url_namespace}:list_resources_of_type'))
