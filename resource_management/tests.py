79026f115177081573889fbce76978580b076a0b
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-08-12 12:07:07 +0100

    Add test_update_organisation test in res_man app

--- resource_management/tests.py
+++ resource_management/tests.py
@@ -1,6 +1,10 @@
+import os
 from django.test import TestCase, SimpleTestCase
 import environ
 import mongomock
+from register.register import register_metadata_xml_file
+from validation import validation
+from resource_management import tests
 
 env = environ.Env()
 
@@ -9,10 +13,10 @@ env = environ.Env()
 # create/destroy that Django automatically does with the default
 # TestCase class. MongoDB is still used.
 class ResourceManagementTestCase(SimpleTestCase):
-    def test_update_resource(self):
+    def test_update_organisation(self):
         """Update a resource and check the overwritten resource is in the revisions collection"""
         client = mongomock.MongoClient()
-        db = client[env('DB_NAME')]
-        db.insert_one({
-            
-        })
+        db = client[env('DB_NAME')]['current-organisations']
+        with open(os.path.join(os.path.curdir, 'resource_management', 'tests', 'Organisation_TEST.xml'), 'r') as file:
+            validation.validate_organisation_metadata_xml_file(file)
+            register_metadata_xml_file(file, db, None)
