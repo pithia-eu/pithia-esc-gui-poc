efcecddbabdc2903bcd95e93567d03c7885ac0f3
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-08-15 12:28:17 +0100

    Update helpers.py - Add functions to aid with cascade delete functionality

--- resource_management/helpers.py
+++ resource_management/helpers.py
@@ -1,3 +1,6 @@
+from register.mongodb_models import CurrentIndividual
+
+
 def get_lower_level_resource_types(resource_type):
     resource_types = [
         'organisation',
@@ -13,5 +16,10 @@ def get_lower_level_resource_types(resource_type):
     ]
     return resource_types[:resource_types.index(resource_type)]
 
-def get_mongodb_models_from_resource_types(resource_types):
-    
+def create_resource_url(namespace, resource_type, localid):
+    return f'https://metadata.pithia.eu/resources/2.2/{namespace}/{resource_type}/{localid}'
+
+def get_individuals_referencing_resource_url(resource_url):
+    return CurrentIndividual.find_one({
+        
+    })
