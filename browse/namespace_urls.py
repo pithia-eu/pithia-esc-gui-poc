37f123814c96b074c2d813c8de7d2d1b92caa695
David Chan <davidchan125@hotmail.co.uk>
2022-08-17 19:41:54 +0100

    Delete namespace_urls.py

--- browse/namespace_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from . import views
-
-app_name = 'browse'
-urlpatterns = [
-    path('', views.list_resources_of_type, name='list_resources_of_type'),
-    path('<resource_id>/', views.detail, name='detail'),
-]
