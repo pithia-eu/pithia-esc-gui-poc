54a988de9fbc219242e1ecae362bb48da62ee24c
David Chan <davidchan125@hotmail.co.uk>
2022-08-17 18:38:59 +0100

    Add mongodb_models.py to common app

---
+++ common/mongodb_models.py
@@ -0,0 +1,25 @@
+from mongodb import db
+
+CurrentOrganisation = db['current-organisations']
+CurrentIndividual = db['current-individuals']
+CurrentProject = db['current-projects']
+CurrentPlatform = db['current-platforms']
+CurrentInstrument = db['current-instruments']
+CurrentOperation = db['current-operations']
+CurrentAcquisition = db['current-acquisitions']
+CurrentComputation = db['current-computations']
+CurrentProcess = db['current-processes']
+CurrentDataCollection = db['current-data-collections']
+
+OrganisationRevision = db['organisation-revisions']
+IndividualRevision = db['individual-revisions']
+ProjectRevision = db['project-revisions']
+PlatformRevision = db['platform-revisions']
+InstrumentRevision = db['instrument-revisions']
+OperationRevision = db['operation-revisions']
+AcquisitionRevision = db['acquisition-revisions']
+ComputationRevision = db['computation-revisions']
+ProcessRevision = db['processe-revisions']
+DataCollectionRevision = db['data-collection-revisions']
+
+OriginalMetadataXml = db['original-metadata-xmls']
