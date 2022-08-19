80f58dc017ed8454232086980c3e1b6edb7c028a
David Chan <davidchan125@hotmail.co.uk>
2022-08-19 11:13:50 +0100

    Update delete urls.py

--- delete/urls.py
+++ delete/urls.py
@@ -3,15 +3,15 @@ from django.urls import include, path
 from . import views
 
 app_name = 'delete'
-urlpatterns = [=
-    path('organisations/<organisation_id>/delete/', views),
-    path('individuals/<individual_id>/delete/', include('resource_management.individual_urls')),
-    path('projects/<project_id>/delete/', include('resource_management.project_urls')),
-    path('platforms/<platform_id>/delete/', include('resource_management.platform_urls')),
-    path('instruments/<instrument_id>/delete/', include('resource_management.instrument_urls')),
-    path('operations/<operation_id>/delete/', include('resource_management.operation_urls')),
-    path('acquisitions/<acquisition_id>/delete/', include('resource_management.acquisition_urls')),
-    path('computations/<computation_id>/delete/', include('resource_management.computation_urls')),
-    path('processes/<process_id>/delete/', include('resource_management.process_urls')),
-    path('data-collections/<data_collection_id>/delete/', include('resource_management.data_collection_urls')),
+urlpatterns = [
+    path('organisations/<organisation_id>/delete/', views.organisation, name='organisation'),
+    path('individuals/<individual_id>/delete/', views.individual, name='individual'),
+    path('projects/<project_id>/delete/', views.project, name='project'),
+    path('platforms/<platform_id>/delete/', views.platform, name='platform'),
+    path('instruments/<instrument_id>/delete/', views.instrument, name='instrument'),
+    path('operations/<operation_id>/delete/', views.operation, name='operation'),
+    path('acquisitions/<acquisition_id>/delete/', views.acquisition, name='acquisition'),
+    path('computations/<computation_id>/delete/', views.computation, name='computation'),
+    path('processes/<process_id>/delete/', views.process, name='process'),
+    path('data-collections/<data_collection_id>/delete/', views.data_collection, name='data_collection'),
 ]
