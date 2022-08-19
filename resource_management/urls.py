d81ea7cabbfed8bf4dcfa619dc565cc61261b811
David Chan <davidchan125@hotmail.co.uk>
2022-08-19 11:14:23 +0100

    Update res_man urls.py

--- resource_management/urls.py
+++ resource_management/urls.py
@@ -1,16 +1,18 @@
 from django.urls import include, path
 
+from . import views
+
 app_name = 'resource_management'
 urlpatterns = [
-    path('manage/', include('resource_management.manage_urls')),
-    path('organisations/', include('resource_management.organisation_urls')),
-    path('individuals/', include('resource_management.individual_urls')),
-    path('projects/', include('resource_management.project_urls')),
-    path('platforms/', include('resource_management.platform_urls')),
-    path('instruments/', include('resource_management.instrument_urls')),
-    path('operations/', include('resource_management.operation_urls')),
-    path('acquisitions/', include('resource_management.acquisition_urls')),
-    path('computations/', include('resource_management.computation_urls')),
-    path('processes/', include('resource_management.process_urls')),
-    path('data-collections/', include('resource_management.data_collection_urls')),
+    path('resources/', views.index, name='index'),
+    path('organisations/', views.organisations.as_view(), name='organisations'),
+    path('individuals/', views.individuals.as_view(), name='individuals'),
+    path('projects/', views.projects.as_view(), name='projects'),
+    path('platforms/', views.platforms.as_view(), name='platforms'),
+    path('instruments/', views.instruments.as_view(), name='instruments'),
+    path('operations/', views.operations.as_view(), name='operations'),
+    path('acquisitions/', views.acquisitions.as_view(), name='acquisitions'),
+    path('computations/', views.computations.as_view(), name='computations'),
+    path('processes/', views.processes.as_view(), name='processes'),
+    path('data-collections/', views.data_collections.as_view(), name='data_collections'),
 ]
