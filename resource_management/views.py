6020e2396be20f40ccab0cf3e6429cb71ab5909c
David Chan <davidchan125@hotmail.co.uk>
2022-08-30 11:13:10 +0100

    Update res_man index.html - Pass in number of resource types registered from view to template

--- resource_management/views.py
+++ resource_management/views.py
@@ -8,7 +8,27 @@ from search.helpers import remove_underscore_from_id_attribute
 
 # Create your views here.
 def index(request):
+    num_current_organsations = CurrentOrganisation.count_documents({})
+    num_current_individuals = CurrentIndividual.count_documents({})
+    num_current_projects = CurrentProject.count_documents({})
+    num_current_platforms = CurrentPlatform.count_documents({})
+    num_current_instruments = CurrentInstrument.count_documents({})
+    num_current_operations = CurrentOperation.count_documents({})
+    num_current_acquisitions = CurrentAcquisition.count_documents({})
+    num_current_computations = CurrentComputation.count_documents({})
+    num_current_processes = CurrentProcess.count_documents({})
+    num_current_data_collections = CurrentDataCollection.count_documents({})
     return render(request, 'resource_management/index.html', {
+        'num_current_organisations': num_current_organsations,
+        'num_current_individuals': num_current_individuals,
+        'num_current_projects': num_current_projects,
+        'num_current_platforms': num_current_platforms,
+        'num_current_instruments': num_current_instruments,
+        'num_current_operations': num_current_operations,
+        'num_current_acquisitions': num_current_acquisitions,
+        'num_current_computations': num_current_computations,
+        'num_current_processes': num_current_processes,
+        'num_current_data_collections': num_current_data_collections,
         'title': 'Manage Resources'
     })
 
