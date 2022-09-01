5424992c58f2d2580b5fa9aeb3687b2cfbb20f81
David Chan <davidchan125@hotmail.co.uk>
2022-09-01 19:35:50 +0100

    Update browse ontology_urls.py

--- browse/ontology_urls.py
+++ browse/ontology_urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = ''
 urlpatterns = [
     path('', views.ontology, name='ontology'),
-    path('<category>/<term_id>', views.ontology, name='ontology_detail'),
+    path('<category>/', views.ontology_category_terms_list, name='ontology_category_terms_list'),
+    path('<category>/<term_id>/', views.ontology_term_detail, name='ontology_term_detail'),
 ]
