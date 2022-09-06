df1f5e1934c98a3bbfe08c165b9d017c119cc72e
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-09-06 16:19:52 +0100

    Update search app files - Remove usage of ONTOLOGY_COMPONENT_ENUMS

--- search/helpers.py
+++ search/helpers.py
@@ -1,14 +1,5 @@
 import re
 
-ONTOLOGY_COMPONENT_ENUMS = {
-    'computationType': 'computationTypes',
-    'featureOfInterest': 'featuresOfInterest',
-    'instrumentType': 'instrumentTypes',
-    'measurand': 'measurands',
-    'observedProperty': 'observedProperties',
-    'phenomenon': 'phenomenons',
-}
-
 def convert_list_to_regex_list(list):
     return [re.compile(x) for x in list]
 
@@ -16,6 +7,7 @@ def map_ontology_components_to_local_ids(list):
     local_ids_list = []
     for x in list:
         local_ids_list.append(x['identifier']['PITHIA_Identifier']['localID'])
+    print(local_ids_list)
     return local_ids_list
 
 def remove_underscore_from_id_attribute(resource):
