5254a49d775458f88c76355a541e4a85e90e70ae
David Chan <davidchan125@hotmail.co.uk>
2022-09-12 21:32:11 +0100

    Update register forms.py - Change 'file' to 'files'

--- register/forms.py
+++ register/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField(label='File Upload', widget=forms.ClearableFileInput(attrs={
+    files = forms.FileField(label='File Upload', widget=forms.ClearableFileInput(attrs={
                 'accept': 'application/xml',
                 'class': 'form-control',
                 'multiple': True,
