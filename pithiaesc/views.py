439db01ff4331eca9c2c2bbae0a4b2469718a5f5
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-09-22 10:52:21 +0100

    Update pithiaesc views.py - Add login view

--- pithiaesc/views.py
+++ pithiaesc/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from common.forms import LoginForm
+
 def index(request):
     return render(request, 'index.html', {
         'title': 'PITHIA e-Science Centre Home',
+    })
+
+def login(request):
+    form = LoginForm()
+    return render(request, 'login.html', {
+        'title': 'Enter password',
+        'form': form
     })
