db4181c9f4d79e72ed795d54d2869a74c3365eda
David Chan <davidchan125@hotmail.co.uk>
2022-09-22 16:55:58 +0100

    Update register forms.py - Update UploadDataCollectionFileForm execution_method prop

--- register/forms.py
+++ register/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 
-EXECUTION_METHOD_CHOICES = [('none', 'None'),
-                            ('url', 'URL (Specified within the XML Metadata File)'),
-                            ('api', 'API')]
+EXECUTION_METHOD_CHOICES = [('api', 'API')]
 
 class UploadFileForm(forms.Form):
     files = forms.FileField(label='File Upload', widget=forms.ClearableFileInput(attrs={
@@ -16,7 +14,7 @@ class UploadDataCollectionFileForm(forms.Form):
                 'accept': 'application/xml',
                 'class': 'form-control',
             }))
-    execution_method = forms.ChoiceField(choices=EXECUTION_METHOD_CHOICES, widget=forms.RadioSelect(attrs={
+    execution_method = forms.MultipleChoiceField(choices=EXECUTION_METHOD_CHOICES, widget=forms.CheckboxSelectMultiple(attrs={
                             'class': 'form-check-input'
                         }))
     api_specification_url = forms.CharField(label='Link to API Specification', required=False, disabled=True, widget=forms.TextInput(attrs={
