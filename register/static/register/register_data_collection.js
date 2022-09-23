55af581321907ceb25efe85761d97bd8394aa3df
David Chan <davidchan125@hotmail.co.uk>
2022-09-23 08:46:36 +0100

    Update register_data_collection.js

--- register/static/register/register_data_collection.js
+++ register/static/register/register_data_collection.js
@@ -1,3 +1,21 @@
-document.querySelector('input[type="checkbox"][name="execution_method"][value="api"]').addEventListener("change", event => {
-    alert('changed');
+const apiExecutionMethodCheckbox = document.querySelector('input[type="checkbox"][name="execution_methods"][value="api"]');
+const apiSpecificationUrlTextInput = document.querySelector("#id_api_specification_url");
+
+function toggleApiSpecificationUrlTextInput(apiExecutionMethodCheckbox) {
+    if (apiExecutionMethodCheckbox.checked) {
+        apiSpecificationUrlTextInput.disabled = false;
+        apiSpecificationUrlTextInput.required = true;
+    } else {
+        apiSpecificationUrlTextInput.disabled = true;
+        apiSpecificationUrlTextInput.required = false;
+    }
+}
+
+apiExecutionMethodCheckbox.addEventListener("change", event => {
+    toggleApiSpecificationUrlTextInput(apiExecutionMethodCheckbox);
+});
+
+// register-dc-script
+window.addEventListener("load", async event => {
+    toggleApiSpecificationUrlTextInput(apiExecutionMethodCheckbox);
 });
