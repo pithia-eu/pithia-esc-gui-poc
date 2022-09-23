974a91a2225ccda4219e71654237aa69be916900
David Chan <davidchan125@hotmail.co.uk>
2022-09-23 10:50:35 +0100

    Update pithiaesc views.py - Finish implementing login view - Add logout view

--- pithiaesc/views.py
+++ pithiaesc/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from common.forms import LoginForm
 
@@ -8,8 +10,23 @@ def index(request):
     })
 
 def login(request):
+    if 'is_authorised' in request.session and request.session['is_authorised'] == True:
+        return HttpResponseRedirect(reverse('home'))
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['is_authorised'] = True
+            print("request.GET.get('next', '')", request.GET.get('next', ''))
+            if request.GET.get('next', '') != '':
+                return HttpResponseRedirect(request.GET.get('next', ''))
+            return HttpResponseRedirect(reverse('home'))
     form = LoginForm()
     return render(request, 'login.html', {
         'title': 'Enter password',
-        'form': form
+        'form': form,
+        'next': request.GET.get('next', '')
     })
+
+def logout(request):
+    del request.session['is_authorised']
+    return HttpResponseRedirect(reverse('home'))
