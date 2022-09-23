4487f1165e1ae714a08f2b4c91952ddfabe3956e
David Chan <davidchan125@hotmail.co.uk>
2022-09-23 10:50:53 +0100

    Update pithiaesc urls.py - Add logout url

--- pithiaesc/urls.py
+++ pithiaesc/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('', include('browse.urls')),
     path('', include('update.urls')),
     path('', include('delete.urls')),
