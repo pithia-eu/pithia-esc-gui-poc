682d3d7714e998b60d5ef7dcbcded7e42a64c76c
David Chan <davidchan125@hotmail.co.uk>
2022-09-28 20:26:27 +0100

    Update register forms.py - Change 'execution' to 'interaction'

--- register/forms.py
+++ register/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-EXECUTION_METHOD_CHOICES = [('api', 'API')]
+INTERACTION_METHOD_CHOICES = [('api', 'API')]
 
 class UploadFileForm(forms.Form):
     files = forms.FileField(label='File Upload', widget=forms.ClearableFileInput(attrs={
@@ -14,7 +14,7 @@ class UploadDataCollectionFileForm(forms.Form):
                 'accept': 'application/xml',
                 'class': 'form-control',
             }))
-    execution_methods = forms.MultipleChoiceField(choices=EXECUTION_METHOD_CHOICES, widget=forms.CheckboxSelectMultiple(attrs={
+    interaction_methods = forms.MultipleChoiceField(choices=INTERACTION_METHOD_CHOICES, widget=forms.CheckboxSelectMultiple(attrs={
                             'class': 'form-check-input'
                         }))
     api_specification_url = forms.CharField(label='Link to API Specification', required=False, disabled=True, widget=forms.TextInput(attrs={
