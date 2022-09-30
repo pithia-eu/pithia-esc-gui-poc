599cf63062d2eeaf0c299358b62692b3bbc461f8
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-09-30 12:34:43 +0100

    Update register_api_specification.py - Change 'api_specification_url' to 'interaction_url'

--- register/register_api_specification.py
+++ register/register_api_specification.py
@@ -3,6 +3,6 @@ from common.mongodb_models import CurrentDataCollectionInteractionMethod
 def register_api_specification(api_specification_url, data_collection_id):
     return CurrentDataCollectionInteractionMethod.insert_one({
         'interaction_method': 'api',
-        'api_specification_url': api_specification_url,
+        'interaction_url': api_specification_url,
         'data_collection_id': data_collection_id
     })
