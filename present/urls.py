21f655490de0cea60675e477e7e76cdda27426b0
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-10-04 10:47:20 +0100

    Update present urls.py - Add url for interact_with_data_collection_through_api view

--- present/urls.py
+++ present/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'present'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('api/<data_collection_id>/', views.interact_with_data_collection_through_api, name='interact_with_data_collection_through_api'),
 ]
