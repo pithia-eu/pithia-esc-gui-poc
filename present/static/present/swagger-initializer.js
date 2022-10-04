d14cb90c3f9daf80bc0d50ad291b3bda75dfee83
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-10-04 10:48:02 +0100

    Update swagger-initializer.js - Update to use passed-in api_specification_url as initial API url

--- present/static/present/swagger-initializer.js
+++ present/static/present/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://vm2.pithia.eu:8080/openapi.json",
+    url: JSON.parse(document.getElementById("api-specification-url").textContent),
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
