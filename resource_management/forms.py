5c7eb7e78b7e53bbc2a9d365be59bbfbd957f787
David Chan <davidchan125@hotmail.co.uk>
2022-10-05 17:23:53 +0100

    Update 'update' forms.py - Add UploadUpdatedDataCollectionFileForm

--- resource_management/forms.py
+++ resource_management/forms.py
@@ -1,7 +1,21 @@
 from django import forms
 
+INTERACTION_METHOD_CHOICES = [('api', 'API')]
+
 class UploadUpdatedFileForm(forms.Form):
     files = forms.FileField(label='File Upload', widget=forms.ClearableFileInput(attrs={
                 'accept': 'application/xml',
                 'class': 'form-control'
             }))
+
+class UploadUpdatedDataCollectionFileForm(forms.Form):
+    files = forms.FileField(label='File Upload', widget=forms.ClearableFileInput(attrs={
+                'accept': 'application/xml',
+                'class': 'form-control',
+            }))
+    interaction_methods = forms.MultipleChoiceField(choices=INTERACTION_METHOD_CHOICES, widget=forms.CheckboxSelectMultiple(attrs={
+                            'class': 'form-check-input'
+                        }))
+    api_specification_url = forms.CharField(label='Link to API Specification', required=False, disabled=True, widget=forms.TextInput(attrs={
+                            'class': 'form-control'
+                        }))
