15a2e6b43908c42e404d2934d2e13b2f9872f763
David Chan <davidchan125@hotmail.co.uk>
2022-10-05 22:19:37 +0100

    Update version_control.py

--- resource_management/version_control.py
+++ resource_management/version_control.py
@@ -1,2 +1,7 @@
-def restore_document(document_id, current_versions_mongodb_model, revisions_mongodb_model):
+from common.mongodb_models import CurrentDataCollectionInteractionMethod, DataCollectionInteractionMethodRevision
+
+def restore_previous_version_of_resource(resource_id, current_versions_mongodb_model, revisions_mongodb_model):
+    print('restore function')
+
+def restore_previous_version_of_interaction_method(data_collection_id, current_versions_mongodb_model, revisions_mongodb_model):
     print('restore function')
