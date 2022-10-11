2f84f2a9a9acce5072d6e29c585fe335014dde53
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-10-11 12:05:58 +0100

    Update pithiaesc views.py - Add index_admin view

--- pithiaesc/views.py
+++ pithiaesc/views.py
@@ -4,3 +4,8 @@ def index(request):
     return render(request, 'index.html', {
         'title': 'PITHIA e-Science Centre Home',
     })
+
+def index_admin(request):
+    return render(request, 'index_admin.html', {
+        'title': 'PITHIA e-Science Centre Home - Admin',
+    })
