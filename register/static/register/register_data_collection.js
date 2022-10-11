b6ca4a3892964b187b1e3b263707fab3296e9492
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-10-11 15:25:18 +0100

    Update register/update_data_collection.js - Remove code moved to other JS files

--- register/static/register/register_data_collection.js
+++ register/static/register/register_data_collection.js
@@ -1,31 +1,8 @@
 import {
-    isEachFileValid
-} from "/static/file_upload.js";
-import {
-    startOpenApiSpecificationUrlValidation
+    startOpenApiSpecificationUrlValidation,
+    apiExecutionMethodCheckbox,
+    toggleApiSpecificationUrlTextInput,
 } from "/static/api_specification_validation.js";
-
-const apiExecutionMethodCheckbox = document.querySelector('input[type="checkbox"][name="interaction_methods"][value="api"]');
-const apiSpecificationUrlTextInput = document.querySelector("#id_api_specification_url");
-
-function toggleApiSpecificationUrlTextInput(apiExecutionMethodCheckbox) {
-    if (apiExecutionMethodCheckbox.checked) {
-        apiSpecificationUrlTextInput.disabled = false;
-        apiSpecificationUrlTextInput.required = true;
-    } else {
-        apiSpecificationUrlTextInput.disabled = true;
-        apiSpecificationUrlTextInput.required = false;
-    }
-}
-
-apiExecutionMethodCheckbox.addEventListener("change", event => {
-    toggleApiSpecificationUrlTextInput(apiExecutionMethodCheckbox);
-    if (apiExecutionMethodCheckbox.checked) {
-        startOpenApiSpecificationUrlValidation();
-    } else {
-        document.querySelector("button[type='submit']").disabled = !isEachFileValid;
-    }
-});
 
 // register-dc-script
 window.addEventListener("load", async event => {
