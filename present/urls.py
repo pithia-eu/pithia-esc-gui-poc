086629fd51d0c052fdea75bfa6a23cc96b665088
David Chan <davidchan125@hotmail.co.uk>
2022-10-20 16:20:44 +0100

    Update present urls.py - Remove index url

--- present/urls.py
+++ present/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 app_name = 'present'
 urlpatterns = [
-    path('', views.index, name='index'),
     path('api/<data_collection_id>/', views.interact_with_data_collection_through_api, name='interact_with_data_collection_through_api'),
 ]
