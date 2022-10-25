93cb5d863c02ad5fc780764aecb18b1694481459
David Chan You Fee <davidchan125@hotmail.co.uk>
2022-10-25 11:10:01 +0100

    Update common app helpers - Add get_acquisition_capabilities_referencing_instrument_operational_ids function

--- common/helpers.py
+++ common/helpers.py
@@ -1,3 +1,6 @@
+from bson import ObjectId
+from .mongodb_models import CurrentAcquisitionCapability, CurrentInstrument
+
 def create_resource_url(resource_type, namespace, localid):
     if resource_type.lower() == 'computationcapabilities' and not localid.startswith('ComputationCapabilities_'):
         localid = f'ComputationCapabilities_{localid}'
@@ -6,3 +9,25 @@ def create_resource_url(resource_type, namespace, localid):
     if not resource_type.lower() == 'computationcapabilities' and not resource_type.lower() == 'acquisitioncapabilities' and not localid.startswith(f'{resource_type.capitalize()}_'):
         localid = f'{resource_type.capitalize()}_{localid}'
     return f'https://metadata.pithia.eu/resources/2.2/{resource_type}/{namespace}/{localid}'
+
+def get_acquisition_capabilities_referencing_instrument_operational_ids(instrument_id: ObjectId) -> list:
+    instrument = CurrentInstrument.find_one({
+        '_id': ObjectId(instrument_id)
+    }, {
+        'identifier': True,
+        'operationalMode': True
+    })
+    if 'operationalMode' not in instrument:
+        return []
+    instrument_url = create_resource_url('instrument', instrument['identifier']['PITHIA_Identifier']['namespace'], instrument['identifier']['PITHIA_Identifier']['localID'])
+    instrument_urls_with_operational_mode_ids = []
+    for om in instrument['operationalMode']:
+        iom = om['InstrumentOperationalMode']
+        instrument_urls_with_operational_mode_ids.append(f'{instrument_url}#{iom["id"]}')
+    current_acquisition_capabilities_referencing_instrument_operational_ids = list(CurrentAcquisitionCapability.find({
+        'instrumentModePair.InstrumentOperationalModePair.mode.@xlink:href': {
+            '$in': instrument_urls_with_operational_mode_ids
+        }
+    }))
+
+    return current_acquisition_capabilities_referencing_instrument_operational_ids
