daa501743899fc6584b765cfa0fd8ca69d4bf90e
David Chan <davidchan125@hotmail.co.uk>
2022-11-16 12:41:49 +0000

    Refactor url_validation.py - Update variable names in multiple functions - Update return values for multiple functions

--- validation/url_validation.py
+++ validation/url_validation.py
@@ -1,4 +1,5 @@
 import validators
+from operator import itemgetter
 from requests import get
 from rdflib import Graph, URIRef, RDF, SKOS
 from common.helpers import get_mongodb_model_by_resource_type_from_resource_url
@@ -38,79 +39,94 @@ def get_invalid_ontology_urls_from_parsed_xml(xml_file_parsed):
 def is_resource_url_structure_valid(resource_url):
     if not validators.url(resource_url):
         return False
-    resource_url_components = divide_resource_url_into_main_components(resource_url)
+    resource_type, namespace, localID = itemgetter('resource_type', 'namespace', 'localID')(divide_resource_url_into_main_components(resource_url))
 
-    is_start_of_localid_equal_to_resource_type = resource_url_components['localID'].startswith(resource_url_components['resource_type'].capitalize())
+    is_start_of_localid_equal_to_resource_type = localID.startswith(resource_type.capitalize())
     # Exceptions to localID starting with resource_type.capitalize():
-    if resource_url_components['resource_type'] == 'collection':
-        is_start_of_localid_equal_to_resource_type = resource_url_components['localID'].startswith('DataCollection')
-    elif resource_url_components['resource_type'] == 'acquisitionCapabilities':
-        is_start_of_localid_equal_to_resource_type = resource_url_components['localID'].startswith('AcquisitionCapabilities')
-    elif resource_url_components['resource_type'] == 'computationCapabilities':
-        is_start_of_localid_equal_to_resource_type = resource_url_components['localID'].startswith('ComputationCapabilities')
-    elif resource_url_components['resource_type'] == 'process':
-        is_start_of_localid_equal_to_resource_type = resource_url_components['localID'].startswith('CompositeProcess')
+    if resource_type == 'collection':
+        is_start_of_localid_equal_to_resource_type = localID.startswith('DataCollection')
+    elif resource_type == 'acquisitionCapabilities':
+        is_start_of_localid_equal_to_resource_type = localID.startswith('AcquisitionCapabilities')
+    elif resource_type == 'computationCapabilities':
+        is_start_of_localid_equal_to_resource_type = localID.startswith('ComputationCapabilities')
+    elif resource_type == 'process':
+        is_start_of_localid_equal_to_resource_type = localID.startswith('CompositeProcess')
 
     return all([
-        get_mongodb_model_by_resource_type_from_resource_url(resource_url_components['resource_type']) != 'unknown',
+        get_mongodb_model_by_resource_type_from_resource_url(resource_type) != 'unknown',
         resource_url.startswith(PITHIA_METADATA_SERVER_HTTPS_URL_BASE),
         resource_url.count(PITHIA_METADATA_SERVER_HTTPS_URL_BASE) == 1,
-        resource_url.count(f'/{resource_url_components["resource_type"]}/') == 1,
-        resource_url.count(f'/{resource_url_components["namespace"]}/') == 1,
-        resource_url.count(f'/{resource_url_components["localID"]}') == 1,
-        resource_url.endswith(resource_url_components["localID"]),
+        resource_url.count(f'/{resource_type}/') == 1,
+        resource_url.count(f'/{namespace}/') == 1,
+        resource_url.count(f'/{localID}') == 1,
+        resource_url.endswith(localID),
         is_start_of_localid_equal_to_resource_type,
     ])
 
 def validate_resource_url(resource_url):
-    if not is_resource_url_structure_valid(resource_url):
-        # The URL was formatted incorrectly.
-        return {
-            'is_valid': False,
-            'error_msg': 'The URL was formatted incorrectly.',
-        }
-    url_components = divide_resource_url_into_main_components(resource_url)
-    resource_mongodb_model = get_mongodb_model_by_resource_type_from_resource_url(url_components['resource_type'])
-
-    referenced_resource = get_resource_by_pithia_identifier_components(resource_mongodb_model, url_components['localID'], url_components['namespace'])
-    if referenced_resource == None:
-        return {
-            'is_valid': False,
-            'error_msg': 'The resource this URL refers to was not found.',
-            'resource_type': url_components['resource_type'],
-        }
-
-    return {
-        'is_valid': True
+    validation_details = {
+        'is_structure_valid': False,
+        'is_pointing_to_registered_resource': False,
+        'type_of_missing_resource': None,
     }
 
+    if not is_resource_url_structure_valid(resource_url):
+        # The URL was formatted incorrectly.
+        return validation_details
+    validation_details['is_structure_valid'] = True
+    resource_type, namespace, localID = itemgetter('resource_type', 'namespace', 'localID')(divide_resource_url_into_main_components(resource_url))
+    resource_mongodb_model = get_mongodb_model_by_resource_type_from_resource_url(resource_type)
+
+    referenced_resource = get_resource_by_pithia_identifier_components(resource_mongodb_model, localID, namespace)
+    if referenced_resource == None:
+        validation_details['type_of_missing_resource'] = resource_type
+        return validation_details
+    validation_details['is_pointing_to_registered_resource'] = True
+
+    return validation_details
+
 def get_invalid_resource_urls_from_parsed_xml(xml_file_parsed):
-    invalid_urls = []
+    invalid_urls = {
+        'urls_with_incorrect_structure': [],
+        'urls_pointing_to_unregistered_resources': [],
+        'types_of_missing_resources': [],
+    }
     root = xml_file_parsed.getroot()
     resource_urls = root.xpath(f"//*[contains(@xlink:href, '{PITHIA_METADATA_SERVER_URL_BASE}') and not(contains(@xlink:href, '#'))]/@*[local-name()='href' and namespace-uri()='http://www.w3.org/1999/xlink']", namespaces={'xlink': 'http://www.w3.org/1999/xlink'})
     for resource_url in resource_urls:
-        resource_url_validation_result = validate_resource_url(resource_url)
-        if resource_url_validation_result['is_valid'] == False:
-            invalid_urls.append((resource_url, resource_url_validation_result['error_msg'], resource_url_validation_result['resource_type']))
+        is_structure_valid, is_pointing_to_registered_resource, type_of_missing_resource = itemgetter('is_structure_valid', 'is_pointing_to_registered_resource', 'type_of_missing_resource')(validate_resource_url(resource_url))
+        if not is_structure_valid:
+            invalid_urls['urls_with_incorrect_structure'].append(resource_url)
+        elif not is_pointing_to_registered_resource:
+            invalid_urls['urls_pointing_to_unregistered_resources'].append(resource_url)
+            invalid_urls['types_of_missing_resources'].append(type_of_missing_resource)
+            invalid_urls['types_of_missing_resources'] = list(set(invalid_urls['types_of_missing_resources']))
+
     return invalid_urls
 
 def get_invalid_resource_urls_with_op_mode_ids_from_parsed_xml(xml_file_parsed):
-    invalid_urls = []
+    invalid_urls = {
+        'urls_with_incorrect_structure': [],
+        'urls_pointing_to_unregistered_resources': [],
+        'urls_with_unregistered_op_modes': [],
+        'types_of_missing_resources': [],
+    }
     root = xml_file_parsed.getroot()
-    resource_urls = root.xpath(f"//*[contains(@xlink:href, '{PITHIA_METADATA_SERVER_URL_BASE}') and contains(@xlink:href, '#')]/@*[local-name()='href' and namespace-uri()='http://www.w3.org/1999/xlink']", namespaces={'xlink': 'http://www.w3.org/1999/xlink'})
-    for resource_url in resource_urls:
-        resource_url_and_op_mode_id = divide_resource_url_from_op_mode_id
-        resource_url = resource_url_and_op_mode_id[:-1]
-        op_mode_id = resource_url_and_op_mode_id[-1]
-        resource_url_validation_result = validate_resource_url(resource_url)
-        if resource_url_validation_result['is_valid'] == False:
-            invalid_urls.append((resource_url, resource_url_validation_result['error_msg'], resource_url_validation_result['resource_type']))
+    resource_urls_with_op_mode_ids = root.xpath(f"//*[contains(@xlink:href, '{PITHIA_METADATA_SERVER_URL_BASE}') and contains(@xlink:href, '#')]/@*[local-name()='href' and namespace-uri()='http://www.w3.org/1999/xlink']", namespaces={'xlink': 'http://www.w3.org/1999/xlink'})
+    for resource_url_with_op_mode_id in resource_urls_with_op_mode_ids:
+        resource_url, op_mode_id = itemgetter('resource_url', 'op_mode_id')(divide_resource_url_from_op_mode_id(resource_url_with_op_mode_id))
+        is_structure_valid, is_pointing_to_registered_resource, type_of_missing_resource = itemgetter('is_structure_valid', 'is_pointing_to_registered_resource', 'type_of_missing_resource')(validate_resource_url(resource_url))
+        if not is_structure_valid:
+            invalid_urls['urls_with_incorrect_structure'].append(resource_url_with_op_mode_id)
+        elif not is_pointing_to_registered_resource:
+            invalid_urls['urls_pointing_to_unregistered_resources'].append(resource_url_with_op_mode_id)
+            invalid_urls['types_of_missing_resources'].append(type_of_missing_resource)
+            invalid_urls['types_of_missing_resources'] = list(set(invalid_urls['types_of_missing_resources']))
             
-        url_components = divide_resource_url_into_main_components(resource_url)
-        resource_mongodb_model = get_mongodb_model_by_resource_type_from_resource_url(url_components['resource_type'])
-        resource_with_op_mode_id = get_resource_by_pithia_identifier_components_and_op_mode_id(resource_mongodb_model, url_components['localID'], url_components['namespace'], op_mode_id)
+        resource_type, namespace, localID = itemgetter('resource_type', 'namespace', 'localID')(divide_resource_url_into_main_components(resource_url))
+        resource_mongodb_model = get_mongodb_model_by_resource_type_from_resource_url(resource_type)
+        resource_with_op_mode_id = get_resource_by_pithia_identifier_components_and_op_mode_id(resource_mongodb_model, localID, namespace, op_mode_id)
         if resource_with_op_mode_id == None:
-            hashtag_index = resource_url.index('#')
-            invalid_url = resource_url[:hashtag_index] + '<b>' + resource_url[hashtag_index:] + '</b>'
-            invalid_urls.append((invalid_url, 'The operational mode this URL is referencing was not found.'))
+            invalid_urls['urls_with_unregistered_op_modes'].append(resource_url_with_op_mode_id)
+            
     return invalid_urls
