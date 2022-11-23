8852368ab9a8e2c093a22e99d42d682d413a4f32
David Chan <davidchan125@hotmail.co.uk>
2022-11-23 15:39:15 +0000

    Update register and update app forms.py

--- resource_management/forms.py
+++ resource_management/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
-INTERACTION_METHOD_CHOICES = [('api', 'API')]
 
 class UploadUpdatedFileForm(forms.Form):
     files = forms.FileField(label='File Upload', widget=forms.ClearableFileInput(attrs={
@@ -13,9 +12,11 @@ class UploadUpdatedDataCollectionFileForm(forms.Form):
                 'accept': 'application/xml',
                 'class': 'form-control',
             }))
-    interaction_methods = forms.MultipleChoiceField(choices=INTERACTION_METHOD_CHOICES, required=False, widget=forms.CheckboxSelectMultiple(attrs={
-                            'class': 'form-check-input'
-                        }))
+
+class UpdateDataCollectionInteractionMethodsForm(forms.Form):
+    api_selected = forms.BooleanField(label='API', widget=forms.CheckboxInput(attrs={
+        'class': 'form-check-input'
+    }))
     api_specification_url = forms.CharField(label='Link to API Specification', required=False, widget=forms.TextInput(attrs={
                             'class': 'form-control'
                         }))
