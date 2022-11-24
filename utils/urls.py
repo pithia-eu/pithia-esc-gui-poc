656c028d23c07e8c5758671e9b5365724525037a
David Chan <davidchan125@hotmail.co.uk>
2022-11-24 15:42:03 +0000

    Add views and urls for viewing resources as XMLs

--- utils/urls.py
+++ utils/urls.py
@@ -5,4 +5,16 @@ from . import views
 app_name = 'utils'
 urlpatterns = [
     path('utils/convert/urls', views.get_esc_url_templates_for_ontology_server_urls_and_resource_server_urls, name='convert_server_urls'),
+    path('organisations/<organisation_id>/xml', views.view_organisation_as_xml.as_view(), name='view_organisation_as_xml'),
+    path('individuals/<individual_id>/xml', views.view_individual_as_xml.as_view(), name='view_individual_as_xml'),
+    path('projects/<project_id>/xml', views.view_project_as_xml.as_view(), name='view_project_as_xml'),
+    path('platforms/<platform_id>/xml', views.view_platform_as_xml.as_view(), name='view_platform_as_xml'),
+    path('operations/<operation_id>/xml', views.view_operation_as_xml.as_view(), name='view_operation_as_xml'),
+    path('instruments/<instrument_id>/xml', views.view_instrument_as_xml.as_view(), name='view_instrument_as_xml'),
+    path('acquisition_capabilitys/<acquisition_capability_id>/xml', views.view_acquisition_capability_as_xml.as_view(), name='view_acquisition_capability_as_xml'),
+    path('acquisitions/<acquisition_id>/xml', views.view_acquisition_as_xml.as_view(), name='view_acquisition_as_xml'),
+    path('computations/<computation_id>/xml', views.view_computation_as_xml.as_view(), name='view_computation_as_xml'),
+    path('computation_capabilitys/<computation_capability_id>/xml', views.view_computation_capability_as_xml.as_view(), name='view_computation_capability_as_xml'),
+    path('processs/<process_id>/xml', views.view_process_as_xml.as_view(), name='view_process_as_xml'),
+    path('data_collections/<data_collection_id>/xml', views.view_data_collection_as_xml.as_view(), name='view_data_collection_as_xml'),
 ]
--- utils/views.py
+++ utils/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
 from .url_helpers import convert_ontology_server_urls_to_browse_urls, convert_resource_server_urls_to_browse_urls
 from django.http import JsonResponse
+from django.views.generic import TemplateView
+from django.http import HttpResponseNotFound
+from bson import ObjectId
+from common.mongodb_models import OriginalMetadataXml
 
 # Create your views here.
 
@@ -18,3 +21,82 @@ def get_esc_url_templates_for_ontology_server_urls_and_resource_server_urls(requ
         'ontology_urls': esc_ontology_urls,
         'resource_urls': esc_resource_urls,
     })
+
+class ViewResourceAsXmlView(TemplateView):
+    resource_id = ''
+    template_name = 'utils/resource_as_xml.html'
+    xml = ''
+
+    def get(self, request, *args, **kwargs):
+        original_metadata_xml = OriginalMetadataXml.find_one({
+            'resourceId': ObjectId(self.resource_id)
+        })
+        if original_metadata_xml is None:
+            return HttpResponseNotFound('The XML for this resource was not found.')
+        self.xml = original_metadata_xml['value']
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['xml'] = self.xml
+        return context
+
+class view_organisation_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['organisation_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_individual_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['individual_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_project_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['project_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_platform_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['platform_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_operation_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['operation_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_instrument_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['instrument_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_acquisition_capability_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['acquisition_capability_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_acquisition_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['acquisition_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_computation_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['computation_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_computation_capability_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['computation_capability_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_process_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['process_id']
+        return super().dispatch(request, *args, **kwargs)
+
+class view_data_collection_as_xml(ViewResourceAsXmlView):
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['data_collection_id']
+        return super().dispatch(request, *args, **kwargs)
