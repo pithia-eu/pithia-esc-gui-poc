496e16669bf9530a91caf3cddb999bb4273603ca
David Chan <davidchan125@hotmail.co.uk>
2022-11-24 15:35:52 +0000

    Update "convert urls" url

--- utils/urls.py
+++ utils/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'utils'
 urlpatterns = [
-    path('convert/urls', views.get_esc_url_templates_for_ontology_server_urls_and_resource_server_urls, name='convert_server_urls'),
+    path('utils/convert/urls', views.get_esc_url_templates_for_ontology_server_urls_and_resource_server_urls, name='convert_server_urls'),
 ]
