94fe221fa01e567bbc96d9ce4aba399973a30aa5
David Chan <davidchan125@hotmail.co.uk>
2022-11-24 16:35:41 +0000

    Update view resource as xml page layout + js - Add xml_download.js - Pass in resource name and localID - Update page styling

---
+++ utils/static/utils/xml_download.js
@@ -0,0 +1,13 @@
+const xmlDownloadButton = document.querySelector(".xml-file-download-btn");
+
+function createXmlFile() {
+
+}
+
+function startXmlFileDownload() {
+
+}
+
+xmlDownloadButton.addEventListener("click", event => {
+    xmlFile = createXmlFile();
+});
--- utils/views.py
+++ utils/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.views.generic import TemplateView
 from django.http import HttpResponseNotFound
 from bson import ObjectId
-from common.mongodb_models import OriginalMetadataXml
+from common.mongodb_models import OriginalMetadataXml, CurrentOrganisation, CurrentIndividual, CurrentProject, CurrentPlatform, CurrentOperation, CurrentInstrument, CurrentAcquisitionCapability, CurrentAcquisition, CurrentComputationCapability, CurrentComputation, CurrentProcess, CurrentDataCollection
 
 # Create your views here.
 
@@ -24,6 +24,9 @@ def get_esc_url_templates_for_ontology_server_urls_and_resource_server_urls(requ
 
 class ViewResourceAsXmlView(TemplateView):
     resource_id = ''
+    resource_mongodb_model = None
+    resource_name = ''
+    resource_localid = ''
     template_name = 'utils/resource_as_xml.html'
     xml = ''
 
@@ -34,69 +37,103 @@ class ViewResourceAsXmlView(TemplateView):
         if original_metadata_xml is None:
             return HttpResponseNotFound('The XML for this resource was not found.')
         self.xml = original_metadata_xml['value']
+        resource = self.resource_mongodb_model.find_one({
+            '_id': ObjectId(self.resource_id)
+        }, projection={
+            'identifier': 1,
+            'name': 1,
+        })
+        self.resource_localid = resource['identifier']['PITHIA_Identifier']['localID']
+        self.resource_name = resource['name']
         return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['resource_name'] = self.resource_name
+        context['resource_localid'] = self.resource_localid
         context['xml'] = self.xml
         return context
 
 class view_organisation_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentOrganisation
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['organisation_id']
         return super().dispatch(request, *args, **kwargs)
 
 class view_individual_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentIndividual
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['individual_id']
         return super().dispatch(request, *args, **kwargs)
 
 class view_project_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentProject
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['project_id']
         return super().dispatch(request, *args, **kwargs)
 
 class view_platform_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentPlatform
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['platform_id']
         return super().dispatch(request, *args, **kwargs)
 
 class view_operation_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentOperation
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['operation_id']
         return super().dispatch(request, *args, **kwargs)
 
 class view_instrument_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentInstrument
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['instrument_id']
         return super().dispatch(request, *args, **kwargs)
 
 class view_acquisition_capability_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentAcquisitionCapability
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['acquisition_capability_id']
         return super().dispatch(request, *args, **kwargs)
 
 class view_acquisition_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentAcquisition
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['acquisition_id']
         return super().dispatch(request, *args, **kwargs)
 
-class view_computation_as_xml(ViewResourceAsXmlView):
-    def dispatch(self, request, *args, **kwargs):
-        self.resource_id = self.kwargs['computation_id']
-        return super().dispatch(request, *args, **kwargs)
-
 class view_computation_capability_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentComputationCapability
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['computation_capability_id']
         return super().dispatch(request, *args, **kwargs)
 
+class view_computation_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentComputation
+
+    def dispatch(self, request, *args, **kwargs):
+        self.resource_id = self.kwargs['computation_id']
+        return super().dispatch(request, *args, **kwargs)
+
 class view_process_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentProcess
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['process_id']
         return super().dispatch(request, *args, **kwargs)
 
 class view_data_collection_as_xml(ViewResourceAsXmlView):
+    resource_mongodb_model = CurrentDataCollection
+
     def dispatch(self, request, *args, **kwargs):
         self.resource_id = self.kwargs['data_collection_id']
         return super().dispatch(request, *args, **kwargs)
