356525e1f9d24b6fa98636556c03741f87aa27a6
David Chan <davidchan125@hotmail.co.uk>
2022-12-02 17:02:07 +0000

    Add test for fetching OPs by instrument types

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from search.search_helpers import get_observed_property_urls_by_instrument_types
 
 # Create your tests here.
+
+class ObservedPropertySearchFormUpdateTestCase(SimpleTestCase):
+    def test_observed_properties_are_fetched_by_instrument_type(self):
+        """
+        get_observed_property_urls_by_instrument_types() returns a list
+        of observed property urls when passed in a list of instrument
+        types
+        """
+        instrument_types = [
+            'GNSS-receiver',
+            'GNSS-receiverScintillation',
+            'InSituRelaxationSounder',
+        ]
+        observed_property_urls = get_observed_property_urls_by_instrument_types(instrument_types)
+        print(observed_property_urls)
+        self.assertTrue(isinstance(observed_property_urls, list))
