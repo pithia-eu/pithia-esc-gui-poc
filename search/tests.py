678ce3cd2478686cea61628a9f2dcb516ec6f6de
David Chan <davidchan125@hotmail.co.uk>
2022-12-07 11:52:38 +0000

    Add tests for detecting registered OPs and FOIs

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from search.views import get_registered_observed_properties, get_registered_features_of_interest
 
 # Create your tests here.
+
+class RegisteredResourcesTestCase(SimpleTestCase):
+    def test_get_registered_observed_properties(self):
+        """
+        Test get_registered_observed_properties()
+        returns an accurate list of registered
+        observed properties
+        """
+        registered_observed_properties = get_registered_observed_properties()
+        # print('registered_observed_properties', registered_observed_properties)
+        self.assertTrue(isinstance(registered_observed_properties, list))
+
+    def test_get_registered_features_of_interest(self):
+        """
+        Test get_registered_features_of_interest()
+        returns an accurate list of registered
+        features of interest
+        """
+        registered_observed_property_ids = get_registered_observed_properties()
+        registered_features_of_interest = get_registered_features_of_interest(registered_observed_property_ids)
+        print('registered_features_of_interest', registered_features_of_interest)
+        self.assertTrue(isinstance(registered_features_of_interest, list))
