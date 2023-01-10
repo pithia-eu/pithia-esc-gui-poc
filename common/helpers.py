ec702018ba5eeb3fa4cd742c3c60581e5686a9ce
David Chan <davidchan125@hotmail.co.uk>
2023-01-10 16:32:53 +0000

    Fix create_resource_url function

--- common/helpers.py
+++ common/helpers.py
@@ -48,7 +48,13 @@ def create_resource_url(resource_type, namespace, localid):
         localid = f'AcquisitionCapabilities_{localid}'
     if resource_type.lower() == 'process' and not localid.startswith('CompositeProcess_'):
         localid = f'CompositeProcess_{localid}'
-    if not resource_type.lower() == 'computationcapabilities' and not resource_type.lower() == 'acquisitioncapabilities' and not  resource_type.lower() == 'process' and not localid.startswith(f'{resource_type.capitalize()}_'):
+    if resource_type.lower() == 'collection' and not localid.startswith('DataCollection_'):
+        localid = f'DataCollection_{localid}'
+    if (not resource_type.lower() == 'computationcapabilities' and
+        not resource_type.lower() == 'acquisitioncapabilities' and
+        not resource_type.lower() == 'process' and
+        not resource_type.lower() == 'collection' and
+        not localid.startswith(f'{resource_type.capitalize()}_')):
         localid = f'{resource_type.capitalize()}_{localid}'
     return f'https://metadata.pithia.eu/resources/2.2/{resource_type}/{namespace}/{localid}'
 
