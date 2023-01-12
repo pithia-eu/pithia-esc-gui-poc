fd63863d1bfad86bd3ed6c26efa6d594f5d97274
David Chan <davidchan125@hotmail.co.uk>
2023-01-12 16:07:15 +0000

    Add success message for resource deletion

--- delete/views.py
+++ delete/views.py
@@ -75,6 +75,9 @@ class ResourceDeleteView(TemplateView):
         return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+        self.resource_to_delete = self.resource_mongodb_model.find_one({
+            '_id': ObjectId(self.resource_id)
+        })
         try:
             with client.start_session() as s:
                 def cb(s):
@@ -87,6 +90,7 @@ class ResourceDeleteView(TemplateView):
                     if self.resource_mongodb_model == CurrentDataCollection:
                         delete_current_versions_and_revisions_of_data_collection_interaction_methods(kwargs['data_collection_id'], session=s)
                 s.with_transaction(cb)
+            messages.success(request, f'Successfully deleted {self.resource_to_delete["name"]}.')
         except BaseException as e:
             print(e)
             messages.error(request, 'An error occurred during resource deletion.')
