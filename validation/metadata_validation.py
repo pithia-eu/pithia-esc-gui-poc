c7f60ca11c8a8b1368fb5be84457a0e0a8362bda
David Chan <davidchan125@hotmail.co.uk>
2023-01-16 18:58:39 +0000

    Refactor metadata_validation.py

--- validation/metadata_validation.py
+++ validation/metadata_validation.py
@@ -18,7 +18,7 @@ from pathlib import Path
 from bson import ObjectId
 from register.xml_metadata_file_conversion import convert_xml_metadata_file_to_dictionary
 from .helpers import (
-    _create_validation_error_details_dict,
+    create_validation_details_error,
     _map_string_to_li_element,
     _map_string_to_li_element_with_register_link,
     _map_etree_element_to_text,
@@ -94,7 +94,7 @@ def validate_xml_against_schema_at_url(xml_file, schema_url):
     xml_schema.validate(xml_file.read())
 
 # Matching file name and localID tag text validation
-def is_xml_file_name_matching_with_localid(xml_file):
+def validate_xml_file_name(xml_file):
     """
     Returns whether the name for an XML file matches
     with the innerText of its <localID> element.
@@ -102,7 +102,11 @@ def is_xml_file_name_matching_with_localid(xml_file):
     xml_file_parsed = parse_xml_file(xml_file)
     localid_tag_text = xml_file_parsed.find('.//{https://metadata.pithia.eu/schemas/2.2}localID').text # There should be only one <localID> tag in the tree
     xml_file_name_with_no_extension = Path(xml_file.name).stem
-    return localid_tag_text == xml_file_name_with_no_extension
+    if localid_tag_text != xml_file_name_with_no_extension:
+        raise FileNameNotMatchingWithLocalID(
+            'File name not matching with localID',
+            f'The file name \"{xml_file_name_with_no_extension}\" must match the localID of the metadata \"{localid_tag_text}\".'
+        )
 
 # Registration validation
 def validate_xml_file_is_unregistered(mongodb_model, xml_file=None, converted_xml_file=None):
@@ -177,13 +181,9 @@ def validate_and_get_validation_details_of_xml_file(
     check_xml_file_localid_matches_existing_resource_localid=False,
     existing_resource_id=''
 ):
-    validation_checklist = {
-        'is_root_element_name_valid': False,
-        'is_syntax_valid': False,
-        'is_valid_against_schema': False,
-        'is_file_name_matching_with_localid': False,
-        'is_each_document_reference_valid': False,
-        'is_each_ontology_reference_valid': False,
+    validation_details = {
+        'error': None,
+        'warnings': [],
     }
 
     try:
@@ -207,15 +207,20 @@ def validate_and_get_validation_details_of_xml_file(
             mongodb_model is not None and
             existing_resource_id != ''):
             if is_updated_xml_file_localid_matching_with_current_resource_localid(xml_file, existing_resource_id, mongodb_model) == False:
-                validation_checklist['error'] = _create_validation_error_details_dict(type(BaseException()), 'The localID and namespace must be matching with the resource being updated.', None)
-                return validation_checklist
+                validation_details['error'] = create_validation_details_error(
+                    message='Invalid localID and namespace',
+                    details='The localID and namespace must be matching with the metadata being updated.',
+                )
+                return validation_details
 
         # Operational mode IDs are changed and pre-existing IDs are referenced by any Acquisition Capabilities validation
         if (check_xml_file_localid_matches_existing_resource_localid == True and
             existing_resource_id != '' and
             mongodb_model is not None and
             mongodb_model == CurrentInstrument):
-            operational_mode_ids_of_updated_xml = list(map(_map_etree_element_to_text, xml_file_parsed.findall('.//{https://metadata.pithia.eu/schemas/2.2}id')))
+            operational_mode_ids_of_updated_xml = list(
+                map(_map_etree_element_to_text, xml_file_parsed.findall('.//{https://metadata.pithia.eu/schemas/2.2}id'))
+            )
             instrument_to_update = CurrentInstrument.find_one({
                                         '_id': ObjectId(existing_resource_id)
                                     }, {
@@ -225,20 +230,15 @@ def validate_and_get_validation_details_of_xml_file(
             operational_mode_ids_intersection = set(operational_mode_ids_of_updated_xml).intersection(set(operational_mode_ids_of_current_xml))
             if len(operational_mode_ids_intersection) < len(operational_mode_ids_of_current_xml):
                 acquisition_capability_sets = get_acquisition_capability_sets_referencing_instrument_operational_ids(existing_resource_id)
-                validation_checklist['error'] = _create_validation_error_details_dict(type(BaseException()), 'Please remove references to this instrument\'s operational mode IDs from the acquisition capabilities listed below, before updating this instrument: <ul>%s</ul>' % ''.join(list(map(_map_acquisition_capability_to_update_link, acquisition_capability_sets))), None)
-                return validation_checklist
+                validation_details['warnings'].append(create_validation_details_error(
+                    message='Any references to this instrument\'s operational mode IDs must will be invalidated after this update.',
+                    details='After updating this instrument, please update any references to this instrument\'s operational mode IDs in the acquisition capabilities listed below: <ul>%s</ul>' % ''.join(list(map(_map_acquisition_capability_to_update_link, acquisition_capability_sets)))
+                ))
+                return validation_details
 
         # Matching file name and localID tag text validation
-        localid_tag_text = xml_file_parsed.find('.//{https://metadata.pithia.eu/schemas/2.2}localID').text # There should be only one <localID> tag in the tree
-        xml_file_name_with_no_extension = Path(xml_file.name).stem
-        if localid_tag_text != xml_file_name_with_no_extension:
-            validation_checklist['error'] = _create_validation_error_details_dict(FileNameNotMatchingWithLocalID, f"The file name \"{xml_file_name_with_no_extension}\" must match the localID of the metadata \"{localid_tag_text}\".", None)
-            return validation_checklist
-        validation_checklist['is_file_name_matching_with_localid'] = True
+        validate_xml_file_name(xml_file)
 
-        # Relation validation (whether a resource the metadata file
-        # is referencing exists in the database or not).
-        
         # Resource URL validation
         invalid_resource_urls = get_invalid_resource_urls_from_parsed_xml(xml_file_parsed)
         invalid_resource_urls_with_op_mode_ids = get_invalid_resource_urls_with_op_mode_ids_from_parsed_xml(xml_file_parsed)
@@ -250,40 +250,61 @@ def validate_and_get_validation_details_of_xml_file(
         if len(resource_urls_with_incorrect_structure) > 0:
             error_msg = 'Invalid document URLs: <ul>%s</ul><div class="mt-2">Your resource URL may reference an unsupported resource type, or may not follow the correct structure.</div>' % ''.join(list(map(_map_string_to_li_element, resource_urls_with_incorrect_structure)))
             error_msg = error_msg + '<div class="mt-2">Expected resource URL structure: <i>https://metadata.pithia.eu/resources/2.2/<b>resource type</b>/<b>namespace</b>/<b>localID</b></i></div>'
-            validation_checklist['error'] = _create_validation_error_details_dict(type(InvalidResourceURL()), error_msg, None)
-            return validation_checklist
+            validation_details['error'] = create_validation_details_error(
+                message='One or multiple resource URLs specified via the xlink:href attribute are invalid.',
+                details=error_msg
+            )
+            return validation_details
 
         if len(resource_urls_pointing_to_unregistered_resources) > 0:
             error_msg = 'Unregistered document URLs: <ul>%s</ul><b>Note:</b> If your URLs start with "<i>http://</i>" please change this to "<i>https://</i>".' % ''.join(list(map(_map_string_to_li_element, resource_urls_pointing_to_unregistered_resources)))
             error_msg = error_msg + '<div class="mt-2">Please use the following links to register the resources referenced in the submitted metadata file:</div>'
             error_msg = error_msg + '<ul class="mt-2">%s</ul>' % ''.join(list(map(_map_string_to_li_element_with_register_link, types_of_missing_resources)))
-            validation_checklist['error'] = _create_validation_error_details_dict(type(UnregisteredResourceReferenced()), error_msg, None)
-            return validation_checklist
+            validation_details['error'] = create_validation_details_error(
+                message='One or multiple resources referenced by the xlink:href attribute have not been registered with the e-Science Centre.',
+                details=error_msg
+            )
+            return validation_details
 
         if len(resource_urls_pointing_to_registered_resources_with_missing_op_modes) > 0:
-            error_msg = 'The operational mode IDs in these document URLs are invalid: <ul>%s</ul>' % ''.join(list(map(_map_string_to_li_element, resource_urls_pointing_to_registered_resources_with_missing_op_modes)))
-            validation_checklist['error'] = _create_validation_error_details_dict(type(BaseException()), error_msg, None)
-            return validation_checklist
-        validation_checklist['is_each_document_reference_valid'] = True
+            error_msg = 'Invalid operational mode references: <ul>%s</ul>' % ''.join(list(map(_map_string_to_li_element, resource_urls_pointing_to_registered_resources_with_missing_op_modes)))
+            validation_details['error'] = create_validation_details_error(
+                message='One or multiple referenced operational modes are invalid.',
+                details=error_msg
+            )
+            return validation_details
 
         # Ontology URL validation
         invalid_ontology_urls = get_invalid_ontology_urls_from_parsed_xml(xml_file_parsed)
         if len(invalid_ontology_urls) > 0:
-            validation_checklist['error'] = _create_validation_error_details_dict(type(UnregisteredOntologyTermReferenced()), 'Invalid ontology term URLs: <ul>%s</ul><div class="mt-2">These ontology URLs may reference terms which have not yet been added to the PITHIA ontology, or no longer exist in the PITHIA ontology. Please also ensure URLs start with "<i>https://</i>" and not "<i>http://</i>".</div>' % ''.join(list(map(_map_string_to_li_element, invalid_ontology_urls))), None)
-            return validation_checklist
-        validation_checklist['is_each_ontology_reference_valid'] = True
+            error_msg = 'Invalid ontology term URLs: <ul>%s</ul><div class="mt-2">These ontology URLs may reference terms which have not yet been added to the PITHIA ontology, or no longer exist in the PITHIA ontology. Please also ensure URLs start with "<i>https://</i>" and not "<i>http://</i>".</div>' % ''.join(list(map(_map_string_to_li_element, invalid_ontology_urls)))
+            validation_details['error'] = create_validation_details_error(
+                message='One or multiple ontology terms referenced by the xlink:href attribute are not valid PITHIA ontology terms.',
+                details=error_msg
+            )
 
     except etree.XMLSyntaxError as err:
         print(traceback.format_exc())
-        validation_checklist['error'] = _create_validation_error_details_dict(type(err), str(err), None)
+        validation_details['error'] = create_validation_details_error(
+            message='Syntax is invalid.',
+            details=str(err),
+        )
     except etree.DocumentInvalid as err:
         print(traceback.format_exc())
-        validation_checklist['error'] = _create_validation_error_details_dict(type(err), str(err), None)
-    except (InvalidRootElementName, FileRegisteredBefore) as err:
-        validation_checklist['error'] = _create_validation_error_details_dict(type(err), str(err), None)
+        validation_details['error'] = create_validation_details_error(
+            message='XML does not conform to the corresponding schema.',
+            details=str(err),
+        )
+    except (InvalidRootElementName, FileRegisteredBefore, FileNameNotMatchingWithLocalID) as err:
+        validation_details['error'] = create_validation_details_error(
+            err.message,
+            err.details
+        )
     except BaseException as err:
         print(traceback.format_exc())
-        validation_checklist['error'] = _create_validation_error_details_dict(type(err), str(err), None)
+        validation_details['error'] = create_validation_details_error(
+            details=str(err)
+        )
     
 
-    return validation_checklist
+    return validation_details
