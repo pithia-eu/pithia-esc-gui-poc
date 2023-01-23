6ab01ae3dd79c89313e59467b02771505ad11140
David Chan <davidchan125@hotmail.co.uk>
2023-01-23 12:41:39 +0000

    Add tests for doi registration functions

--- register/tests.py
+++ register/tests.py
@@ -4,8 +4,13 @@ import mongomock
 from bson.objectid import ObjectId
 from django.test import SimpleTestCase, tag
 from register.register import register_metadata_xml_file
+from register.doi_registration import (
+    generate_doi,
+    add_doi_to_xml_file,
+)
 from validation.errors import FileRegisteredBefore
 from pithiaesc.settings import BASE_DIR
+from pathlib import Path
 
 _XML_METADATA_FILE_DIR = os.path.join(BASE_DIR, 'common', 'test_files', 'xml_metadata_files')
 
@@ -19,8 +24,7 @@ class RegisterMetadataTestCase(SimpleTestCase):
     @tag('fast')
     def test_register_organisation(self):
         """
-        Ensure the organisation registration process doesn't encounter any
-        unexpected errors.
+        register_metadata_xml_file() returns a dict with an auto-generated '_id' property
         """
         try:
             client = mongomock.MongoClient()
@@ -32,6 +36,31 @@ class RegisterMetadataTestCase(SimpleTestCase):
                     None
                 )
                 self.assertIs(ObjectId.is_valid(registered_resource['_id']), True)
+                print(f'Passed registration validation for {Path(xml_file.name).name}.')
         except FileRegisteredBefore as err:
             print(err)
             self.fail('register_metadata_xml_file() unexpectedly raised an error!')
+
+class DoiFunctionalityTestCase(SimpleTestCase):
+    fake_resource_id = '85d32cad243eb3953dceca32'
+
+    @tag('fast')
+    def test_generate_fake_doi(self):
+        """
+        generate_doi() returns a dict representing a DOI object.
+        """
+        doi = generate_doi(self.fake_resource_id)
+        self.assertIsInstance(doi, dict)
+        print('doi', doi)
+        print('Passed DOI generation test!')
+
+    @tag('fast')
+    def test_add_doi_tag_to_xml_file(self):
+        """
+        add_doi_to_xml_file() adds a filled out <doi> element to the XML file.
+        """
+        doi = generate_doi(self.fake_resource_id)
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'DataSubset_Test-2023-01-01_DataCollectionTest.xml')) as xml_file:
+            updated_xml_file = add_doi_to_xml_file(xml_file, doi)
+            print('updated_xml_file', updated_xml_file)
+            print(f'Passed new DOI element addition for {Path(xml_file.name).name}.')
