d37eef59e6d2561039c509c9366752e392fbcb4b
David Chan <davidchan125@hotmail.co.uk>
2023-02-22 17:38:39 +0000

    Add logging to update version_control.py - Replace print statements with logging statements

--- update/version_control.py
+++ update/version_control.py
@@ -1,5 +1,13 @@
 from bson import ObjectId
-from common.mongodb_models import OriginalMetadataXml, CurrentDataCollectionInteractionMethod, DataCollectionInteractionMethodRevision
+from common.mongodb_models import (
+    OriginalMetadataXml,
+    CurrentDataCollectionInteractionMethod,
+    DataCollectionInteractionMethodRevision
+)
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def create_revision_of_current_resource_version(
@@ -13,7 +21,7 @@ def create_revision_of_current_resource_version(
         'identifier.PITHIA_Identifier.namespace': resource_pithia_identifier['namespace'],
     })
     if not current_version_of_resource:
-        print('Resource not found.')
+        logger.warning(f"Could not create a revision of the resource with a localID {resource_pithia_identifier['localID']} and namespace {resource_pithia_identifier['namespace']}, as that resource could not be found.")
         return 'Resource not found.'
     current_version_of_resource.pop('_id', None)
     resource_revision_mongodb_model.insert_one(current_version_of_resource, session=session)
@@ -24,7 +32,7 @@ def assign_original_xml_file_entry_to_revision_id(old_resource_id, revision_id, 
         'resourceId': ObjectId(str(old_resource_id))
     })
     if current_orginal_xml_file is None:
-        print('The original XML metadata string for this resource was not found.')
+        logger.warning(f'Could not assign the original XML metadata string for resource with ID {old_resource_id} to the resource\'s revision, as an original XML metadata string for that resource was not found.')
         return 'The original XML metadata string for this resource was not found.'
     return OriginalMetadataXml.update_one({
         'resourceId': ObjectId(str(old_resource_id))
@@ -38,7 +46,7 @@ def create_revision_of_data_collection_api_interaction_method(data_collection_lo
         'interaction_method': 'api',
     })
     if current_version_of_api_interaction_method == None:
-        print('No API interaction method for this data collection was found.')
+        logger.warning(f'Could not make a revision of the API interaction method for the Data Collection with localID {data_collection_localid}, as no Data Collection with that localID was found.')
         return 'No API interaction method for this data collection was found.'
     current_version_of_api_interaction_method.pop('_id', None)
     DataCollectionInteractionMethodRevision.insert_one(current_version_of_api_interaction_method, session=session)
