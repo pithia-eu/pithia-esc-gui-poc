ff0a99515e733bd2377c48571d9a78d6ff1b7620
David Chan <davidchan125@hotmail.co.uk>
2023-02-24 18:06:36 +0000

    Add add_doi_to_xml_file() to handle_management.py

--- register/handle_management.py
+++ register/handle_management.py
@@ -1,5 +1,6 @@
 import os
 import json
+import etree
 from pyhandle.clientcredentials import PIDClientCredentials
 from pyhandle.handleclient import PyHandleClient, RESTHandleClient
 from pyhandle.handleexceptions import *
@@ -51,7 +52,7 @@ def get_handle_url(handle: str, client: RESTHandleClient) -> str:
     return read_value
 
 def get_handle_record(handle: str, client: RESTHandleClient) -> dict:
-    handle_record= client.retrieve_handle_record(handle)
+    handle_record = client.retrieve_handle_record(handle)
 
     if handle_record != None:
         logger.info('OK: Handle exists.')
@@ -71,3 +72,49 @@ def update_handle_url(handle: str, new_handle_value: str, client: RESTHandleClie
         logger.critical('CRITICAL: Modify handle URL returned unexpected value.')
         logger.info(f'Expected: {new_handle_value}')
         logger.info(f'Returned: {get_value_result}')
+
+def add_doi_to_xml_file(xml_file, doi):
+    # Use lxml to append a new filled in doi element
+    # The passed in xml_file should be open in 'wb' mode,
+    # so it can be written to.
+    parser = etree.XMLParser(remove_blank_text=True)
+    xml_file_parsed = etree.parse(xml_file, parser)
+    root = xml_file_parsed.getroot()
+    doi_element_content = '''
+    <doi xmlns:doi="http://www.doi.org/2010/DOISchema">
+        <doi:kernelMetadata>
+            <doi:registrationAgencyDoiName>%s</doi:registrationAgencyDoiName>
+            <doi:issueDate>%s</doi:issueDate>
+            <doi:issueNumber>%s</doi:issueNumber>
+            <doi:referentCreation>
+                <doi:name primaryLanguage="%s">
+                    <value>%s</value>
+                    <type>%s</type>
+                </doi:name>
+                <doi:identifier>
+                    <doi:nonUriValue>%s</doi:nonUriValue>
+                    <doi:uri returnType="%s">%s</doi:uri>
+                    <doi:type>%s</doi:type>
+                </doi:identifier>
+            </doi:referentCreation>
+        </doi:kernelMetadata>
+    </doi>
+    ''' % (
+        doi['registrationAgencyDoiName'],
+        doi['issueDate'],
+        doi['issueNumber'],
+        doi['name']['@primaryLanguage'],
+        doi['name']['value'],
+        doi['name']['type'],
+        doi['identifier']['nonUriValue'],
+        doi['identifier']['uri']['@returnType'],
+        doi['identifier']['uri']['#text'],
+        doi['identifier']['type']
+    )
+    doi_element_content = (' '.join(doi_element_content.split())).replace('> <', '><')
+    doi_element = etree.fromstring(doi_element_content)
+    root.append(doi_element)
+    etree.indent(root, space='    ')
+    with open(xml_file.name, 'wb') as xml_file:
+        xml_file_parsed.write(xml_file.name, pretty_print=True)
+    return xml_file
