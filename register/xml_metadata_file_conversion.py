462d9bae79f2a7f0a95341f0bccc25e43efab41f
David Chan <davidchan125@hotmail.co.uk>
2023-03-02 19:23:36 +0000

    Refactor XML conversion function parameters

--- register/xml_metadata_file_conversion.py
+++ register/xml_metadata_file_conversion.py
@@ -1,12 +1,13 @@
 import io
 import json
+from typing import Union
 import xmltodict
 
-def convert_xml_metadata_file_to_dictionary(xml_file):
-    xml = xml_file
-    if isinstance(xml_file, io.TextIOWrapper):
-        xml_file.seek(0)
-        xml = xml_file.read()
+def convert_xml_metadata_file_to_dictionary(xml_file_or_string: Union[io.TextIOWrapper, str]) -> dict:
+    xml = xml_file_or_string
+    if isinstance(xml_file_or_string, io.TextIOWrapper):
+        xml_file_or_string.seek(0)
+        xml = xml_file_or_string.read()
     xml_as_dict = xmltodict.parse(xml)
     xml_as_json = json.dumps(xml_as_dict)
     # Some formatting to get rid of '\n' characters and extra
