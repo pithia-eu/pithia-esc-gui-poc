615a6bf350bf050c7db8300481043712d5b3c604
David Chan <davidchan125@hotmail.co.uk>
2023-03-10 14:23:48 +0000

    Tidy up forms.py files

--- register/forms.py
+++ register/forms.py
@@ -2,25 +2,46 @@ from django import forms
 
 
 class UploadFileForm(forms.Form):
-    files = forms.FileField(label='Multiple File Upload', label_suffix='', widget=forms.ClearableFileInput(attrs={
-                'accept': 'application/xml',
-                'class': 'form-control',
-                'multiple': True,
-            }))
+    files = forms.FileField(
+        label='Multiple File Upload',
+        label_suffix='',
+        widget=forms.ClearableFileInput(attrs={
+            'accept': 'application/xml',
+            'class': 'form-control',
+            'multiple': True,
+        })
+    )
 
 class UploadDataCollectionFileForm(forms.Form):
-    files = forms.FileField(label='File Upload', label_suffix='', widget=forms.ClearableFileInput(attrs={
-                'accept': 'application/xml',
-                'class': 'form-control',
-            }))
-    api_selected = forms.BooleanField(label='API', required=False, widget=forms.CheckboxInput(attrs={
-        'class': 'form-check-input'
-    }))
-    api_specification_url = forms.CharField(label='Link to API Specification', required=False, disabled=True, widget=forms.TextInput(attrs={
-                            'class': 'form-control'
-                        }))
-    api_description = forms.CharField(label='Description', required=False, disabled=True, widget=forms.Textarea(attrs={
-                            'class': 'form-control',
-                            'rows': 3,
-                            'style': 'resize: none;'
-                        }))
+    files = forms.FileField(
+        label='File Upload',
+        label_suffix='',
+        widget=forms.ClearableFileInput(attrs={
+            'accept': 'application/xml',
+            'class': 'form-control',
+        }))
+    api_selected = forms.BooleanField(
+        label='API',
+        required=False,
+        widget=forms.CheckboxInput(attrs={
+            'class': 'form-check-input'
+        })
+    )
+    api_specification_url = forms.CharField(
+        label='Link to API Specification',
+        required=False,
+        disabled=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    api_description = forms.CharField(
+        label='Description',
+        required=False,
+        disabled=True,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 3,
+            'style': 'resize: none;'
+        })
+    )
