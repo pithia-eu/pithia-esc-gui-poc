5d07303d47566e5c1dd215e2cf667fd8a4ef5bd1
David Chan <davidchan125@hotmail.co.uk>
2023-03-13 16:06:40 +0000

    Split handle management functions into two files - API functions and XML-related functions

---
+++ handle_management/handle_api.py
@@ -0,0 +1,111 @@
+import json
+import logging
+import os
+import requests
+from dateutil import parser
+from django.urls import reverse_lazy
+from pyhandle.clientcredentials import PIDClientCredentials
+from pyhandle.handleclient import PyHandleClient, RESTHandleClient
+import urllib.parse
+
+logger = logging.getLogger(__name__)
+
+def instantiate_client_and_load_credentials() -> tuple[RESTHandleClient, PIDClientCredentials]:
+    handle_api_url = os.environ['HANDLE_API_ENDPOINT_URL']
+    credentials = None
+    with open('credentials.json', 'w+') as credentials_json_file:
+        credentials_data = {
+            'client': os.environ['HANDLE_API_CLIENT'],
+            'handle_server_url': handle_api_url,
+            'baseuri': handle_api_url,
+            'username': os.environ['HANDLE_API_USERNAME'],
+            'password': os.environ['HANDLE_API_KEY'],
+            'prefix': os.environ['HANDLE_PREFIX'],
+        }
+        json.dump(credentials_data, credentials_json_file, indent=4)
+    credentials = PIDClientCredentials.load_from_JSON('credentials.json')
+    client = PyHandleClient('rest').instantiate_with_credentials(credentials)
+    return client, credentials
+
+def create_handle(credentials: PIDClientCredentials, handle_suffix: str) -> str:
+    return f'{credentials.get_prefix()}/{handle_suffix}'
+
+def register_handle(handle: str, handle_value: str, client: RESTHandleClient):
+    logger.info(f'Registering handle {handle}')
+    register_result = client.register_handle(handle, handle_value)
+
+    if register_result == handle:
+        logger.info('OK: Register handle successful.')
+    else:
+        logger.info('PROBLEM: Register handle returned unexpected response.')
+
+    return register_result
+
+def create_and_register_handle_for_resource(resource_id: str) -> tuple[str, RESTHandleClient, PIDClientCredentials]:
+    client, credentials = instantiate_client_and_load_credentials()
+    handle = create_handle(credentials, resource_id)
+    resource_details_page_url = reverse_lazy('browse:catalogue_data_subset_detail', kwargs={ 'catalogue_data_subset_id': resource_id })
+    resource_details_page_url_string = f'{os.environ["HANDLE_URL_PREFIX"]}{str(resource_details_page_url)}'
+    register_handle(handle, resource_details_page_url_string, client)
+    return handle, client, credentials
+
+def delete_handle(handle: str, client: RESTHandleClient):
+    delete_result = client.delete_handle(handle)
+    logger.info('OK: Delete handle successful.')
+    return delete_result
+
+def get_handle_url(handle: str, client: RESTHandleClient) -> str:
+    key = 'URL'
+    read_value = client.get_value_from_handle(handle, key)
+
+    return read_value
+
+def get_handle_record(handle: str, client: RESTHandleClient) -> dict:
+    handle_record = client.retrieve_handle_record(handle)
+
+    if handle_record != None:
+        logger.info('OK: Handle exists.')
+    else:
+        logger.error('PROBLEM: Retrieving handle record returned unexpected response.')
+
+    return handle_record
+
+def get_handle_raw(handle: str) -> dict:
+    handle_api_endpoint = os.environ['HANDLE_API_ENDPOINT_URL']
+    response = requests.get(f'{handle_api_endpoint}/api/handles/{handle}')
+    handle_raw = response.json()
+    return handle_raw
+
+def get_time_handle_was_issued_as_string(handle: str) -> str:
+    handle_raw = get_handle_raw(handle)
+    date_issued = handle_raw['values'][0]['timestamp']
+    return date_issued
+
+def get_date_handle_was_issued_as_string(handle: str) -> str:
+    handle_issue_time_string = get_time_handle_was_issued_as_string(handle)
+    print('handle_issue_time_string', handle_issue_time_string)
+    handle_issue_date = parser.isoparse(handle_issue_time_string).strftime('%Y-%m-%d')
+    return handle_issue_date
+
+def update_handle_url(handle: str, new_handle_value: str, client: RESTHandleClient):
+    key = 'URL'
+    modify_result = client.modify_handle_value(handle, **{ key: new_handle_value })
+    get_value_result = client.get_value_from_handle(handle, key)
+
+    if get_value_result == new_handle_value:
+        logger.info('OK: Update handle URL successful.')
+    else:
+        logger.critical('CRITICAL: Modify handle URL returned unexpected value.')
+        logger.info(f'Expected: {new_handle_value}')
+        logger.info(f'Returned: {get_value_result}')
+    
+    return modify_result
+
+def get_handles_with_prefix(prefix):
+    handle_api_endpoint = os.environ['HANDLE_API_ENDPOINT_URL']
+    response = requests.get(
+        f'{handle_api_endpoint}/api/handles?prefix={prefix}',
+        auth=(urllib.parse.quote_plus(os.environ['HANDLE_API_USERNAME']), os.environ['HANDLE_API_KEY'])
+    )
+    handles_with_prefix = response.json()
+    return handles_with_prefix
