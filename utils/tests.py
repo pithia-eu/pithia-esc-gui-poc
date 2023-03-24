bc188dfd82da0b3ebf959ff1b4c854c87c738baf
David Chan <davidchan125@hotmail.co.uk>
2023-03-24 18:48:37 +0000

    Move URL division tests to utils app

--- utils/tests.py
+++ utils/tests.py
@@ -1,7 +1,56 @@
 from django.test import SimpleTestCase
-from .url_helpers import convert_ontology_server_urls_to_browse_urls, convert_resource_server_urls_to_browse_urls
+from .url_helpers import (
+    convert_ontology_server_urls_to_browse_urls,
+    convert_resource_server_urls_to_browse_urls,
+    divide_resource_url_from_op_mode_id,
+    divide_resource_url_into_main_components,
+)
 
 # Create your tests here.
+
+class UrlDivisionTestCase(SimpleTestCase):
+    def test_resource_urls_are_divided_correctly(self):
+        """
+        divide_resource_url_into_main_components() divides resource URLs
+        into their main components:
+        - url_base: e.g., https://metadata.pithia.eu/resources/2.2
+        - resource_type: e.g., organisation
+        - namespace: e.g., pithia
+        - localid: e.g., Organisation_PITHIA
+        """
+        resource_url_division_1 = divide_resource_url_into_main_components('https://metadata.pithia.eu/resources/2.2/pithia/project/Project_TEST')
+        resource_url_division_2 = divide_resource_url_into_main_components('https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2/organisation/pithia/Organisation_TEST')
+        resource_url_division_3 = divide_resource_url_into_main_components('https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2/organisation/pithia/Organisation_TEST/organisation/pithia/Organisation_TEST/organisation/pithia/Organisation_TEST')
+        
+        self.assertEquals(resource_url_division_1['url_base'], 'https://metadata.pithia.eu/resources/2.2')
+        self.assertEquals(resource_url_division_1['resource_type'], 'pithia')
+        self.assertEquals(resource_url_division_1['namespace'], 'project')
+        self.assertEquals(resource_url_division_1['localid'], 'Project_TEST')
+        self.assertEquals(resource_url_division_2['url_base'], 'https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2')
+        self.assertEquals(resource_url_division_3['url_base'], 'https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2/organisation/pithia/Organisation_TEST/organisation/pithia/Organisation_TEST')
+        self.assertEquals(resource_url_division_3['resource_type'], 'organisation')
+        self.assertEquals(resource_url_division_3['namespace'], 'pithia')
+
+    def test_resource_urls_with_op_mode_ids_are_divided_correctly(self):
+        """
+        divide_resource_url_from_op_mode_id() divides resource URLs into
+        two components:
+        - resource_url: e.g., https://metadata.pithia.eu/resources/2.2/instrument/pithia/Instrument_PITHIA#ionogram
+        - op_mode_id: e.g., https://metadata.pithia.eu/resources/2.2/instrument/pithia/Instrument_PITHIA#ionogram
+        """
+        resource_url_division_1 = divide_resource_url_from_op_mode_id('https://metadata.pithia.eu/resources/2.2/pithia/project/Project_TEST#ionogram')
+        resource_url_division_2 = divide_resource_url_from_op_mode_id('https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2/organisation/pithia/Organisation_TEST#sweep')
+        resource_url_division_3 = divide_resource_url_from_op_mode_id('https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2/organisation/pithia/Organisation_TEST/organisation/pithia/Organisation_TEST/organisation/pithia/Organisation_TEST#test')
+        resource_url_division_4 = divide_resource_url_from_op_mode_id('https://metadata.pithia.eu/resources/2.2/pithia/project/Project_TEST#ionogram#ionogram#ionogram#ionogram')
+
+        self.assertEquals(resource_url_division_1['resource_url'], 'https://metadata.pithia.eu/resources/2.2/pithia/project/Project_TEST')
+        self.assertEquals(resource_url_division_1['op_mode_id'], 'ionogram')
+        self.assertEquals(resource_url_division_2['resource_url'], 'https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2/organisation/pithia/Organisation_TEST')
+        self.assertEquals(resource_url_division_2['op_mode_id'], 'sweep')
+        self.assertEquals(resource_url_division_3['resource_url'], 'https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2https://metadata.pithia.eu/resources/2.2/organisation/pithia/Organisation_TEST/organisation/pithia/Organisation_TEST/organisation/pithia/Organisation_TEST')
+        self.assertEquals(resource_url_division_3['op_mode_id'], 'test')
+        self.assertEquals(resource_url_division_4['resource_url'], 'https://metadata.pithia.eu/resources/2.2/pithia/project/Project_TEST#ionogram#ionogram#ionogram')
+        self.assertEquals(resource_url_division_4['op_mode_id'], 'ionogram')
 
 class UrlConversionTestCase(SimpleTestCase):
     def test_ontology_url_conversion(self):
