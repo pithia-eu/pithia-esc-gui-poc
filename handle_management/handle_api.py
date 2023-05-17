db1f70165833a7bd89d630267b2251a43d0b1c6a
David Chan <davidchan125@hotmail.co.uk>
2023-05-17 16:21:38 +0100

    Integrate generate_and_register_handle()

--- handle_management/handle_api.py
+++ handle_management/handle_api.py
@@ -51,10 +51,9 @@ def register_handle(handle: str, handle_value: str, client: RESTHandleClient, in
 
 def create_and_register_handle_for_resource(resource_id: str, initial_doi_dict_values: dict = {}) -> tuple[str, RESTHandleClient, PIDClientCredentials]:
     client, credentials = instantiate_client_and_load_credentials()
-    handle = create_handle(credentials, resource_id)
     resource_details_page_url = reverse_lazy('browse:catalogue_data_subset_detail', kwargs={ 'catalogue_data_subset_id': resource_id })
     resource_details_page_url_string = f'{os.environ["HANDLE_URL_PREFIX"]}{str(resource_details_page_url)}'
-    register_handle(handle, resource_details_page_url_string, client, initial_doi_dict_values=initial_doi_dict_values)
+    handle = generate_and_register_handle(resource_details_page_url_string, credentials, client, initial_doi_dict_values=initial_doi_dict_values)
     return handle, client, credentials
 
 def delete_handle(handle: str, client: RESTHandleClient):
