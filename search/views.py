2b3b797db05ce4c390eb5e8399b7d450cae2248d
David Chan <davidchan125@hotmail.co.uk>
2023-05-24 11:26:59 +0100

    Fix registered inst./comp. type functions - Update parameters for search-related helper functions

--- search/views.py
+++ search/views.py
@@ -1,3 +1,4 @@
+import re
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -12,9 +13,11 @@ from ontology.utils import (
     categorise_observed_property_dict_by_top_level_phenomenons,
 )
 from .search_helpers import (
+    BASE_ONTOLOGY_COMPUTATION_TYPE_URL,
+    BASE_ONTOLOGY_INSTRUMENT_TYPE_URL,
     find_matching_data_collections,
     group_instrument_types_by_observed_property,
-    group_computation_types_by_observed_property
+    group_computation_types_by_observed_property,
 )
 from common.mongodb_models import (
     CurrentAcquisitionCapability,
@@ -42,7 +45,7 @@ def get_tree_form_for_ontology_component(request, ontology_component):
     parents_of_registered_ontology_terms = []
     if ontology_component.lower() == 'observedproperty':
         registered_ontology_terms = get_registered_observed_properties()
-        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, None, [])
+        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, [])
         return render(request, 'search/observed_property_tree_categories_template.html', {
             'observed_property_categories': categorise_observed_property_dict_by_top_level_phenomenons(dictionary),
             'ontology_component_name': ontology_component,
@@ -52,21 +55,21 @@ def get_tree_form_for_ontology_component(request, ontology_component):
     elif ontology_component.lower() == 'featureofinterest':
         registered_observed_property_ids = get_registered_observed_properties()
         registered_ontology_terms = get_registered_features_of_interest(registered_observed_property_ids)
-        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, None, [])
+        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, [])
     elif ontology_component.lower() == 'instrumenttype':
         registered_ontology_terms = get_registered_instrument_types()
-        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, None, [])
+        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, [])
     elif ontology_component.lower() == 'computationtype':
         registered_ontology_terms = get_registered_computation_types()
-        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, None, [])
+        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, [])
     elif ontology_component.lower() == 'phenomenon':
         registered_observed_property_ids = get_registered_observed_properties()
         registered_ontology_terms = get_registered_phenomenons(registered_observed_property_ids)
-        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, None, [])
+        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, [])
     elif ontology_component.lower() == 'measurand':
         registered_observed_property_ids = get_registered_observed_properties()
         registered_ontology_terms = get_registered_measurands(registered_observed_property_ids)
-        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, None, [])
+        parents_of_registered_ontology_terms = get_parents_of_registered_ontology_terms(registered_ontology_terms, ontology_component, [])
     return render(request, 'search/ontology_tree_template_outer.html', {
         'ontology_component': dictionary,
         'ontology_component_name': ontology_component,
@@ -153,10 +156,16 @@ def get_registered_features_of_interest(registered_observed_property_ids):
     return list(set(feature_of_interest_ids))
 
 def get_registered_instrument_types():
-    return list(map(extract_localid_from_xlink_href, list(CurrentInstrument.find().distinct('type.@xlink:href'))))
+    types_from_instruments = list(map(extract_localid_from_xlink_href, list(CurrentInstrument.find({'type.@xlink:href': re.compile(f'^{BASE_ONTOLOGY_INSTRUMENT_TYPE_URL}')}).distinct('type.@xlink:href'))))
+    types_from_data_collections = list(map(extract_localid_from_xlink_href, list(CurrentDataCollection.find({'type.@xlink:href': re.compile(f'^{BASE_ONTOLOGY_INSTRUMENT_TYPE_URL}')}).distinct('type.@xlink:href'))))
+    all_registered_instrument_types = list(set(types_from_instruments + types_from_data_collections))
+    return all_registered_instrument_types
 
 def get_registered_computation_types():
-    return list(map(extract_localid_from_xlink_href, list(CurrentComputationCapability.find().distinct('type.@xlink:href'))))
+    types_from_computation_capability_sets = list(map(extract_localid_from_xlink_href, list(CurrentComputationCapability.find({'type.@xlink:href': re.compile(f'^{BASE_ONTOLOGY_COMPUTATION_TYPE_URL}')}).distinct('type.@xlink:href'))))
+    types_from_data_collections = list(map(extract_localid_from_xlink_href, list(CurrentDataCollection.find({'type.@xlink:href': re.compile(f'^{BASE_ONTOLOGY_COMPUTATION_TYPE_URL}')}).distinct('type.@xlink:href'))))
+    all_registered_computation_types = list(set(types_from_computation_capability_sets + types_from_data_collections))
+    return all_registered_computation_types
 
 def get_registered_phenomenons(registered_observed_property_ids):
     phenomenon_ids = []
@@ -172,12 +181,12 @@ def get_registered_measurands(registered_observed_property_ids):
         get_measurand_ids_from_observed_property_id(id, g_op, measurand_ids)
     return measurand_ids
 
-def get_parents_of_registered_ontology_terms(ontology_term_ids, ontology_component, g, parent_node_ids):
+def get_parents_of_registered_ontology_terms(ontology_term_ids, ontology_component, parent_node_ids, g=None):
     if g is None:
         g = get_graph_of_pithia_ontology_component(ontology_component)
     for id in ontology_term_ids:
-        parent_node_ids_of_id = get_parent_node_ids_of_node_id(id, ontology_component, g, [])
+        parent_node_ids_of_id = get_parent_node_ids_of_node_id(id, ontology_component, [], g)
         if len(parent_node_ids_of_id) > 0:
             parent_node_ids.extend(parent_node_ids_of_id)
-            parent_node_ids = get_parents_of_registered_ontology_terms(parent_node_ids_of_id, ontology_component, g, parent_node_ids)
+            parent_node_ids = get_parents_of_registered_ontology_terms(parent_node_ids_of_id, ontology_component, parent_node_ids, g=g)
     return parent_node_ids
