6cf991356951d70274370d8c858303f4088f2416
David Chan <davidchan125@hotmail.co.uk>
2023-05-30 09:44:19 +0100

    Implement metadata managers

--- common/models.py
+++ common/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .managers import OrganisationManager
+from .managers import *
 
 # Create your models here.
 class ScientificMetadata(models.Model):
@@ -119,57 +119,169 @@ class Organisation(ScientificMetadata):
         proxy = True
 
 class Individual(ScientificMetadata):
+    type_in_metadata_server_url = 'individual'
+    weight = 2
+    type_readable = 'individual'
+    type_plural_readable = 'individuals'
+    a_or_an = 'an'
+
+    objects = IndividualManager()
+
     class Meta:
         proxy = True
 
 class Project (ScientificMetadata):
+    type_in_metadata_server_url = 'project'
+    weight = 3
+    type_readable = 'project'
+    type_plural_readable = 'projects'
+    a_or_an = 'a'
+
+    objects = ProjectManager()
+
     class Meta:
         proxy = True
 
 class Platform(ScientificMetadata):
+    type_in_metadata_server_url = 'platform'
+    weight = 4
+    type_readable = 'platform'
+    type_plural_readable = 'platforms'
+    a_or_an = 'a'
+
+    objects = PlatformManager()
+
     class Meta:
         proxy = True
 
 class Operation(ScientificMetadata):
+    type_in_metadata_server_url = 'operation'
+    weight = 5
+    type_readable = 'operation'
+    type_plural_readable = 'operations'
+    a_or_an = 'an'
+
+    objects = OperationManager()
+
     class Meta:
         proxy = True
 
 class Instrument(ScientificMetadata):
+    type_in_metadata_server_url = 'instrument'
+    weight = 6
+    type_readable = 'instrument'
+    type_plural_readable = 'instruments'
+    a_or_an = 'an'
+
+    objects = InstrumentManager()
+
     class Meta:
         proxy = True
 
 class AcquisitionCapabilities(ScientificMetadata):
+    type_in_metadata_server_url = 'acquisitionCapabilities'
+    weight = 7
+    type_readable = 'acquisition capabilities'
+    type_plural_readable = 'acquisition capabilities'
+    a_or_an = 'an'
+
+    objects = AcquisitionCapabilitiesManager()
+
     class Meta:
         proxy = True
 
 class Acquisition(ScientificMetadata):
+    type_in_metadata_server_url = 'acquisition'
+    weight = 8
+    type_readable = 'acquisition'
+    type_plural_readable = 'acquisitions'
+    a_or_an = 'an'
+
+    objects = AcquisitionManager()
+
     class Meta:
         proxy = True
 
 class ComputationCapabilities(ScientificMetadata):
+    type_in_metadata_server_url = 'computationCapabilities'
+    weight = 9
+    type_readable = 'computation capabilities'
+    type_plural_readable = 'computation capabilities'
+    a_or_an = 'a'
+
+    objects = ComputationCapabilitiesManager()
+
     class Meta:
         proxy = True
 
 class Computation(ScientificMetadata):
+    type_in_metadata_server_url = 'computation'
+    weight = 10
+    type_readable = 'computation'
+    type_plural_readable = 'computations'
+    a_or_an = 'a'
+
+    objects = ComputationManager()
+
     class Meta:
         proxy = True
 
 class Process(ScientificMetadata):
+    type_in_metadata_server_url = 'process'
+    weight = 11
+    type_readable = 'process'
+    type_plural_readable = 'processes'
+    a_or_an = 'a'
+
+    objects = ProcessManager()
+
     class Meta:
         proxy = True
 
 class DataCollection(ScientificMetadata):
+    type_in_metadata_server_url = 'collection'
+    weight = 12
+    type_readable = 'data collection'
+    type_plural_readable = 'data collections'
+    a_or_an = 'a'
+
+    objects = DataCollectionManager()
+
     class Meta:
         proxy = True
 
 class Catalogue(ScientificMetadata):
+    type_in_metadata_server_url = 'catalogue'
+    weight = 13
+    type_readable = 'catalogue'
+    type_plural_readable = 'catalogues'
+    a_or_an = 'a'
+
+    objects = CatalogueManager()
+
     class Meta:
         proxy = True
 
 class CatalogueEntry(ScientificMetadata):
+    type_in_metadata_server_url = 'catalogue'
+    weight = 14
+    type_readable = 'catalogue entry'
+    type_plural_readable = 'catalogue entries'
+    a_or_an = 'a'
+
+    objects = CatalogueEntryManager()
+
     class Meta:
         proxy = True
 
 class CatalogueDataSubset(ScientificMetadata):
+    type_in_metadata_server_url = 'catalogue'
+    weight = 15
+    type_readable = 'catalogue data subset'
+    type_plural_readable = 'catalogue data subsets'
+    a_or_an = 'a'
+
+    objects = CatalogueDataSubsetManager()
+
     class Meta:
         proxy = True
