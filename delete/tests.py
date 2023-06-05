a6a0942551722257c2fd2fff6ff1295d3d5a1ccc
David Chan <davidchan125@hotmail.co.uk>
2023-06-05 15:36:41 +0100

    Add Organisation delete chain test

--- delete/tests.py
+++ delete/tests.py
@@ -1,37 +1,58 @@
-import environ
-from django.test import TestCase, SimpleTestCase
-import mongomock
+import os
+from django.test import TestCase
 
-env = environ.Env()
+from common.models import (
+    Organisation,
+    Individual,
+    Project,
+    Platform,
+    Operation,
+    Instrument,
+    AcquisitionCapabilities,
+    Acquisition,
+    ComputationCapabilities,
+    Computation,
+    Process,
+    DataCollection,
+    Catalogue,
+    CatalogueEntry,
+    CatalogueDataSubset,
+)
+from pithiaesc.settings import BASE_DIR
+
+_XML_METADATA_FILE_DIR = os.path.join(BASE_DIR, 'common', 'test_files', 'xml_metadata_files')
 
 # Create your tests here.
 
-# class PymongoTestCase(SimpleTestCase):
-#     def test_pymongo_sessions(self):
-#         """
-#         pymongo undoes a MongoDB transaction
-#         if an error occurs during an operation.
+class OrganisationTestCase(TestCase):
+    def setUp(self) -> None:
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Individual_Test.xml')) as xml_file:
+            Individual.objects.create_from_xml_string(xml_file.read())
+            
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Project_Test.xml')) as xml_file:
+            project = Project.objects.create_from_xml_string(xml_file.read())
 
-#         TEST DOES NOT WORK AS MONGOMOCK DOES NOT
-#         SUPPORT SESSIONS YET.
-#         """
-#         client = mongomock.MongoClient()
-#         MockCurrentDataCollection = client[env('DB_NAME')]['current-data-collections']
-#         test_data_collection_name = 'Test Data Collection'
-#         MockCurrentDataCollection.insert_one({
-#             'name': test_data_collection_name
-#         })
-#         try:
-#             with client.start_session() as s:
-#                 def cb(s):
-#                     MockCurrentDataCollection.delete_one({
-#                         'name': test_data_collection_name
-#                     })
-#                     raise BaseException('Test exception')
-#                 s.with_transaction(cb)
-#         except BaseException as e:
-#             print(e)
-#             test_data_collection = MockCurrentDataCollection.find_one({
-#                 'name': test_data_collection_name
-#             })
-#             self.assertNotEqual(test_data_collection, None)
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Platform_Test.xml')) as xml_file:
+            Platform.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Instrument_Test.xml')) as xml_file:
+            Instrument.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'DataCollection_Test.xml')) as xml_file:
+            DataCollection.objects.create_from_xml_string(xml_file.read())
+
+        return super().setUp()
+    
+    def test_metadata_dependents(self):
+        """
+        metadata_dependents() returns all metadata
+        registration dependents for an
+        Organisation.
+        """
+        organisation = None
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Organisation_Test.xml')) as xml_file:
+            organisation = Organisation.objects.create_from_xml_string(xml_file.read())
+        print('organisation.metadata_dependents' , organisation.metadata_dependents)
+        print('len(organisation.metadata_dependents)' , len(organisation.metadata_dependents))
+        self.assertTrue(len(organisation.metadata_dependents) > 0)
+
