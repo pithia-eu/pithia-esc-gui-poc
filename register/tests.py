a188cab454bcd2aef894095b3c30207d13f10a3c
davidchan125 <davidchan125@hotmail.co.uk>
2023-06-07 17:56:08 +0100

    Add test for create_from_xml_string()

--- register/tests.py
+++ register/tests.py
@@ -1,12 +1,19 @@
-import os
 import environ
 import mongomock
+import os
 from bson.objectid import ObjectId
-from django.test import SimpleTestCase, tag
-from register.register import register_metadata_xml_file
-from validation.errors import FileRegisteredBefore
-from pithiaesc.settings import BASE_DIR
+from django.test import (
+    SimpleTestCase,
+    TestCase,
+    tag,
+)
 from pathlib import Path
+
+from .register import register_metadata_xml_file
+
+from common.models import Organisation
+from pithiaesc.settings import BASE_DIR
+from validation.errors import FileRegisteredBefore
 
 _XML_METADATA_FILE_DIR = os.path.join(BASE_DIR, 'common', 'test_files', 'xml_metadata_files')
 
@@ -36,3 +43,18 @@ class RegisterMetadataTestCase(SimpleTestCase):
         except FileRegisteredBefore as err:
             print(err)
             self.fail('register_metadata_xml_file() unexpectedly raised an error!')
+
+class ManagerTestCase(TestCase):
+    def test_create_from_xml_string(self):
+        """
+        Model.objects.create_from_xml_string() returns a new
+        registration with an ID property.
+        """
+        try:
+            with open(os.path.join(_XML_METADATA_FILE_DIR, 'Organisation_Test.xml')) as xml_file:
+                organisation = Organisation.objects.create_from_xml_string(xml_file.read())
+                self.assertIn('id', organisation)
+                print('organisation.id', organisation.id)
+        except BaseException as err:
+            print(err)
+            self.fail('test_create_from_xml_string() unexpectedly raised an error!')
