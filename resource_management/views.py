f84610acfa0da9bd98cd8934d18c35c3b4ab500e
davidchan125 <davidchan125@hotmail.co.uk>
2023-06-09 16:42:40 +0100

    Remove unused code in res_man views.py

--- resource_management/views.py
+++ resource_management/views.py
@@ -3,23 +3,6 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 from common import models
-from common.mongodb_models import (
-    CurrentAcquisition,
-    CurrentAcquisitionCapability,
-    CurrentComputation,
-    CurrentComputationCapability,
-    CurrentDataCollection,
-    CurrentIndividual,
-    CurrentInstrument,
-    CurrentOperation,
-    CurrentOrganisation,
-    CurrentPlatform,
-    CurrentProcess,
-    CurrentProject,
-    CurrentCatalogue,
-    CurrentCatalogueEntry,
-    CurrentCatalogueDataSubset,
-)
 from utils.mapping_functions import prepare_resource_for_template
 
 _INDEX_PAGE_TITLE = 'Register & Manage Metadata'
@@ -48,19 +31,6 @@ def data_collection_related_metadata_index(request):
     num_current_computations = models.Computation.objects.count()
     num_current_processes = models.Process.objects.count()
     num_current_data_collections = models.DataCollection.objects.count()
-    # TODO: remove old code
-    # num_current_organsations = CurrentOrganisation.estimated_document_count()
-    # num_current_individuals = CurrentIndividual.estimated_document_count()
-    # num_current_projects = CurrentProject.estimated_document_count()
-    # num_current_platforms = CurrentPlatform.estimated_document_count()
-    # num_current_instruments = CurrentInstrument.estimated_document_count()
-    # num_current_operations = CurrentOperation.estimated_document_count()
-    # num_current_acquisition_capability_sets = CurrentAcquisitionCapability.estimated_document_count()
-    # num_current_acquisitions = CurrentAcquisition.estimated_document_count()
-    # num_current_computation_capability_sets = CurrentComputationCapability.estimated_document_count()
-    # num_current_computations = CurrentComputation.estimated_document_count()
-    # num_current_processes = CurrentProcess.estimated_document_count()
-    # num_current_data_collections = CurrentDataCollection.estimated_document_count()
     return render(request, 'resource_management/data_collection_index.html', {
         'num_current_organisations': num_current_organsations,
         'num_current_individuals': num_current_individuals,
@@ -83,10 +53,6 @@ def catalogue_related_metadata_index(request):
     num_current_catalogues = models.Catalogue.objects.count()
     num_current_catalogue_entries = models.CatalogueEntry.objects.count()
     num_current_catalogue_data_subsets = models.CatalogueDataSubset.objects.count()
-    # TODO: remove old code
-    # num_current_catalogues = CurrentCatalogue.estimated_document_count()
-    # num_current_catalogue_entries = CurrentCatalogueEntry.estimated_document_count()
-    # num_current_catalogue_data_subsets = CurrentCatalogueDataSubset.estimated_document_count()
     return render(request, 'resource_management/catalogue_index.html', {
         'num_current_catalogues': num_current_catalogues,
         'num_current_catalogue_entries': num_current_catalogue_entries,
@@ -116,7 +82,6 @@ class ResourceManagementListView(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = _create_manage_resource_page_title(self.model.type_plural_readable)
         context['resource_type_plural'] = self.model.type_plural_readable
-        context['resource_list'] = self.get_resource_list()
         context['empty_resource_list_text'] = f'No {self.model.type_plural_readable.lower()} have been registered with the e-Science Centre.'
         context['resource_delete_page_url_name'] = self.resource_delete_page_url_name
         context['resource_update_page_url_name'] = self.resource_update_page_url_name
@@ -127,13 +92,9 @@ class ResourceManagementListView(ListView):
         context['resource_management_category_list_page_breadcrumb_url_name'] = self.resource_management_category_list_page_breadcrumb_url_name
         return context
 
-    def get_resource_list(self):
-        resource_list = list(self.resource_mongodb_model.find({}))
-        return list(map(prepare_resource_for_template, resource_list))
-
 class OrganisationManagementListView(ResourceManagementListView):
     model = models.Organisation
-    resource_mongodb_model = CurrentOrganisation
+
     resource_delete_page_url_name = 'delete:organisation'
     resource_update_page_url_name = 'update:organisation'
     resource_register_page_url_name = 'register:organisation'
@@ -141,7 +102,7 @@ class OrganisationManagementListView(ResourceManagementListView):
 
 class IndividualManagementListView(ResourceManagementListView):
     model = models.Individual
-    resource_mongodb_model = CurrentIndividual
+
     resource_delete_page_url_name = 'delete:individual'
     resource_update_page_url_name = 'update:individual'
     resource_register_page_url_name = 'register:individual'
@@ -149,7 +110,7 @@ class IndividualManagementListView(ResourceManagementListView):
 
 class ProjectManagementListView(ResourceManagementListView):
     model = models.Project
-    resource_mongodb_model = CurrentProject
+
     resource_delete_page_url_name = 'delete:project'
     resource_update_page_url_name = 'update:project'
     resource_register_page_url_name = 'register:project'
@@ -158,7 +119,7 @@ class ProjectManagementListView(ResourceManagementListView):
 class PlatformManagementListView(ResourceManagementListView):
     model = models.Platform
     template_name = 'resource_management/platform_management_list.html'
-    resource_mongodb_model = CurrentPlatform
+
     resource_delete_page_url_name = 'delete:platform'
     resource_update_page_url_name = 'update:platform'
     resource_register_page_url_name = 'register:platform'
@@ -173,15 +134,20 @@ class PlatformManagementListView(ResourceManagementListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         pithia_platforms, non_pithia_platforms = [], []
-        for p in context['resource_list']:
-            pithia_platforms.append(p) if p['identifier']['PITHIA_Identifier']['namespace'] == 'pithia' else non_pithia_platforms.append(p)
+        for r in context['resources']:
+            if r.namespace == 'pithia':
+                pithia_platforms.append(r)
+            else:
+                non_pithia_platforms.append(r)
         context['pithia_platforms'] = pithia_platforms
         context['non_pithia_platforms'] = non_pithia_platforms
+        context['no_platform_networks_message'] = 'No platform networks have been registered with the e-Science Centre.'
+        context['no_platforms_message'] = 'No individual platforms have been registered with the e-Science Centre.'
         return context
 
 class OperationManagementListView(ResourceManagementListView):
     model = models.Operation
-    resource_mongodb_model = CurrentOperation
+
     resource_delete_page_url_name = 'delete:operation'
     resource_update_page_url_name = 'update:operation'
     resource_register_page_url_name = 'register:operation'
@@ -189,7 +155,7 @@ class OperationManagementListView(ResourceManagementListView):
 
 class InstrumentManagementListView(ResourceManagementListView):
     model = models.Instrument
-    resource_mongodb_model = CurrentInstrument
+
     resource_delete_page_url_name = 'delete:instrument'
     resource_update_page_url_name = 'update:instrument'
     resource_register_page_url_name = 'register:instrument'
@@ -197,7 +163,7 @@ class InstrumentManagementListView(ResourceManagementListView):
 
 class AcquisitionCapabilitiesManagementListView(ResourceManagementListView):
     model = models.AcquisitionCapabilities
-    resource_mongodb_model = CurrentAcquisitionCapability
+
     resource_delete_page_url_name = 'delete:acquisition_capability_set'
     resource_update_page_url_name = 'update:acquisition_capability_set'
     resource_register_page_url_name = 'register:acquisition_capability_set'
@@ -205,7 +171,7 @@ class AcquisitionCapabilitiesManagementListView(ResourceManagementListView):
 
 class AcquisitionManagementListView(ResourceManagementListView):
     model = models.Acquisition
-    resource_mongodb_model = CurrentAcquisition
+
     resource_delete_page_url_name = 'delete:acquisition'
     resource_update_page_url_name = 'update:acquisition'
     resource_register_page_url_name = 'register:acquisition'
@@ -213,7 +179,7 @@ class AcquisitionManagementListView(ResourceManagementListView):
 
 class ComputationCapabilitiesManagementListView(ResourceManagementListView):
     model = models.ComputationCapabilities
-    resource_mongodb_model = CurrentComputationCapability
+
     resource_delete_page_url_name = 'delete:computation_capability_set'
     resource_update_page_url_name = 'update:computation_capability_set'
     resource_register_page_url_name = 'register:computation_capability_set'
@@ -221,7 +187,7 @@ class ComputationCapabilitiesManagementListView(ResourceManagementListView):
 
 class ComputationManagementListView(ResourceManagementListView):
     model = models.Computation
-    resource_mongodb_model = CurrentComputation
+
     resource_delete_page_url_name = 'delete:computation'
     resource_update_page_url_name = 'update:computation'
     resource_register_page_url_name = 'register:computation'
@@ -229,7 +195,7 @@ class ComputationManagementListView(ResourceManagementListView):
 
 class ProcessManagementListView(ResourceManagementListView):
     model = models.Process
-    resource_mongodb_model = CurrentProcess
+
     resource_delete_page_url_name = 'delete:process'
     resource_update_page_url_name = 'update:process'
     resource_register_page_url_name = 'register:process'
@@ -237,7 +203,7 @@ class ProcessManagementListView(ResourceManagementListView):
 
 class DataCollectionManagementListView(ResourceManagementListView):
     model = models.DataCollection
-    resource_mongodb_model = CurrentDataCollection
+
     resource_delete_page_url_name = 'delete:data_collection'
     resource_update_page_url_name = 'update:data_collection'
     resource_register_page_url_name = 'register:data_collection'
@@ -250,7 +216,7 @@ class DataCollectionManagementListView(ResourceManagementListView):
 
 class CatalogueManagementListView(ResourceManagementListView):
     model = models.Catalogue
-    resource_mongodb_model = CurrentCatalogue
+
     resource_delete_page_url_name = 'delete:catalogue'
     resource_update_page_url_name = 'update:catalogue'
     resource_register_page_url_name = 'register:catalogue'
@@ -260,7 +226,7 @@ class CatalogueManagementListView(ResourceManagementListView):
 
 class CatalogueEntryManagementListView(ResourceManagementListView):
     model = models.CatalogueEntry
-    resource_mongodb_model = CurrentCatalogueEntry
+
     resource_delete_page_url_name = 'delete:catalogue_entry'
     resource_update_page_url_name = 'update:catalogue_entry'
     resource_register_page_url_name = 'register:catalogue_entry'
@@ -270,7 +236,7 @@ class CatalogueEntryManagementListView(ResourceManagementListView):
 
 class CatalogueDataSubsetManagementListView(ResourceManagementListView):
     model = models.CatalogueDataSubset
-    resource_mongodb_model = CurrentCatalogueDataSubset
+
     resource_delete_page_url_name = 'delete:catalogue_data_subset'
     resource_update_page_url_name = 'update:catalogue_data_subset'
     resource_register_page_url_name = 'register:catalogue_data_subset'
