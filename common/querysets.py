90d7f754c29d409d0bd87c4e5ec6efb4387eb584
davidchan125 <davidchan125@hotmail.co.uk>
2023-06-12 13:19:01 +0100

    Fix PlatformQuerySet.referencing_party_url()

--- common/querysets.py
+++ common/querysets.py
@@ -50,7 +50,7 @@ class ProjectQuerySet(ScientificMetadataQuerySet, AbstractProjectDatabaseQueries
 
 class PlatformQuerySet(ScientificMetadataQuerySet, AbstractPlatformDatabaseQueries):
     def referencing_party_url(self, party_url: str):
-        return self.filter(**{'json__relatedParty__contains': [{'ResponsiblePartyInfo': {'party': {'@xlink:href': party_url}}}]})
+        return self.filter(**{'json__relatedParty__contains': {'ResponsiblePartyInfo': {'party': {'@xlink:href': party_url}}}})
     
     def referencing_platform_url(self, platform_url: str):
         return self.filter(**{'json__childPlatform__@xlink:href': platform_url})
