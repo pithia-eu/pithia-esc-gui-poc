0bf7ac8c73671b202cb3dc2237eb7bbb42bf8c44
davidchan125 <davidchan125@hotmail.co.uk>
2023-06-13 11:31:07 +0100

    Remove PyMongo-related test in register app

--- register/tests.py
+++ register/tests.py
@@ -1,48 +1,17 @@
 import environ
-import mongomock
 import os
-from bson.objectid import ObjectId
 from django.test import (
-    SimpleTestCase,
     TestCase,
-    tag,
 )
-from pathlib import Path
-
-from .register import register_metadata_xml_file
 
 from common.models import Organisation
 from pithiaesc.settings import BASE_DIR
-from validation.errors import FileRegisteredBefore
 
 _XML_METADATA_FILE_DIR = os.path.join(BASE_DIR, 'common', 'test_files', 'xml_metadata_files')
 
 env = environ.Env()
 
 # Create your tests here.
-# The SimpleTestCase class is used to disable the automatic SQL database
-# create/destroy that Django automatically does with the default
-# TestCase class. MongoDB is still used.
-class RegisterMetadataTestCase(SimpleTestCase):
-    @tag('fast')
-    def test_register_organisation(self):
-        """
-        register_metadata_xml_file() returns a dict with an auto-generated '_id' property
-        """
-        try:
-            client = mongomock.MongoClient()
-            MockCurrentOrganisation = client[env('DB_NAME')]['current-organisations']
-            with open(os.path.join(_XML_METADATA_FILE_DIR, 'Organisation_Test.xml')) as xml_file:
-                registered_resource = register_metadata_xml_file(
-                    xml_file,
-                    MockCurrentOrganisation,
-                    None
-                )
-                self.assertIs(ObjectId.is_valid(registered_resource['_id']), True)
-                print(f'Passed registration validation for {Path(xml_file.name).name}.')
-        except FileRegisteredBefore as err:
-            print(err)
-            self.fail('register_metadata_xml_file() unexpectedly raised an error!')
 
 class ManagerTestCase(TestCase):
     def test_create_from_xml_string(self):
