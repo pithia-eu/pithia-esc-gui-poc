7cfcf77df14b031fecedcb0ae3c5ce89e30ea3a0
davidchan125 <davidchan125@hotmail.co.uk>
2023-06-14 00:25:50 +0100

    Add temp attribute for ResourceRegisterFormView

--- register/views.py
+++ register/views.py
@@ -65,6 +65,7 @@ class ResourceRegisterFormView(FormView):
 
     # TODO: remove old code
     resource_mongodb_model = None
+    resource_conversion_validate_and_correct_function = None
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
