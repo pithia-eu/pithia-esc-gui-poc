3afb84d6fb3733989514884486d2a7d32cefa24b
David Chan <davidchan125@hotmail.co.uk>
2023-06-13 18:46:36 +0100

    Implement test for update_config() method

--- update/tests.py
+++ update/tests.py
@@ -1,7 +1,12 @@
 import os
 from django.test import TestCase
 
-from common.models import Organisation
+from common.models import (
+    Organisation,
+    DataCollection,
+    InteractionMethod,
+    APIInteractionMethod,
+)
 from pithiaesc.settings import BASE_DIR
 
 _XML_METADATA_FILE_DIR = os.path.join(BASE_DIR, 'common', 'test_files', 'xml_metadata_files')
@@ -29,4 +34,26 @@ class XMLMetadataFileUpdateTestCase(TestCase):
         print('organisation_updated.name', organisation_updated.name)
         self.assertEqual(organisation_original.pk, organisation_updated.pk)
         self.assertNotEqual(organisation_original.name, organisation_updated.name)
-        
+
+class InteractionMethodTestCase(TestCase):
+    def test_update_config(self):
+        data_collection = None
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'DataCollection_Test.xml')) as xml_file:
+            data_collection = DataCollection.objects.create_from_xml_string(xml_file.read())
+        interaction_method = InteractionMethod.api_interaction_methods.create_api_interaction_method(
+            'https://www.example.com',
+            '',
+            data_collection
+        )
+
+        new_specification_url = 'https://www.example-updated.com'
+        new_description = 'description updated'
+        updated_interaction_method = APIInteractionMethod.objects.update_config(
+            interaction_method.pk,
+            new_specification_url,
+            new_description
+        )
+        print('updated_interaction_method.specification_url', updated_interaction_method.specification_url)
+        print('updated_interaction_method.description', updated_interaction_method.description)
+        self.assertEqual(updated_interaction_method.specification_url, new_specification_url)
+        self.assertEqual(updated_interaction_method.description, new_description)
