4f7b36d8290321e84f3a8e98b2b7fddbaa85e42a
davidchan125 <davidchan125@hotmail.co.uk>
2023-06-30 09:53:03 +0100

    Re-add DB name in mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -10,4 +10,4 @@ try:
     client = MongoClient(env('MONGODB_CONNECTION_STRING'), serverSelectionTimeoutMS=1)
 except ServerSelectionTimeoutError:
     client = MongoClient(env('MONGODB_CONNECTION_STRING'), tlsCAFile=certifi.where())
-db = client[env('MIGRATION_TEST_MONGODB_NAME')]
+db = client[env('DB_NAME')]
