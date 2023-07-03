85f4bbaf8fdfd2835b8beec09ffbb2adfe0e6903
davidchan125 <davidchan125@hotmail.co.uk>
2023-07-03 12:03:56 +0100

    Refactor pithiaesc views.py

--- pithiaesc/views.py
+++ pithiaesc/views.py
@@ -4,18 +4,22 @@ import requests
 from django.shortcuts import render
 
 from user_management.services import (
+    CREATION_URL_BASE,
     get_user_info,
 )
 
 def logout(request):
+    # Send a GET request to the EGI Check-in Logout endpoint
     ID_TOKEN_HINT = request.META.get('OIDC_id_token')
-    # Logout
     logout_response = requests.get(f'https://aai-demo.egi.eu/auth/realms/egi/protocol/openid-connect/logout?id_token_hint={ID_TOKEN_HINT}')
-#    return HttpResponseRedirect(f'https://aai-demo.egi.eu/auth/realms/egi/protocol/openid-connect/logout?id_token_hint={ID_TOKEN_HINT}')
+    
+    # TEST: Redirect straight to the EGI Check-in logout page.
+    # return HttpResponseRedirect(f'https://aai-demo.egi.eu/auth/realms/egi/protocol/openid-connect/logout?id_token_hint={ID_TOKEN_HINT}')
+    
+    # Verify EGI Check-in logout was successful by checking for an
+    # error from the UserInfo API.
     url = 'https://aai-demo.egi.eu/auth/realms/egi/protocol/openid-connect/userinfo'
-    request_meta = request.META.items()
     ACCESS_TOKEN = request.META.get('OIDC_access_token')
-
     user_info_response = get_user_info(url, ACCESS_TOKEN)
     user_info = json.loads(user_info_response.text)
     error_in_user_info = 'error' in user_info
@@ -35,5 +39,5 @@ def index(request):
 def index_admin(request):
     return render(request, 'index.html', {
         'title': 'Admin Dashboard',
-        'create_perun_organisation_url': 'https://perun.egi.eu/egi/registrar/?vo=vo.esc.pithia.eu&group=organizationRequests',
+        'create_perun_organisation_url': CREATION_URL_BASE,
     })
