1d35668428f82d57f6d37bd53e02299f335fe752
David Chan <davidchan125@hotmail.co.uk>
2023-07-05 15:24:50 +0100

    Remove login URL

--- pithiaesc/urls.py
+++ pithiaesc/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('', include('browse.urls')),
     path('ontology/', include('ontology.urls')),
