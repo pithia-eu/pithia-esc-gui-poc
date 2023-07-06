19f22539b89af1d65a13a0f244e1f9b85eb9d509
davidchan125 <davidchan125@hotmail.co.uk>
2023-07-06 16:45:51 +0100

    Update: create session var getters for user subgroups & ID

--- user_management/services.py
+++ user_management/services.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from urllib.parse import unquote
 
 # Used in the main menu. Used as the base link for creating new institutions.
 CREATION_URL_BASE = 'https://perun.egi.eu/egi/registrar/?vo=vo.esc.pithia.eu&group=organizations:'
@@ -25,3 +26,38 @@ def get_user_info(access_token):
     }
     response = requests.get(url, headers=headers)
     return json.loads(response.text)
+
+def get_institution_subgroups_of_logged_in_user(request):
+    eduperson_entitlement = request.session.get('eduperson_entitlement')
+
+    # Find all groups that are a part of
+    # the 'organizations' parent group.
+    organisations = []
+    for s in eduperson_entitlement:
+        index = s.rfind('organizations', 0, -1)
+        if index == -1:
+            # The 'organizations' group is not mentioned.
+            continue
+
+        index1 = s.rfind('role', 0, -1)
+        new_string = s[index: index1 - 1]
+        if len(new_string) <= len('organizations'):
+            # No subgroup of 'organizations' found, just
+            # the string for the 'organizations' group
+            # itself.
+            continue
+
+        index2 = new_string.find(':', 0, -1)
+        organisations.append(new_string[index2 + 1:])
+    
+    # Find the subgroups
+    subgroups = []
+    for o in organisations:
+        index = o.find(':')
+        if index == -1:
+            continue
+        subgroups.append(unquote(o[:]))
+    return subgroups
+
+def get_logged_in_user_id(request):
+    return request.session.get('email')
