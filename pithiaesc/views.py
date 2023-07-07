0ce3ba73668886f356a65f0d829b67877af685ab
davidchan125 <davidchan125@hotmail.co.uk>
2023-07-07 17:28:23 +0100

    Comment out call to EGI Check-in logout endpoint

--- pithiaesc/views.py
+++ pithiaesc/views.py
@@ -21,8 +21,8 @@ def logout(request):
     remove_login_session_variables(request)
     
     # Send a GET request to the EGI Check-in Logout endpoint
-    ID_TOKEN_HINT = request.META.get('OIDC_id_token')
-    logout_response = requests.get(f'https://aai-demo.egi.eu/auth/realms/egi/protocol/openid-connect/logout?id_token_hint={ID_TOKEN_HINT}')
+    # ID_TOKEN_HINT = request.META.get('OIDC_id_token')
+    # logout_response = requests.get(f'https://aai-demo.egi.eu/auth/realms/egi/protocol/openid-connect/logout?id_token_hint={ID_TOKEN_HINT}')
 
     absolute_home_page_uri = request.build_absolute_uri(reverse("home"))
     return HttpResponseRedirect(f'/authorised/?{urlencode({"logout": absolute_home_page_uri})}')
