c02eb8752742a3bbf39aa7981b712444d409e598
davidchan125 <davidchan125@hotmail.co.uk>
2023-07-07 11:28:10 +0100

    Update logout view - Require POST request - Remove logic used for testing - Perform a home page redirect instead of render()

--- pithiaesc/views.py
+++ pithiaesc/views.py
@@ -1,7 +1,9 @@
 import environ
 import requests
 from django.http import HttpResponseRedirect
+from django.views.decorators.http import require_POST
 from django.shortcuts import render
+from django.urls import reverse
 
 # Initialise environment variables
 env = environ.Env()
@@ -13,29 +15,32 @@ from user_management.services import (
     remove_login_session_variables,
 )
 
+@require_POST
 def logout(request):
     # Send a GET request to the EGI Check-in Logout endpoint
     ID_TOKEN_HINT = request.META.get('OIDC_id_token')
     logout_response = requests.get(f'https://aai-demo.egi.eu/auth/realms/egi/protocol/openid-connect/logout?id_token_hint={ID_TOKEN_HINT}')
+
+    # TEST: Verify logout was successful by checking for success
+    # status code in response.
+    # print('logout_response', logout_response)
     
     # TEST: Redirect straight to the EGI Check-in logout page.
     # return HttpResponseRedirect(f'https://aai-demo.egi.eu/auth/realms/egi/protocol/openid-connect/logout?id_token_hint={ID_TOKEN_HINT}')
     
-    # Verify EGI Check-in logout was successful by checking for an
+    # TEST: Verify EGI Check-in logout was successful by checking for an
     # error from the UserInfo API.
-    ACCESS_TOKEN = request.META.get('OIDC_access_token')
-    user_info = get_user_info(ACCESS_TOKEN)
-    error_in_user_info = 'error' in user_info
+    # ACCESS_TOKEN = request.META.get('OIDC_access_token')
+    # try:
+    #     user_info = get_user_info(ACCESS_TOKEN)
+    #     error_in_user_info = 'error' in user_info
+    # except:
+    #     pass
 
     # Remove relevant session variables
     remove_login_session_variables(request)
 
-    return render(request, 'index.html', {
-        'title': f'PITHIA e-Science Centre Home',
-        'logout_response': logout_response,
-        'ID_TOKEN_HINT': ID_TOKEN_HINT,
-        'error_in_user_info': error_in_user_info,
-    })
+    return HttpResponseRedirect(reverse('home'))
 
 def index(request):
     return render(request, 'index.html', {
