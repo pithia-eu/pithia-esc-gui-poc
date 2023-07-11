9c74961a32e28e1696a2804855bbdcedee64d4d2
davidchan125 <davidchan125@hotmail.co.uk>
2023-07-11 15:53:29 +0100

    Reset common app views.py

--- common/views.py
+++ common/views.py
@@ -1,22 +1,3 @@
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.utils.decorators import method_decorator
-from django.views.generic import View
-from functools import wraps
+from django.shortcuts import render
 
 # Create your views here.
-# def institution_for_login_session_required(function):
-#     @wraps(function)
-#     def wrap(request, *args, **kwargs):
-#         # if ((request.META.get('OIDC_access_token')
-#         #     or request.session.get('is_logged_in') is True)
-#         #     and 'institution_for_login_session' not in request.session
-#         #     and 'subgroup_for_login_session' not in request.session):
-#         #     return HttpResponseRedirect(reverse('choose_perun_organisation_subgroup_for_session'))
-#         return function(request, *args, **kwargs)
-    
-#     return wrap
-
-# @method_decorator(institution_for_login_session_required, name='dispatch')
-# class LoginInstitutionRequiredView(View):
-#     pass
