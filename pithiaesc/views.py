0fffcf4eb416b211c3b905414e61a028423565d6
davidchan125 <davidchan125@hotmail.co.uk>
2023-07-11 09:59:16 +0100

    Fix decorator issue with index_admin view

--- pithiaesc/views.py
+++ pithiaesc/views.py
@@ -2,9 +2,12 @@ import environ
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.utils.http import urlencode
 
-from common.views import LoginInstitutionRequiredView
+from common.views import (
+    institution_for_login_session_required,
+)
 
 # Initialise environment variables
 env = environ.Env()
@@ -27,7 +30,8 @@ def index(request):
         'title': 'PITHIA e-Science Centre Home',
     })
 
-def index_admin(request, LoginInstitutionRequiredView):
+@method_decorator(institution_for_login_session_required, name='dispatch')
+def index_admin(request):
     return render(request, 'index.html', {
         'title': 'Admin Dashboard',
         'create_perun_organisation_url': CREATION_URL_BASE,
