e19872cffc2c3ae6b320dd4ef0776797f01cd2d0
davidchan125 <davidchan125@hotmail.co.uk>
2023-07-12 13:17:17 +0100

    Refactor LoginMiddleware

--- utils/middleware.py
+++ utils/middleware.py
@@ -37,48 +37,25 @@ class LoginMiddleware(object):
 
         # Check if an OIDC_access_token exists in
         # request.META.
-        access_token = request.META.get('OIDC_access_token')
-        access_token_copy = request.session.get('OIDC_access_token')
+        access_token_in_headers = request.META.get('OIDC_access_token')
+        access_token_in_session = request.session.get('OIDC_access_token')
 
-        if access_token:
-            # If an OIDC_access_token exists, check that
-            # it's matching with the one in the session.
+        if access_token_in_headers:
+            # If an OIDC_access_token exists in the headers,
+            # keep an up-to-date copy of it in the session.
+            # This ensures that any outdated tokens in the
+            # session are overwritten with each request.
+            request.session['OIDC_access_token'] = access_token_in_headers
+            access_token_in_session = access_token_in_headers
 
-            if access_token == access_token_copy:
-                # If the OIDC_access_token in the headers
-                # is the same as the one in the session,
-                # all the login variables should be set
-                # in the session.
-                return response
-
-            # If there isn't an OIDC_access_token in the
-            # session, or, if there is an OIDC_access_token
-            # in the session, assume that it's outdated and
-            # copy the OIDC_access_token from the headers to
-            # the session.
-            request.session['OIDC_access_token'] = access_token
-
-            # Once the session OIDC_access_token has been
-            # overwritten, call the User Info API.
-            self._get_user_info_and_set_login_variables(request, access_token)
+        if access_token_in_session is None:
+            # If there isn't one in the session, can probably
+            # just assume that the user is logged out.
+            remove_login_session_variables(request)
+            if '/authorised' in request.path:
+                return HttpResponseRedirect(reverse('home'))
         else:
-            # If an OIDC_access_token is not in the headers
-            # (e.g., the user is accessing a non-authorised
-            # URL), use the copy in the session.
-
-            if access_token_copy is None:
-                # If there isn't one in the session, can probably
-                # just assume that the user is logged out.
-                remove_login_session_variables(request)
-                if '/authorised' in request.path:
-                    return HttpResponseRedirect(reverse('home'))
-                return response
-
-            # If there is one in the session, verify it is
-            # still valid by calling the User Info API. If
-            # it is not valid, delete the login vars and
-            # log the user out.
-            self._get_user_info_and_set_login_variables(request, access_token_copy)
+            self._get_user_info_and_set_login_variables(request, access_token_in_session)
 
         response = self.get_response(request)
 
