f9e6e39cee0454f4d9c8e23eb6e9afe793f40913
davidchan125 <davidchan125@hotmail.co.uk>
2023-09-05 11:01:54 +0100

    Add STATIC_ROOT property to settings.py

--- pithiaesc/settings.py
+++ pithiaesc/settings.py
@@ -167,6 +167,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = 'staticfiles/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
