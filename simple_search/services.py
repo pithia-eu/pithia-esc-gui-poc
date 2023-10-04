4888e79996f2cfc6e44502e14ba200777b066f5f
davidchan125 <davidchan125@hotmail.co.uk>
2023-10-04 13:48:18 +0100

    Add metadata dependents to simple search - Add more tests for individual simple search components

--- simple_search/services.py
+++ simple_search/services.py
@@ -1,18 +1,65 @@
 from lxml import etree
 
-from common.models import DataCollection
+from common import models
 
-def find_data_collections_for_simple_search(query):
-    data_collections = DataCollection.objects.all().values('id', 'xml')
-    data_collections_with_match = []
-    for dc in data_collections:
-        parsed_xml = etree.fromstring(dc['xml'].encode())
+def find_metadata_registrations_matching_query(query, model):
+    registrations = model.objects.all()
+    registrations_with_match = []
+    for r in registrations:
+        parsed_xml = etree.fromstring(r.xml.encode())
         if parsed_xml.xpath(f'.//*[contains(translate(., "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"), translate("{query}", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"))]'):
             # If partial case-insensitive text match found.
             # Credit: https://stackoverflow.com/a/36427554
-            data_collections_with_match.append(dc)
+            registrations_with_match.append(r)
     
-    return data_collections_with_match
+    return registrations_with_match
+
+def find_metadata_registrations_matching_query_exactly(query, model):
+    registrations = model.objects.all()
+    registrations_with_match = []
+    for r in registrations:
+        parsed_xml = etree.fromstring(r.xml.encode())
+        if parsed_xml.xpath(f'.//*[text()[contains(., "{query}")]]'):
+            registrations_with_match.append(r)
+
+    return registrations_with_match
+
+def get_data_collections_from_metadata_dependents(metadata_dependents):
+    return [md for md in metadata_dependents if md.type_in_metadata_server_url == models.DataCollection.type_in_metadata_server_url]
+
+def get_data_collections_from_other_metadata(registrations):
+    data_collections_found = []
+    for r in registrations:
+        data_collections_from_metadata_dependents = get_data_collections_from_metadata_dependents(r.metadata_dependents)
+        data_collections_found += data_collections_from_metadata_dependents
+    return data_collections_found
+
+def find_data_collections_for_simple_search(query):
+    data_collections_matching_query = []
+
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.Organisation))
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.Individual))
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.Project))
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.Platform))
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.Operation))
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.Instrument))
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.AcquisitionCapabilities))
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.Acquisition))
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.ComputationCapabilities))
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.Computation))
+    data_collections_matching_query += get_data_collections_from_other_metadata(find_metadata_registrations_matching_query(query, models.Process))
+
+    data_collections = find_metadata_registrations_matching_query(query, models.DataCollection)
+    data_collections_matching_query += data_collections
+
+    # Ensure there are no duplicate data collections
+    data_collections_matching_query = {dc.id: dc for dc in data_collections_matching_query}.values()
+
+    print('query', query)
+    print('data_collections_matching_query', data_collections_matching_query)
+    print('')
+    
+    return data_collections_matching_query
 
 def find_data_collections_for_case_sensitive_simple_search(query):
     # https://stackoverflow.com/a/14300008
--- simple_search/tests.py
+++ simple_search/tests.py
@@ -1,9 +1,13 @@
 import os
 from django.test import TestCase
 
-from .services import find_data_collections_for_simple_search
+from .services import (
+    find_data_collections_for_simple_search,
+    find_metadata_registrations_matching_query,
+    find_metadata_registrations_matching_query_exactly,
+)
 
-from common.models import DataCollection
+from common import models
 from pithiaesc.settings import BASE_DIR
 
 
@@ -11,19 +15,83 @@ _XML_METADATA_FILE_DIR = os.path.join(BASE_DIR, 'common', 'test_files', 'xml_met
 
 # Create your tests here.
 class SimpleSearchTestCase(TestCase):
+    def setUp(self) -> None:
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Organisation_Test.xml')) as xml_file:
+            models.Organisation.objects.create_from_xml_string(xml_file.read())
+            
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Individual_Test.xml')) as xml_file:
+            models.Individual.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Project_Test.xml')) as xml_file:
+            models.Project.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Platform_Test.xml')) as xml_file:
+            models.Platform.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Operation_Test.xml')) as xml_file:
+            models.Operation.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Instrument_Test.xml')) as xml_file:
+            models.Instrument.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'AcquisitionCapabilities_Test.xml')) as xml_file:
+            models.AcquisitionCapabilities.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Acquisition_Test.xml')) as xml_file:
+            models.Acquisition.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'Computation_Test.xml')) as xml_file:
+            models.ComputationCapabilities.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'ComputationCapabilities_Test.xml')) as xml_file:
+            models.Computation.objects.create_from_xml_string(xml_file.read())
+
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'CompositeProcess_Test.xml')) as xml_file:
+            models.Process.objects.create_from_xml_string(xml_file.read())
+            
+        with open(os.path.join(_XML_METADATA_FILE_DIR, 'DataCollection_Test.xml')) as xml_file:
+            models.DataCollection.objects.create_from_xml_string(xml_file.read())
+
+        return super().setUp()
+
+    
+    def test_find_metadata_registrations_matching_query(self):
+        """
+        Searches only the given Model's registrations and returns
+        registrations matching the query.
+        """
+        organisations = find_metadata_registrations_matching_query('lorem', models.Organisation)
+        data_collections = find_metadata_registrations_matching_query('lorem', models.DataCollection)
+
+        self.assertEqual(len(organisations), 1)
+        self.assertEqual(len(data_collections), 1)
+
+    def test_find_registrations_matching_query_exactly(self):
+        """
+        Searches on the given Model's registrations and returns
+        registrations matching the query exactly.
+        """
+        data_collections_1 = find_metadata_registrations_matching_query_exactly('Lorem', models.DataCollection)
+        data_collections_2 = find_metadata_registrations_matching_query_exactly('lorem', models.DataCollection)
+        data_collections_3 = find_metadata_registrations_matching_query_exactly('Lorem  ', models.DataCollection)
+        data_collections_4 = find_metadata_registrations_matching_query_exactly('Lorem ', models.DataCollection)
+
+        print('len(data_collections_1)', len(data_collections_1))
+        print('len(data_collections_2)', len(data_collections_2))
+        print('len(data_collections_3)', len(data_collections_3))
+        print('len(data_collections_4)', len(data_collections_4))
+
+        self.assertEqual(len(data_collections_1), 1)
+        self.assertEqual(len(data_collections_2), 0)
+        self.assertEqual(len(data_collections_3), 0)
+
+
     def test_for_simple_search(self):
         """
-        Returns Data Collections matching the
-        simple search query.
+        Performs a simple search returning Data Collections matching
+        the query.
         """
-        with open(os.path.join(_XML_METADATA_FILE_DIR, 'DataCollection_Test.xml')) as xml_file:
-            data_collection = DataCollection.objects.create_from_xml_string(xml_file.read())
-            print('data_collection.name', data_collection.name)
-            
-        with open(os.path.join(_XML_METADATA_FILE_DIR, 'DataCollection_DEMO_Stim.xml')) as xml_file:
-            data_collection = DataCollection.objects.create_from_xml_string(xml_file.read())
-            print('data_collection.name', data_collection.name)
-
+        # Disable "using" parameter in managers.py before running test
         # Case-insensitive match
         data_collections_1 = find_data_collections_for_simple_search('lorem')
 
@@ -43,6 +111,6 @@ class SimpleSearchTestCase(TestCase):
 
         self.assertEqual(len(data_collections_1), 1)
         self.assertEqual(len(data_collections_2), 1)
-        self.assertEqual(len(data_collections_3), 0)
+        self.assertEqual(len(data_collections_3), 1)
         self.assertEqual(len(data_collections_4), 0)
         
