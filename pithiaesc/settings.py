6038d151f94167f1a3133c1c4b3182fe1d775343
David Chan <davidchan125@hotmail.co.uk>
2023-10-18 12:07:29 +0100

    Use WhiteNoise for serving static files

--- pithiaesc/settings.py
+++ pithiaesc/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -169,6 +170,12 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = 'staticfiles/'
+
+STORAGES = {
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
