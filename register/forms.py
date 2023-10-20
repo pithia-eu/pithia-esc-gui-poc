68acfd14abca12c0a9e0bf46d01d739efcd8dd0e
David Chan <davidchan125@hotmail.co.uk>
2023-10-20 10:26:42 +0100

    Fix multiple file input on file upload page

--- register/forms.py
+++ register/forms.py
@@ -1,14 +1,28 @@
 from django import forms
 
 
-class UploadFileForm(forms.Form):
-    files = forms.FileField(
-        label='Upload File(s)',
-        widget=forms.ClearableFileInput(attrs={
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('widget', MultipleFileInput(attrs={
             'accept': 'application/xml',
             'class': 'form-control',
-            'allow_multiple_selected': True,
-        })
+        }))
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+class UploadFileForm(forms.Form):
+    files = MultipleFileField(
+        label='Upload File(s)',
     )
 
 
