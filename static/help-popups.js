cddc5253fce4c0e7aff298869f364d6d5c1623c4
davidchan125 <davidchan125@hotmail.co.uk>
2023-12-07 12:19:42 +0000

    Add home menu help dialogs w/ placeholder text - Refactor card help button code

---
+++ static/help-popups.js
@@ -0,0 +1,39 @@
+function setupDialog(dialog, showButton, closeButton) {
+    showButton.addEventListener("click", () => {
+        dialog.showModal();
+    });
+
+    closeButton.addEventListener("click", () => {
+        dialog.close();
+    });
+}
+
+// Search by content help dialog
+const searchByContentHelpDialog = document.querySelector("#dialog-search-by-content-help");
+const searchByContentHelpDialogShowButton = document.querySelector("#btn-show-dialog-search-by-content-help");
+const searchByContentHelpDialogCloseButton = document.querySelector("#dialog-search-by-content-help .btn-close");
+setupDialog(searchByContentHelpDialog, searchByContentHelpDialogShowButton, searchByContentHelpDialogCloseButton);
+
+// Simple search help dialog
+const simpleSearchHelpDialog = document.querySelector("#dialog-simple-search-help");
+const simpleSearchHelpDialogShowButton = document.querySelector("#btn-show-dialog-simple-search-help");
+const simpleSearchHelpDialogCloseButton = document.querySelector("#dialog-simple-search-help .btn-close");
+setupDialog(simpleSearchHelpDialog, simpleSearchHelpDialogShowButton, simpleSearchHelpDialogCloseButton);
+
+// Data Collections help dialog
+const dataCollectionsHelpDialog = document.querySelector("#dialog-data-collections-help");
+const dataCollectionsHelpDialogShowButton = document.querySelector("#btn-show-dialog-data-collections-help");
+const dataCollectionsHelpDialogCloseButton = document.querySelector("#dialog-data-collections-help .btn-close");
+setupDialog(dataCollectionsHelpDialog, dataCollectionsHelpDialogShowButton, dataCollectionsHelpDialogCloseButton);
+
+// Catalogues help dialog
+const cataloguesHelpDialog = document.querySelector("#dialog-catalogues-help");
+const cataloguesHelpDialogShowButton = document.querySelector("#btn-show-dialog-catalogues-help");
+const cataloguesHelpDialogCloseButton = document.querySelector("#dialog-catalogues-help .btn-close");
+setupDialog(cataloguesHelpDialog, cataloguesHelpDialogShowButton, cataloguesHelpDialogCloseButton);
+
+// All scientific metadata help dialog
+const allScientificMetadataHelpDialog = document.querySelector("#dialog-all-scientific-metadata-help");
+const allScientificMetadataHelpDialogShowButton = document.querySelector("#btn-show-dialog-all-scientific-metadata-help");
+const allScientificMetadataHelpDialogCloseButton = document.querySelector("#dialog-all-scientific-metadata-help .btn-close");
+setupDialog(allScientificMetadataHelpDialog, allScientificMetadataHelpDialogShowButton, allScientificMetadataHelpDialogCloseButton);
