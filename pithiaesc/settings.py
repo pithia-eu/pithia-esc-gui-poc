6cfa2854349716ca87b6b921bf310d77ceecdc9c
davidchan125 <davidchan125@hotmail.co.uk>
2024-01-15 11:08:52 +0000

    Remove workflows app from INSTALLED_APPS

--- pithiaesc/settings.py
+++ pithiaesc/settings.py
@@ -60,7 +60,6 @@ INSTALLED_APPS = [
     'user_management.apps.UserManagementConfig',
     'utils.apps.UtilsConfig',
     'validation.apps.ValidationConfig',
-    'workflows.apps.WorkflowsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
