b186f06c7d55410e62a5017e900016783e866d72
davidchan125 <davidchan125@hotmail.co.uk>
2024-03-06 16:49:13 +0000

    Fix local ID in form submission

--- register/static/register/no_file_register_form.js
+++ register/static/register/no_file_register_form.js
@@ -1,4 +1,6 @@
+const inputSupportForm = document.getElementById("metadata-input-support-form");
 const input = document.querySelector("#id_phone");
+
 window.intlTelInput(input, {
     allowDropdown: false,
     autoPlaceholder: false,
@@ -7,4 +9,9 @@ window.intlTelInput(input, {
     placeholderNumberType: "FIXED_LINE",
     showFlags: false,
     utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@19.5.4/build/js/utils.js",
+});
+
+inputSupportForm.addEventListener("submit", () => {
+    const localIdInput = document.querySelector("input[name='localid']");
+    localIdInput.disabled = false;
 });
