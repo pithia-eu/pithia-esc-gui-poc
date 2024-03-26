295170303b2ab10c468d234f3a1aded1352504c5
David Chan <davidchan125@hotmail.co.uk>
2024-03-26 18:20:09 +0000

    Update RP and KW table dynamic field code - Update JS code to fix duplication of element IDs - Start update of RP table JS code

--- register_with_support/static/register_with_support/project/keywords_table.js
+++ register_with_support/static/register_with_support/project/keywords_table.js
@@ -23,83 +23,116 @@ function getNumRemainingKeywordRows() {
     return keywordsTableBody.querySelectorAll("tr").length;
 }
 
+
+function removeKeyword(event) {
+    const removeKeywordButton = event.currentTarget;
+    const containingTableRow = getKeywordTableRowByChildNode(removeKeywordButton);
+    const keywordsListElement = containingTableRow.querySelector("ul");
+    const keywordLiElements = keywordsListElement.querySelectorAll("li");
+    for (const liElement of keywordLiElements) {
+        if (liElement.contains(removeKeywordButton)) {
+            keywordsListElement.removeChild(liElement);
+            break;
+        }
+    }
+    const remainingKeywordInputsInRow = getNumRemainingKeywordInputsOfRow(containingTableRow);
+    if (remainingKeywordInputsInRow === 1) {
+        containingTableRow.querySelector(".td-keywords .remove-kw-button").disabled = true;
+    }
+}
+
 function setupRemoveKeywordButton(removeKeywordButton) {
-    removeKeywordButton.addEventListener("click", () => {
-        const containingTableRow = getKeywordTableRowByChildNode(removeKeywordButton);
-        const keywordsListElement = containingTableRow.querySelector("ul");
-        const keywordLiElements = keywordsListElement.querySelectorAll("li");
-        for (const liElement of keywordLiElements) {
-            if (liElement.contains(removeKeywordButton)) {
-                keywordsListElement.removeChild(liElement);
-                break;
-            }
-        }
-        const remainingKeywordInputsInRow = getNumRemainingKeywordInputsOfRow(containingTableRow);
-        if (remainingKeywordInputsInRow === 1) {
-            containingTableRow.querySelector(".td-keywords .remove-kw-button").disabled = true;
-        }
-    });
+    removeKeywordButton.addEventListener("click", removeKeyword);
+}
+
+function addKeyword(event) {
+    const addKeywordButton = event.currentTarget;
+    const containingTableRow = getKeywordTableRowByChildNode(addKeywordButton);
+    const keywordsListElement = containingTableRow.querySelector("ul");
+    const firstKeywordLiElement = keywordsListElement.querySelector("li");
+    const newKeywordLiElement = document.createElement("li");
+    newKeywordLiElement.classList = firstKeywordLiElement.classList;
+    newKeywordLiElement.innerHTML = firstKeywordLiElement.innerHTML;
+    const removeKeywordButton = newKeywordLiElement.querySelector(".remove-kw-button");
+    removeKeywordButton.disabled = false;
+    setupRemoveKeywordButton(removeKeywordButton);
+    keywordsListElement.appendChild(newKeywordLiElement);
+    const remainingKeywordInputsInRow = getNumRemainingKeywordInputsOfRow(containingTableRow);
+    if (remainingKeywordInputsInRow > 1) {
+        containingTableRow.querySelector(".td-keywords .remove-kw-button").disabled = false;
+    }
 }
 
 function setupAddKeywordButton(addKeywordButton) {
-    addKeywordButton.addEventListener("click", () => {
-        const containingTableRow = getKeywordTableRowByChildNode(addKeywordButton);
-        const keywordsListElement = containingTableRow.querySelector("ul");
-        const firstKeywordLiElement = keywordsListElement.querySelector("li");
-        const newKeywordLiElement = document.createElement("li");
-        newKeywordLiElement.classList = firstKeywordLiElement.classList;
-        newKeywordLiElement.innerHTML = firstKeywordLiElement.innerHTML;
-        const removeKeywordButton = newKeywordLiElement.querySelector(".remove-kw-button");
-        removeKeywordButton.disabled = false;
-        setupRemoveKeywordButton(removeKeywordButton);
-        keywordsListElement.appendChild(newKeywordLiElement);
-        const remainingKeywordInputsInRow = getNumRemainingKeywordInputsOfRow(containingTableRow);
-        if (remainingKeywordInputsInRow > 1) {
-            containingTableRow.querySelector(".td-keywords .remove-kw-button").disabled = false;
-        }
-    });
+    addKeywordButton.addEventListener("click", addKeyword);
+}
+
+function removeKeywordsRow(event) {
+    const removeKeywordsRowButton = event.currentTarget;
+    const containingTableRow = getKeywordTableRowByChildNode(removeKeywordsRowButton);
+    keywordsTableBody.removeChild(containingTableRow);
+    const numRemainingTableRows = getNumRemainingKeywordRows();
+    if (numRemainingTableRows === 1) {
+        const firstRemoveRowButton = keywordsTableBody.querySelector(".remove-kwrow-button");
+        firstRemoveRowButton.disabled = true;
+    }
 }
 
 function setupRemoveKeywordsRowButton(removeKeywordsRowButton) {
-    removeKeywordsRowButton.addEventListener("click", () => {
-        const containingTableRow = getKeywordTableRowByChildNode(removeKeywordsRowButton);
-        keywordsTableBody.removeChild(containingTableRow);
-        const numRemainingTableRows = getNumRemainingKeywordRows();
-        if (numRemainingTableRows === 1) {
-            const firstRemoveRowButton = keywordsTableBody.querySelector(".remove-kwrow-button");
-            firstRemoveRowButton.disabled = true;
-        }
+    removeKeywordsRowButton.addEventListener("click", removeKeywordsRow);
+}
+
+function addKeywordsRow(event) {
+    // Create a new table row by copying
+    // the first table row.
+    const keywordsTableFirstRow = keywordsTableBody.querySelector("tr");
+    const newRow = document.createElement("TR");
+    newRow.innerHTML = keywordsTableFirstRow.innerHTML;
+    const newRowKeywordsList = newRow.querySelector("td.td-keywords ul");
+    if (newRowKeywordsList.querySelectorAll("li").length > 0) {
+        newRowKeywordsList.innerHTML = newRowKeywordsList.querySelector("li").outerHTML;
+    }
+    const newRowHighlightedInputs = newRow.querySelectorAll("input.was-validated");
+    newRowHighlightedInputs.forEach(input => {
+        input.classList.remove("was-validated");
+        input.classList.remove("is-invalid");
     });
+
+    // Ensure the IDs of any child elements are
+    // unique.
+    const childElementsWithIdAttribute = newRow.querySelectorAll("[id]");
+    childElementsWithIdAttribute.forEach(elem => {
+        const newId = `${elem.id}${Date.now()}`;
+        const correspondingLabels = newRow.querySelectorAll(`label[for="${elem.id}"]`)
+        elem.id = newId;
+        correspondingLabels.forEach(label => {
+            label.htmlFor = newId;
+        });
+    });
+
+    // Setup each of the row's buttons
+    const addKeywordButton = newRow.querySelector(".add-kw-button");
+    setupAddKeywordButton(addKeywordButton);
+    const removeKeywordButton = newRow.querySelector(".remove-kw-button");
+    setupRemoveKeywordButton(removeKeywordButton);
+    const removeKeywordsRowButton = newRow.querySelector(".remove-kwrow-button");
+    removeKeywordsRowButton.disabled = false;
+    setupRemoveKeywordsRowButton(removeKeywordsRowButton);
+
+    // Add the row to the table
+    keywordsTableBody.appendChild(newRow);
+
+    // Enable the first row's remove button
+    // if it is not already enabled.
+    const numRemainingTableRows = getNumRemainingKeywordRows();
+    if (numRemainingTableRows > 1) {
+        const firstRemoveRowButton = keywordsTableBody.querySelector(".remove-kwrow-button");
+        firstRemoveRowButton.disabled = false;
+    }
 }
 
 function setupAddKeywordsRowButton() {
-    addKeywordsRowButton.addEventListener("click", () => {
-        const keywordsTableFirstRow = keywordsTableBody.querySelector("tr");
-        const newRow = document.createElement("TR");
-        newRow.innerHTML = keywordsTableFirstRow.innerHTML;
-        const newRowKeywordsList = newRow.querySelector("td.td-keywords ul");
-        if (newRowKeywordsList.querySelectorAll("li").length > 0) {
-            newRowKeywordsList.innerHTML = newRowKeywordsList.querySelector("li").outerHTML;
-        }
-        const newRowHighlightedInputs = newRow.querySelectorAll("input.was-validated");
-        newRowHighlightedInputs.forEach(input => {
-            input.classList.remove("was-validated");
-            input.classList.remove("is-invalid");
-        });
-        const addKeywordButton = newRow.querySelector(".add-kw-button");
-        setupAddKeywordButton(addKeywordButton);
-        const removeKeywordButton = newRow.querySelector(".remove-kw-button");
-        setupRemoveKeywordButton(removeKeywordButton);
-        const removeKeywordsRowButton = newRow.querySelector(".remove-kwrow-button");
-        removeKeywordsRowButton.disabled = false;
-        setupRemoveKeywordsRowButton(removeKeywordsRowButton);
-        keywordsTableBody.appendChild(newRow);
-        const numRemainingTableRows = getNumRemainingKeywordRows();
-        if (numRemainingTableRows > 1) {
-            const firstRemoveRowButton = keywordsTableBody.querySelector(".remove-kwrow-button");
-            firstRemoveRowButton.disabled = false;
-        }
-    });
+    addKeywordsRowButton.addEventListener("click", addKeywordsRow);
 }
 
 export function setupKeywordsTable() {
--- register_with_support/static/register_with_support/project/related_parties_table.js
+++ register_with_support/static/register_with_support/project/related_parties_table.js
@@ -23,83 +23,138 @@ function getNumRemainingRelatedPartyRoles() {
     return relatedPartiesTableBody.querySelectorAll("tr").length;
 }
 
+function removeRelatedParty(event) {
+    const removeRelatedPartyButton = event.currentTarget;
+    const containingTableRow = getRelatedPartiesTableRowByChildNode(removeRelatedPartyButton);
+    const relatedPartiesListElement = containingTableRow.querySelector("ul");
+    const relatedPartyLiElements = relatedPartiesListElement.querySelectorAll("li");
+    for (const liElement of relatedPartyLiElements) {
+        if (liElement.contains(removeRelatedPartyButton)) {
+            relatedPartiesListElement.removeChild(liElement);
+            break;
+        }
+    }
+    const remainingRelatedPartySelectsInRow = getNumRemainingRelatedPartySelectsOfRow(containingTableRow);
+    if (remainingRelatedPartySelectsInRow === 1) {
+        containingTableRow.querySelector(".td-related-parties .remove-rp-button").disabled = true;
+    }
+}
+
 function setupRemoveRelatedPartyButton(removeRelatedPartyButton) {
-    removeRelatedPartyButton.addEventListener("click", () => {
-        const containingTableRow = getRelatedPartiesTableRowByChildNode(removeRelatedPartyButton);
-        const relatedPartiesListElement = containingTableRow.querySelector("ul");
-        const relatedPartyLiElements = relatedPartiesListElement.querySelectorAll("li");
-        for (const liElement of relatedPartyLiElements) {
-            if (liElement.contains(removeRelatedPartyButton)) {
-                relatedPartiesListElement.removeChild(liElement);
-                break;
-            }
-        }
-        const remainingRelatedPartySelectsInRow = getNumRemainingRelatedPartySelectsOfRow(containingTableRow);
-        if (remainingRelatedPartySelectsInRow === 1) {
-            containingTableRow.querySelector(".td-related-parties .remove-rp-button").disabled = true;
-        }
+    removeRelatedPartyButton.addEventListener("click", removeRelatedParty);
+}
+
+function addRelatedParty(event) {
+    // Create a new related party input by
+    // copying the first one in the list.
+    const addRelatedPartyButton = event.currentTarget;
+    const containingTableRow = getRelatedPartiesTableRowByChildNode(addRelatedPartyButton);
+    const relatedPartiesListElement = containingTableRow.querySelector("ul");
+    const firstRelatedPartyLiElement = relatedPartiesListElement.querySelector("li");
+    const newRelatedPartyLiElement = document.createElement("li");
+    newRelatedPartyLiElement.classList = firstRelatedPartyLiElement.classList;
+    newRelatedPartyLiElement.innerHTML = firstRelatedPartyLiElement.innerHTML;
+    
+    // Update any IDs that are duplicated
+    const childElementsWithIdAttribute = newRelatedPartyLiElement.querySelectorAll("[id]");
+    childElementsWithIdAttribute.forEach(elem => {
+        const newId = `${elem.id}${Date.now()}`;
+        const relatedPartyLabel = containingTableRow.querySelector(`label[for="${elem.id}"]`);
+        elem.id = newId;
+        elem.setAttribute("aria-label", relatedPartyLabel.innerHTML);
     });
+
+    // Enable the remove button if it's
+    // disabled as there's more than one
+    // related party input now.
+    const removeRelatedPartyButton = newRelatedPartyLiElement.querySelector(".remove-rp-button");
+    removeRelatedPartyButton.disabled = false;
+    // Setup the remove button
+    setupRemoveRelatedPartyButton(removeRelatedPartyButton);
+    // Add the new related party input
+    relatedPartiesListElement.appendChild(newRelatedPartyLiElement);
+    
+    // Enable the first select menu's remove
+    // button if it's not already enabled.
+    const remainingRelatedPartySelectsInRow = getNumRemainingRelatedPartySelectsOfRow(containingTableRow);
+    if (remainingRelatedPartySelectsInRow > 1) {
+        containingTableRow.querySelector(".td-related-parties .remove-rp-button").disabled = false;
+    }
 }
 
 function setupAddRelatedPartyButton(addRelatedPartyButton) {
-    addRelatedPartyButton.addEventListener("click", () => {
-        const containingTableRow = getRelatedPartiesTableRowByChildNode(addRelatedPartyButton);
-        const relatedPartiesListElement = containingTableRow.querySelector("ul");
-        const firstRelatedPartyLiElement = relatedPartiesListElement.querySelector("li");
-        const newRelatedPartyLiElement = document.createElement("li");
-        newRelatedPartyLiElement.classList = firstRelatedPartyLiElement.classList;
-        newRelatedPartyLiElement.innerHTML = firstRelatedPartyLiElement.innerHTML;
-        const removeRelatedPartyButton = newRelatedPartyLiElement.querySelector(".remove-rp-button");
-        removeRelatedPartyButton.disabled = false;
-        setupRemoveRelatedPartyButton(removeRelatedPartyButton);
-        relatedPartiesListElement.appendChild(newRelatedPartyLiElement);
-        const remainingRelatedPartySelectsInRow = getNumRemainingRelatedPartySelectsOfRow(containingTableRow);
-        if (remainingRelatedPartySelectsInRow > 1) {
-            containingTableRow.querySelector(".td-related-parties .remove-rp-button").disabled = false;
-        }
-    });
+    addRelatedPartyButton.addEventListener("click", addRelatedParty);
+}
+
+function removeRelatedPartyRole(event) {
+    const removeRelatedPartyRoleButton = event.currentTarget;
+    const containingTableRow = getRelatedPartiesTableRowByChildNode(removeRelatedPartyRoleButton);
+    relatedPartiesTableBody.removeChild(containingTableRow);
+    const numRemainingTableRows = getNumRemainingRelatedPartyRoles();
+    if (numRemainingTableRows === 1) {
+        const firstRemoveRowButton = relatedPartiesTableBody.querySelector(".remove-rprrow-button");
+        firstRemoveRowButton.disabled = true;
+    }
 }
 
 function setupRemoveRelatedPartyRoleButton(removeRelatedPartyRoleButton) {
-    removeRelatedPartyRoleButton.addEventListener("click", () => {
-        const containingTableRow = getRelatedPartiesTableRowByChildNode(removeRelatedPartyRoleButton);
-        relatedPartiesTableBody.removeChild(containingTableRow);
-        const numRemainingTableRows = getNumRemainingRelatedPartyRoles();
-        if (numRemainingTableRows === 1) {
-            const firstRemoveRowButton = relatedPartiesTableBody.querySelector(".remove-rprrow-button");
-            firstRemoveRowButton.disabled = true;
-        }
+    removeRelatedPartyRoleButton.addEventListener("click", removeRelatedPartyRole);
+}
+
+function addRelatedPartyRole(event) {
+    // Create new table row by copying HTML of
+    // first table row.
+    const relatedPartiesTableFirstRow = relatedPartiesTableBody.querySelector("tr");
+    const newRow = document.createElement("TR");
+    newRow.innerHTML = relatedPartiesTableFirstRow.innerHTML;
+    
+    // Reset any child elements to their default
+    // states if not already.
+    const newRowRelatedPartiesList = newRow.querySelector("td.td-related-parties ul");
+    if (newRowRelatedPartiesList.querySelectorAll("li").length > 0) {
+        newRowRelatedPartiesList.innerHTML = newRowRelatedPartiesList.querySelector("li").outerHTML;
+    }
+    const newRowHighlightedInputs = newRow.querySelectorAll("input.was-validated");
+    newRowHighlightedInputs.forEach(input => {
+        input.classList.remove("was-validated");
+        input.classList.remove("is-invalid");
     });
+
+    // Ensure the IDs of any child elements are
+    // unique.
+    const childElementsWithIdAttribute = newRow.querySelectorAll("[id]");
+    childElementsWithIdAttribute.forEach(elem => {
+        const newId = `${elem.id}${Date.now()}`;
+        const correspondingLabels = newRow.querySelectorAll(`label[for="${elem.id}"]`)
+        elem.id = newId;
+        correspondingLabels.forEach(label => {
+            label.htmlFor = newId;
+        });
+    });
+    
+    // Set up each of the buttons in the row
+    const addRelatedPartyButton = newRow.querySelector(".add-rp-button");
+    setupAddRelatedPartyButton(addRelatedPartyButton);
+    const removeRelatedPartyButton = newRow.querySelector(".remove-rp-button");
+    setupRemoveRelatedPartyButton(removeRelatedPartyButton);
+    const removeRelatedPartyRoleButton = newRow.querySelector(".remove-rprrow-button");
+    removeRelatedPartyRoleButton.disabled = false;
+    setupRemoveRelatedPartyRoleButton(removeRelatedPartyRoleButton);
+
+    // Add the row to the table
+    relatedPartiesTableBody.appendChild(newRow);
+
+    // Enable the first row's remove button if
+    // it's not already enabled.
+    const numRemainingTableRows = getNumRemainingRelatedPartyRoles();
+    if (numRemainingTableRows > 1) {
+        const firstRemoveRoleButton = relatedPartiesTableBody.querySelector(".remove-rprrow-button");
+        firstRemoveRoleButton.disabled = false;
+    }
 }
 
 function setupAddRelatedPartyRoleButton() {
-    addRelatedPartyRoleButton.addEventListener("click", () => {
-        const relatedPartiesTableFirstRow = relatedPartiesTableBody.querySelector("tr");
-        const newRow = document.createElement("TR");
-        newRow.innerHTML = relatedPartiesTableFirstRow.innerHTML;
-        const newRowRelatedPartiesList = newRow.querySelector("td.td-related-parties ul");
-        if (newRowRelatedPartiesList.querySelectorAll("li").length > 0) {
-            newRowRelatedPartiesList.innerHTML = newRowRelatedPartiesList.querySelector("li").outerHTML;
-        }
-        const newRowHighlightedInputs = newRow.querySelectorAll("input.was-validated");
-        newRowHighlightedInputs.forEach(input => {
-            input.classList.remove("was-validated");
-            input.classList.remove("is-invalid");
-        });
-        const addRelatedPartyButton = newRow.querySelector(".add-rp-button");
-        setupAddRelatedPartyButton(addRelatedPartyButton);
-        const removeRelatedPartyButton = newRow.querySelector(".remove-rp-button");
-        setupRemoveRelatedPartyButton(removeRelatedPartyButton);
-        const removeRelatedPartyRoleButton = newRow.querySelector(".remove-rprrow-button");
-        removeRelatedPartyRoleButton.disabled = false;
-        setupRemoveRelatedPartyRoleButton(removeRelatedPartyRoleButton);
-        relatedPartiesTableBody.appendChild(newRow);
-        const numRemainingTableRows = getNumRemainingRelatedPartyRoles();
-        if (numRemainingTableRows > 1) {
-            const firstRemoveRoleButton = relatedPartiesTableBody.querySelector(".remove-rprrow-button");
-            firstRemoveRoleButton.disabled = false;
-        }
-    });
+    addRelatedPartyRoleButton.addEventListener("click", addRelatedPartyRole);
 }
 
 export function setupRelatedPartiesTable() {
