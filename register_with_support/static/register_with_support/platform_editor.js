86374b263b58b6a5389b90e04cf3b64ff6be0efc
davidchan125 <davidchan125@hotmail.co.uk>
2024-03-27 14:54:23 +0000

    Add platform editor JS file

---
+++ register_with_support/static/register_with_support/platform_editor.js
@@ -0,0 +1,38 @@
+import {
+    setupLocalIdAndNamespaceRelatedEventListeners,
+} from "/static/register_with_support/components/localid_validation.js";
+import {
+    editorForm,
+    validateAndRegister,
+} from "/static/register_with_support/components/base_editor.js";
+import {
+    prepareRelatedPartiesJSON,
+} from "/static/register_with_support/components/json_field_processing.js";
+import {
+    setupRelatedPartiesTable,
+} from "/static/register_with_support/components/related_parties_table.js";
+import {
+    checkRelatedPartiesSectionValidity,
+} from "/static/register_with_support/components/additional_form_validation.js";
+
+function prepareFormForSubmission() {
+    prepareRelatedPartiesJSON();
+}
+
+editorForm.addEventListener("submit", async e => {
+    e.preventDefault();
+
+    // Run custom form validation
+    if (!checkRelatedPartiesSectionValidity()) {
+        return false;
+    }
+
+    prepareFormForSubmission();
+
+    validateAndRegister();
+});
+
+window.addEventListener("load", () => {
+    setupLocalIdAndNamespaceRelatedEventListeners();
+    setupRelatedPartiesTable();
+});
