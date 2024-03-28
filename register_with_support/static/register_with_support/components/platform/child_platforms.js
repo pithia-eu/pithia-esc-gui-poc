e2aec2de1400a92c46bd6100573617c86265c745
davidchan125 <davidchan125@hotmail.co.uk>
2024-03-28 11:52:35 +0000

    Update child platforms field in project editor

--- register_with_support/static/register_with_support/components/platform/child_platforms.js
+++ register_with_support/static/register_with_support/components/platform/child_platforms.js
@@ -5,12 +5,31 @@ import {
 const addChildPlatformButton = editorForm.querySelector("#add-cp-button");
 const childPlatformsList = editorForm.querySelector("#list-child-platforms");
 
-function removeChildPlatform() {
+function getLiElementByChildNode(childNode) {
+    const childPlatformListItems = childPlatformsList.querySelectorAll("li");
+    for (const listItem of childPlatformListItems) {
+        if (listItem.contains(childNode)) {
+            return listItem;
+        }
+    }
+}
 
+function getRemainingChildPlatformsInList() {
+    return childPlatformsList.querySelectorAll("li").length;
+}
+
+function removeChildPlatform(event) {
+    const removeChildPlatformButton = event.currentTarget;
+    const containingLiElement = getLiElementByChildNode(removeChildPlatformButton);
+    childPlatformsList.removeChild(containingLiElement);
+    if (getRemainingChildPlatformsInList() === 1) {
+        const firstRemoveChildPlatformButton = childPlatformsList.querySelector("li .remove-cp-button");
+        firstRemoveChildPlatformButton.disabled = true;
+    }
 }
 
 function setupRemoveChildPlatformButton(removeChildPlatformButton) {
-    removeChildPlatformButton.addEventListener("click", removeChildPlatformButton);
+    removeChildPlatformButton.addEventListener("click", removeChildPlatform);
 }
 
 function addChildPlatform() {
@@ -20,8 +39,13 @@ function addChildPlatform() {
     newLiElement.outerHTML = firstCpLiElement.outerHTML;
     newLiElement.innerHTML = firstCpLiElement.innerHTML;
     const removeChildPlatformButton = newLiElement.querySelector(".remove-cp-button");
+    removeChildPlatformButton.disabled = false;
     setupRemoveChildPlatformButton(removeChildPlatformButton);
     childPlatformsList.appendChild(newLiElement);
+    if (getRemainingChildPlatformsInList() > 1) {
+        const firstRemoveChildPlatformButton = childPlatformsList.querySelector("li .remove-cp-button");
+        firstRemoveChildPlatformButton.disabled = false;
+    }
 }
 
 function setupAddChildPlatformButton() {
@@ -33,5 +57,8 @@ export function setupChildPlatformsList() {
     removeChildPlatformButtons.forEach(btn => {
         setupRemoveChildPlatformButton(btn);
     });
+    if (removeChildPlatformButtons.length === 1) {
+        removeChildPlatformButtons[0].disabled = true;
+    }
     setupAddChildPlatformButton();
 }
