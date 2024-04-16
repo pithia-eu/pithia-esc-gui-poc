bb13f096cc0500c799524637db2d0b231f6f8528
davidchan125 <davidchan125@hotmail.co.uk>
2024-04-16 09:14:44 +0100

    Add missing schemaLocation attr to metadata builder - Increase metadata builder indentation size from 2 to 4

--- register_with_support/metadata_builder/metadata_structure_components.py
+++ register_with_support/metadata_builder/metadata_structure_components.py
@@ -12,6 +12,7 @@ class NamespacePrefix:
     OM = 'om'
     PITHIA = None
     XLINK = 'xlink'
+    XSI = 'xsi'
 
 class Namespace:
     GCO = 'http://www.isotc211.org/2005/gco'
@@ -20,14 +21,23 @@ class Namespace:
     OM = 'http://www.opengis.net/om/2.0'
     PITHIA = 'https://metadata.pithia.eu/schemas/2.2'
     XLINK = 'http://www.w3.org/1999/xlink'
+    XSI = 'http://www.w3.org/2001/XMLSchema-instance'
 
 
 class BaseMetadataComponent:
     def __init__(self, root_tag, nsmap_extensions={}) -> None:
-        self.root = etree.Element(root_tag, nsmap={
-            NamespacePrefix.PITHIA: Namespace.PITHIA,
-            **nsmap_extensions,
-        })
+        root_element_attributes = {
+            '{%s}schemaLocation' % Namespace.XSI: 'https://metadata.pithia.eu/schemas/2.2 https://metadata.pithia.eu/schemas/2.2/pithia.xsd'
+        }
+        self.root = etree.Element(
+            root_tag, 
+            **root_element_attributes,
+            nsmap={
+                NamespacePrefix.PITHIA: Namespace.PITHIA,
+                NamespacePrefix.XSI: Namespace.XSI,
+                **nsmap_extensions,
+            }
+        )
 
     def append_identifier(self, localid, namespace, version='1', creation_date=datetime.now(timezone.utc).replace(second=0, microsecond=0).isoformat().replace('+00:00', 'Z'), last_modification_date=datetime.now(timezone.utc).replace(second=0, microsecond=0).isoformat().replace('+00:00', 'Z')):
         # Container elements
@@ -54,6 +64,7 @@ class BaseMetadataComponent:
 
     @property
     def xml(self) -> str:
+        etree.indent(self.root, space="    ")
         return etree.tostring(self.root, pretty_print=True).decode()
 
 
