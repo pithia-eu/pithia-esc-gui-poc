952a51664f3c13004bed74a3e733e85633269075
davidchan125 <davidchan125@hotmail.co.uk>
2024-05-14 11:29:09 +0100

    Add local ID auto-generation to acquisition editor

--- register_with_support/static/register_with_support/acquisition_editor.js
+++ register_with_support/static/register_with_support/acquisition_editor.js
@@ -1,8 +1,21 @@
 import {
+    editorForm,
+    validateAndRegister,
+} from "/static/register_with_support/components/base_editor.js";
+import {
+    setupLocalIdAndNamespaceRelatedEventListeners,
+} from "/static/register_with_support/components/localid_validation.js";
+import {
     setupCapabilityLinksTab,
 } from "/static/register_with_support/components/acquisition_and_computation/capability_links_tab.js";
 
+editorForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    validateAndRegister();
+});
 
 window.addEventListener("load", () => {
+    setupLocalIdAndNamespaceRelatedEventListeners();
     const capabilityLinksTab = setupCapabilityLinksTab();
 });
