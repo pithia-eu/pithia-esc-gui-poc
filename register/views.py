96083b7fd4758fe1897578c23faa87e1d51f4ec8
davidchan125 <davidchan125@hotmail.co.uk>
2024-06-06 10:40:09 +0100

    Add feedback when registering API w/ data collection

--- register/views.py
+++ register/views.py
@@ -244,6 +244,7 @@ class DataCollectionRegisterFormView(ResourceRegisterFormView):
                 api_description,
                 self.new_registration
             )
+            messages.success(request, f'<p>Added an API interaction method for {self.new_registration.name}.</p><p class="mb-0">It can be viewed and/or updated from the <a href="{reverse_lazy("update:data_collection_interaction_methods", kwargs={"resource_id": self.new_registration.pk})}">interaction methods page</a> for this data collection.</p>')
         except BaseException as err:
             logger.exception('An unexpected error occurred during API interaction method registration.')
             messages.error(request, 'An unexpected error occurred during API interaction method registration.')
