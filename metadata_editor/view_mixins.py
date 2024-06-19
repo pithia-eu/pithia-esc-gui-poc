f18cabb552859cc77e8c8d4fb697be785af29d63
davidchan125 <davidchan125@hotmail.co.uk>
2024-06-19 15:21:23 +0100

    Refactor metadata_editor view_mixins.py

--- metadata_editor/view_mixins.py
+++ metadata_editor/view_mixins.py
@@ -9,198 +9,124 @@ from common.helpers import clean_localid_or_namespace
 from ontology.utils import get_graph_of_pithia_ontology_component
 
 
-class CapabilitiesSelectFormViewMixin(View):
-    def get_observed_property_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('observedProperty')
-        observed_property_dict = {}
+class OntologyCategoryChoicesViewMixin:
+    def get_terms_from_ontology_category(self, ontology_category):
+        g = get_graph_of_pithia_ontology_component(ontology_category)
+        term_dict = {}
         for s, p, o in g.triples((None, SKOS.member, None)):
             o_pref_label = g.value(o, SKOS.prefLabel)
-            observed_property_dict[str(o)] = str(o_pref_label)
+            term_dict[str(o)] = str(o_pref_label)
+        return [(key, value) for key, value in term_dict.items()]
+
+    def get_choices_from_ontology_category(self, ontology_category):
         return (
             ('', ''),
-            *[(key, value) for key, value in observed_property_dict.items()],
+            *self.get_terms_from_ontology_category(ontology_category),
         )
+
+    def get_choices_from_multiple_ontology_categories(self, optgroup_oc_pairs):
+        choices_categorised = []
+        for optgroup, ontology_category in optgroup_oc_pairs:
+            choices_categorised.append(
+                (optgroup, list((key, value) for key, value in self.get_terms_from_ontology_category(ontology_category)))
+            )
+        return (
+            ('', ''),
+            *choices_categorised
+        )
+
+class ResourceChoicesViewMixin:
+    def get_resources_with_model_ordered_by_name(self, model):
+        return model.objects.annotate(json_name=KeyTextTransform('name', 'json')).all().order_by(Lower('json_name'))
+
+    def get_resource_choices_with_model(self, model):
+        return (
+            ('', ''),
+            *[(r.metadata_server_url, r.name) for r in self.get_resources_with_model_ordered_by_name(model)],
+        )
+
+    def get_resource_choices_with_multiple_models(self, optgroup_model_pairs):
+        choices_categorised = []
+        for optgroup, model in optgroup_model_pairs:
+            choices_categorised.append(
+                (optgroup, list((r.metadata_server_url, r.name) for r in self.get_resources_with_model_ordered_by_name(model)))
+            )
+        return (
+            ('', ''),
+            *choices_categorised
+        )
+
+class CapabilitiesSelectFormViewMixin(OntologyCategoryChoicesViewMixin):
+    def get_observed_property_choices_for_form(self):
+        return self.get_choices_from_ontology_category('observedProperty')
 
     def get_coordinate_system_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('crs')
-        crs_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            crs_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *[(key, value) for key, value in crs_dict.items()],
-        )
+        return self.get_choices_from_ontology_category('crs')
 
     def get_dimensionality_instance_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('dimensionalityInstance')
-        dimensionality_instance_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            dimensionality_instance_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *[(key, value) for key, value in dimensionality_instance_dict.items()],
-        )
+        return self.get_choices_from_ontology_category('dimensionalityInstance')
 
     def get_dimensionality_timeline_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('dimensionalityTimeline')
-        dimensionality_timeline_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            dimensionality_timeline_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *[(key, value) for key, value in dimensionality_timeline_dict.items()],
-        )
+        return self.get_choices_from_ontology_category('dimensionalityTimeline')
 
     def get_qualifier_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('qualifier')
-        qualifier_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            qualifier_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *[(key, value) for key, value in qualifier_dict.items()],
-        )
+        return self.get_choices_from_ontology_category('qualifier')
 
     def get_unit_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('unit')
-        unit_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            unit_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *[(key, value) for key, value in unit_dict.items()],
-        )
+        return self.get_choices_from_ontology_category('unit')
 
     def get_vector_representation_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('component')
-        component_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            component_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *[(key, value) for key, value in component_dict.items()],
-        )
+        return self.get_choices_from_ontology_category('component')
 
-class DataLevelSelectFormViewMixin(View):
+class DataLevelSelectFormViewMixin(OntologyCategoryChoicesViewMixin):
     def get_data_level_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('dataLevel')
-        data_level_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            data_level_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *[(key, value) for key, value in data_level_dict.items()],
-        )
+        return self.get_choices_from_ontology_category('dataLevel')
 
-class SrsNameSelectFormViewMixin(View):
+class SrsNameSelectFormViewMixin(OntologyCategoryChoicesViewMixin):
     def get_crs_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('crs')
-        crs_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            crs_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *[(key, value) for key, value in crs_dict.items()],
-        )
+        return self.get_choices_from_ontology_category('crs')
 
-class OrganisationSelectFormViewMixin(View):
+class OrganisationSelectFormViewMixin(ResourceChoicesViewMixin):
     def get_organisation_choices_for_form(self):
         return (
             ('', ''),
-            *[(o.metadata_server_url, f'{o.name} ({clean_localid_or_namespace(o.short_name.lower())})') for o in models.Organisation.objects.annotate(json_name=KeyTextTransform('name', 'json')).all().order_by(Lower('json_name'))],
+            *[(o.metadata_server_url, f'{o.name} ({clean_localid_or_namespace(o.short_name.lower())})') for o in self.get_resources_with_model_ordered_by_name(models.Organisation)],
         )
 
-class PlatformSelectFormViewMixin(View):
+class PlatformSelectFormViewMixin(ResourceChoicesViewMixin):
     def get_platform_choices_for_form(self):
-        return (
-            ('', ''),
-            *[(p.metadata_server_url, p.name) for p in models.Platform.objects.annotate(json_name=KeyTextTransform('name', 'json')).all().order_by(Lower('json_name'))],
-        )
+        return self.get_resource_choices_with_model(models.Platform)
 
-class DataCollectionSelectFormViewMixin(View):
+class DataCollectionSelectFormViewMixin(ResourceChoicesViewMixin):
     def get_data_collection_choices_for_form(self):
-        return (
-            ('', ''),
-            *[(data_collection.metadata_server_url, data_collection.name) for data_collection in models.DataCollection.objects.annotate(json_name=KeyTextTransform('name', 'json')).all().order_by(Lower('json_name'))],
-        )
+        return self.get_resource_choices_with_model(models.DataCollection)
 
-class InstrumentTypeSelectFormViewMixin(View):
+class InstrumentTypeSelectFormViewMixin(OntologyCategoryChoicesViewMixin):
     def get_instrument_type_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('instrumentType')
-        type_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            type_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *((key, value) for key, value in type_dict.items())
-        )
+        return self.get_choices_from_ontology_category('instrumentType')
 
-class ComputationTypeSelectFormViewMixin(View):
+class ComputationTypeSelectFormViewMixin(OntologyCategoryChoicesViewMixin):
     def get_computation_type_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('computationType')
-        type_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            type_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *((key, value) for key, value in type_dict.items())
-        )
+        return self.get_choices_from_ontology_category('computationType')
 
-class QualityAssessmentSelectFormViewMixin(View):
+class QualityAssessmentSelectFormViewMixin(OntologyCategoryChoicesViewMixin):
     def get_data_quality_flag_choices_for_form(self):
-        g_dqf = get_graph_of_pithia_ontology_component('dataQualityFlag')
-        dqf_dict = {}
-        for s, p, o in g_dqf.triples((None, SKOS.member, None)):
-            o_pref_label = g_dqf.value(o, SKOS.prefLabel)
-            dqf_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *[(key, value) for key, value in dqf_dict.items()],
-        )
+        return self.get_choices_from_ontology_category('dataQualityFlag')
 
     def get_metadata_quality_flag_choices_for_form(self):
-        g_mqf = get_graph_of_pithia_ontology_component('metadataQualityFlag')
-        mqf_dict = {}
-        for s, p, o in g_mqf.triples((None, SKOS.member, None)):
-            o_pref_label = g_mqf.value(o, SKOS.prefLabel)
-            mqf_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *[(key, value) for key, value in mqf_dict.items()],
-        )
+        return self.get_choices_from_ontology_category('metadataQualityFlag')
 
-class RelatedPartiesSelectFormViewMixin(View):
+class RelatedPartiesSelectFormViewMixin(
+    OntologyCategoryChoicesViewMixin,
+    ResourceChoicesViewMixin):
     def get_related_party_choices_for_form(self):
-        return (
-            ('', ''),
-            ('Organisations', list(
-                (o.metadata_server_url, o.name) for o in models.Organisation.objects.annotate(json_name=KeyTextTransform('name', 'json')).all().order_by(Lower('json_name'))
-            )),
-            ('Individuals', list(
-                (o.metadata_server_url, o.name) for o in models.Individual.objects.annotate(json_name=KeyTextTransform('name', 'json')).all().order_by(Lower('json_name'))
-            ))
-        )
+        return self.get_resource_choices_with_multiple_models([
+            ('Organisations', models.Organisation),
+            ('Individuals', models.Individual),
+        ])
 
     def get_related_party_role_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('relatedPartyRole')
-        status_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            status_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *((key, value) for key, value in status_dict.items())
-        )
+        return self.get_choices_from_ontology_category('relatedPartyRole')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -220,14 +146,6 @@ class StandardIdentifiersFormViewMixin(View):
         )
         return context
 
-class StatusSelectFormViewMixin(View):
+class StatusSelectFormViewMixin(OntologyCategoryChoicesViewMixin):
     def get_status_choices_for_form(self):
-        g = get_graph_of_pithia_ontology_component('status')
-        status_dict = {}
-        for s, p, o in g.triples((None, SKOS.member, None)):
-            o_pref_label = g.value(o, SKOS.prefLabel)
-            status_dict[str(o)] = str(o_pref_label)
-        return (
-            ('', ''),
-            *((key, value) for key, value in status_dict.items())
-        )
+        return self.get_choices_from_ontology_category('status')
