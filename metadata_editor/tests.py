d07396ae52c42411f377f08f158f15017e019a79
davidchan125 <davidchan125@hotmail.co.uk>
2024-07-02 10:30:00 +0100

    Add tests for metadata_editor app

--- metadata_editor/tests.py
+++ metadata_editor/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
+
+from .metadata_components import (
+    ContactInfoAddressMetadataUpdate,
+    ContactInfoMetadataUpdate,
+    PithiaIdentifierMetadataUpdate,
+)
+from .utils import OrganisationEditor
+
+from common.test_xml_files import (
+    ORGANISATION_METADATA_XML,
+)
+
 
 # Create your tests here.
+class MetadataEditorTestCase(SimpleTestCase):
+    def test_organisation_editor(self):
+        organisation_editor = OrganisationEditor()
+        pithia_identifier = PithiaIdentifierMetadataUpdate(
+            localid='Organisation_Test',
+            namespace='test',
+            version='100',
+            creation_date='2022-02-03T12:50:00Z'
+        )
+        organisation_editor.update_pithia_identifier(pithia_identifier)
+        contact_info_address = ContactInfoAddressMetadataUpdate(
+            city='London'
+        )
+        contact_info = ContactInfoMetadataUpdate(
+            address=contact_info_address,
+            phone='123',
+            online_resource='https://www.example.com/'
+        )
+        organisation_editor.update_contact_info(contact_info)
+        xml = organisation_editor.to_xml()
+        print('xml', xml)
+
+    def test_organisation_editor_with_file(self):
+        organisation_editor = OrganisationEditor(xml_string=ORGANISATION_METADATA_XML.read().decode())
+        contact_info_address = ContactInfoAddressMetadataUpdate(country='Antarctica')
+        contact_info = ContactInfoMetadataUpdate(
+            address=contact_info_address,
+            phone='+1234567890',
+            online_resource='abc',
+            hours_of_service='1:00am-8:00pm',
+            contact_instructions='Contact by phone or post'
+        )
+        organisation_editor.update_contact_info(contact_info)
+        xml = organisation_editor.to_xml()
+        print('xml', xml)
