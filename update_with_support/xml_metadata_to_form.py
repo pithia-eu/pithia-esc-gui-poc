7046f1555568248f62a1265e6fe2eed415992cc2
davidchan125 <davidchan125@hotmail.co.uk>
2024-07-19 15:35:34 +0100

    Add platform xml data loading into wizard

--- update_with_support/xml_metadata_to_form.py
+++ update_with_support/xml_metadata_to_form.py
@@ -1,8 +1,10 @@
 from .xml_metadata_to_form_components import (
     ContactInfoXmlMetadataToFormMixin,
     DocumentationXmlMetadataToFormMixin,
+    LocationXmlMetadataToFormMixin,
     RelatedPartyXmlMetadataToFormMixin,
     ResourceXmlToFormDataConverter,
+    TypeXmlMetadataToFormMixin,
 )
 
 from metadata_editor.xml_ns_enums import NamespacePrefix
@@ -42,3 +44,33 @@ class ProjectXmlMetadataToFormConverter(
             'abstract': './/%s:abstract' % self.PITHIA_NS_PREFIX_FOR_XPATH,
             'url': './/%s:URL' % NamespacePrefix.GMD,
         })
+
+
+class PlatformXmlMetadataToFormConverter(
+    DocumentationXmlMetadataToFormMixin,
+    LocationXmlMetadataToFormMixin,
+    TypeXmlMetadataToFormMixin,
+    RelatedPartyXmlMetadataToFormMixin,
+    ResourceXmlToFormDataConverter):
+    def __init__(self, xml_string) -> None:
+        super().__init__(xml_string)
+        self.basic_form_field_to_xml_mappings.update({
+            'short_name': './/%s:shortName' % self.PITHIA_NS_PREFIX_FOR_XPATH,
+            'url': './/%s:URL' % NamespacePrefix.GMD,
+        })
+
+    def _map_standard_identifiers_to_form(self):
+        standard_identifier_elements = self.xml_string_parsed.xpath('.//%s:standardIdentifier' % self.PITHIA_NS_PREFIX_FOR_XPATH, namespaces=self.namespaces)
+        standard_identifiers = []
+        for e in standard_identifier_elements:
+            standard_identifiers.append({
+                'authority': e.attrib['authority'],
+                'value': e.text,
+            })
+        return standard_identifiers
+
+    def map_complex_xml_fields_to_form(self, form_data):
+        form_data = super().map_complex_xml_fields_to_form(form_data)
+        form_data['child_platforms'] = self.xml_string_parsed.xpath('.//%s:childPlatform/@%s:href' % (self.PITHIA_NS_PREFIX_FOR_XPATH, NamespacePrefix.XLINK), namespaces=self.namespaces)
+        form_data['standard_identifiers_json'] = self._map_standard_identifiers_to_form()
+        return form_data
--- update_with_support/xml_metadata_to_form_components.py
+++ update_with_support/xml_metadata_to_form_components.py
@@ -94,6 +94,52 @@ class DocumentationXmlMetadataToFormMixin:
         })
 
 
+class LocationXmlMetadataToFormMixin:
+    def _get_name_location(self, nested_location_element):
+        name_location_character_string_elements = nested_location_element.xpath('.//%s:nameLocation/%s:EX_GeographicDescription/%s:geographicIdentifier/%s:MD_Identifier/%s:code/%s:CharacterString' % (self.PITHIA_NS_PREFIX_FOR_XPATH, NamespacePrefix.GMD, NamespacePrefix.GMD, NamespacePrefix.GMD, NamespacePrefix.GMD, NamespacePrefix.GCO), namespaces=self.namespaces)
+        if not name_location_character_string_elements:
+            return ''
+        return name_location_character_string_elements[0].text
+
+    def _get_geometry_location_data(self, nested_location_element):
+        geometry_location_elements = nested_location_element.xpath('.//%s:geometryLocation' % (self.PITHIA_NS_PREFIX_FOR_XPATH), namespaces=self.namespaces)
+        geometry_location_data = {
+            'geometry_location_point_id': '',
+            'geometry_location_point_srs_name': '',
+            'geometry_location_point_pos_1': '',
+            'geometry_location_point_pos_2': '',
+        }
+        if not geometry_location_elements:
+            return geometry_location_data
+        geometry_location_element = geometry_location_elements[0]
+        geometry_location_data['geometry_location_point_id'] = next(iter(geometry_location_element.xpath('.//%s:Point/@%s:id' % (NamespacePrefix.GML, NamespacePrefix.GML), namespaces=self.namespaces)), '')
+        geometry_location_data['geometry_location_point_srs_name'] = next(iter(geometry_location_element.xpath('.//%s:Point/@srsName' % (NamespacePrefix.GML), namespaces=self.namespaces)), '')
+        geometry_location_point_pos_elements = geometry_location_element.xpath('.//%s:Point/%s:pos' % (NamespacePrefix.GML, NamespacePrefix.GML), namespaces=self.namespaces)
+        if geometry_location_point_pos_elements:
+            pos_text = geometry_location_point_pos_elements[0].text
+            pos_text_split = pos_text.split(' ')
+            geometry_location_data['geometry_location_point_pos_1'] = pos_text_split[0]
+            geometry_location_data['geometry_location_point_pos_2'] = pos_text_split[1]
+        return geometry_location_data
+    
+    def map_complex_xml_fields_to_form(self, form_data):
+        form_data = super().map_complex_xml_fields_to_form(form_data)
+        nested_location_elements = self.xml_string_parsed.xpath('.//%s:location/%s:Location' % (self.PITHIA_NS_PREFIX_FOR_XPATH, self.PITHIA_NS_PREFIX_FOR_XPATH), namespaces=self.namespaces)
+        if not nested_location_elements:
+            return form_data
+        nested_location_element = nested_location_elements[0]
+        name_location = self._get_name_location(nested_location_element)
+        if name_location:
+            form_data['location_name'] = name_location
+        geometry_location_data = self._get_geometry_location_data(nested_location_element)
+        for key, value in geometry_location_data.items():
+            if not value:
+                continue
+            form_data[key] = value
+
+        return form_data
+
+
 class RelatedPartyXmlMetadataToFormMixin:
     def map_complex_xml_fields_to_form(self, form_data):
         form_data = super().map_complex_xml_fields_to_form(form_data)
@@ -107,3 +153,11 @@ class RelatedPartyXmlMetadataToFormMixin:
                 'parties': parties,
             })
         return form_data
+
+
+class TypeXmlMetadataToFormMixin:
+    def __init__(self, xml_string) -> None:
+        super().__init__(xml_string)
+        self.basic_form_field_to_xml_mappings.update({
+            'type': './/%s:type/@%s:href' % (self.PITHIA_NS_PREFIX_FOR_XPATH, NamespacePrefix.XLINK)
+        })
