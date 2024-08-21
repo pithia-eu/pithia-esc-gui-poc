7c4d233d1d34181c7224602409604d575d933858
davidchan125 <davidchan125@hotmail.co.uk>
2024-08-21 11:19:59 +0100

    Fix error thrown in metadata detail page

--- browse/static/browse/detail.js
+++ browse/static/browse/detail.js
@@ -1,28 +1,36 @@
 const descriptionWrapperElement = document.querySelector(".resource-description-wrapper");
 const expandDescriptionButton = document.querySelector(".btn-expand-resource-description");
 
+function setupExpandDescriptionButton() {
+    // Credit for overflow detection: https://stackoverflow.com/a/67455839.
+    const isTextClamped = el => el.scrollHeight > el.clientHeight;
+    new ResizeObserver(e => {
+        if (descriptionWrapperElement.classList.contains("expanded")) {
+            return expandDescriptionButton.classList.remove("hidden");
+        }
+        
+        if (isTextClamped(e[0].target)) {
+            return expandDescriptionButton.classList.remove("hidden");
+        }
+        return expandDescriptionButton.classList.add("hidden");
+    }).observe(descriptionWrapperElement);
+    
+    expandDescriptionButton.addEventListener("click", () => {
+        if (expandDescriptionButton.classList.contains("expanded")) {
+            expandDescriptionButton.classList.remove("expanded");
+            descriptionWrapperElement.classList.remove("expanded");
+            expandDescriptionButton.innerText = "Show more";
+            return;
+        }
+        expandDescriptionButton.classList.add("expanded");
+        descriptionWrapperElement.classList.add("expanded");
+        expandDescriptionButton.innerText = "Show less";
+    });
+}
 
-// Credit for overflow detection: https://stackoverflow.com/a/67455839.
-const isTextClamped = el => el.scrollHeight > el.clientHeight;
-new ResizeObserver(e => {
-    if (descriptionWrapperElement.classList.contains("expanded")) {
-        return expandDescriptionButton.classList.remove("hidden");
-    }
-
-    if (isTextClamped(e[0].target)) {
-        return expandDescriptionButton.classList.remove("hidden");
-    }
-    return expandDescriptionButton.classList.add("hidden");
-}).observe(descriptionWrapperElement);
-
-expandDescriptionButton.addEventListener("click", () => {
-    if (expandDescriptionButton.classList.contains("expanded")) {
-        expandDescriptionButton.classList.remove("expanded");
-        descriptionWrapperElement.classList.remove("expanded");
-        expandDescriptionButton.innerText = "Show more";
+window.addEventListener("load", () => {
+    if (!descriptionWrapperElement) {
         return;
     }
-    expandDescriptionButton.classList.add("expanded");
-    descriptionWrapperElement.classList.add("expanded");
-    expandDescriptionButton.innerText = "Show less";
+    setupExpandDescriptionButton();
 });
