44bd87ab7d274ef1723290a7873d42976c20cc6b
davidchan125 <davidchan125@hotmail.co.uk>
2024-08-30 13:33:45 +0100

    Computation detail page - finish implementing cap links section

--- browse/static/browse/detail.js
+++ browse/static/browse/detail.js
@@ -33,7 +33,14 @@ function setupExpandDescriptionButton(expandDescriptionButton) {
     });
 }
 
+function enablePopovers() {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl, {trigger: 'focus'}));
+}
+
 window.addEventListener("load", () => {
+    enablePopovers();
+
     if (!expandDescriptionButtons) {
         return;
     }
