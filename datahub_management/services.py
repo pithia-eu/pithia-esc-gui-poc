20a1666838d3ba9bc70209786e3092a7c1d31e73
davidchan125 <davidchan125@hotmail.co.uk>
2024-11-05 16:01:41 +0000

    Update DataHubService in datahub_management app - Rename DataHubService to WorkflowDataHubService - Add more methods to WorkflowDataHubService

--- datahub_management/services.py
+++ datahub_management/services.py
@@ -1,19 +1,31 @@
 import os
+import pathlib
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from pithiaesc.settings import BASE_DIR
 
 
-class DataHubService:
+class WorkflowDataHubService:
     @classmethod
-    def store_or_overwrite_workflow_details_file(self, workflow_details_file: SimpleUploadedFile, workflow_id: str):
-        # Change the name of the workflow details file
-        # to the workflow id.
+    def _get_workflow_details_file_path(self, workflow_id):
+        return os.path.join(BASE_DIR, 'datahub', 'workflow_details', f'{workflow_id}.pdf')
 
-        # Store the file in DataHub.
-        pass
+    @classmethod
+    def store_or_overwrite_workflow_details_file(cls, workflow_details_file: SimpleUploadedFile, workflow_id: str):
+        # Change the name of the workflow details file
+        # to the workflow ID and store in DataHub.
+        with open(cls._get_workflow_details_file_path(workflow_id), 'wb+') as destination:
+            for chunk in workflow_details_file.chunks():
+                destination.write(chunk)
+
+        return pathlib.Path(cls._get_workflow_details_file_path(workflow_id))
     
     @classmethod
-    def get_workflow_details_file(self, workflow_id: str):
+    def get_workflow_details_file(cls, workflow_id: str):
         # Get the details file by the workflow ID.
-        return open(os.path.join(BASE_DIR, 'datahub', f'{workflow_id}.pdf'), 'rb')
+        return open(cls._get_workflow_details_file_path(workflow_id), 'rb')
+
+    @classmethod
+    def delete_workflow_details_file(cls, workflow_id: str):
+        workflow_details_file_to_remove = pathlib.Path(cls._get_workflow_details_file_path(workflow_id))
+        return workflow_details_file_to_remove.unlink()
