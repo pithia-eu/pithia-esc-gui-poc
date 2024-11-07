5b8c401ef6c204887890e24ba41a7c610420c124
davidchan125 <davidchan125@hotmail.co.uk>
2024-11-07 10:58:42 +0000

    Fix imports in datahub_mgmt views.py

--- datahub_management/views.py
+++ datahub_management/views.py
@@ -5,13 +5,13 @@ from django.http import (
 )
 from django.utils.html import escape
 
-from .services import DataHubService
+from .services import WorkflowDataHubService
 
 
 # Create your views here.
 def get_workflow_details_file(request, workflow_id):
     try:
-        workflow_details_file = DataHubService.get_workflow_details_file(workflow_id)
+        workflow_details_file = WorkflowDataHubService.get_workflow_details_file(workflow_id)
         return FileResponse(workflow_details_file, content_type='application/pdf')
     except IOError:
         return HttpResponseNotFound(f'The details file for workflow with ID "<i>{escape(workflow_id)}</i>" was not found.')
