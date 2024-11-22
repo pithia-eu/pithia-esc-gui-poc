3bed0f8aaf582bcbbf0aad7d6af2f25f92252dda
davidchan125 <davidchan125@hotmail.co.uk>
2024-11-22 17:09:49 +0000

    Update search by content error message

--- search/static/search/search.js
+++ search/static/search/search.js
@@ -444,6 +444,6 @@ document.getElementById("ontology-search-form").addEventListener("submit", event
     const checkedCheckboxes = document.querySelectorAll("#ontology-search-form input[type=checkbox].search-term-checkbox:checked:not(:disabled, [name=phenomenon], [name=measurand])");
     if (checkedCheckboxes.length === 0) {
         event.preventDefault();
-        return alert('Select at least one Feature of Interest, Instrument Type, Computation Type or Observed Property.');
+        return alert('Select at least one Feature of Interest, Computation Type, Instrument Type, Annotation Type or Observed Property.');
     }
 });
