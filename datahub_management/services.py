6ff853ecc15b339c0042909be1ece19a83168fda
davidchan125 <davidchan125@hotmail.co.uk>
2024-12-02 18:44:33 +0000

    Add CatalogueDataSubsetDataHubService

--- datahub_management/services.py
+++ datahub_management/services.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import pathlib
+import shutil
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from pithiaesc.settings import BASE_DIR
@@ -9,31 +10,146 @@ from pithiaesc.settings import BASE_DIR
 logger = logging.getLogger(__name__)
 
 
-class WorkflowDataHubService:
+class DataHubService:
     @classmethod
-    def _get_workflow_details_file_path(cls, workflow_id):
-        return os.path.join(BASE_DIR, os.path.normpath(os.environ['DATAHUB_DIRECTORY_PATH']), 'workflows', f'{workflow_id}.pdf')
+    def _get_datahub_directory_path(cls):
+        return os.path.join(BASE_DIR, os.path.normpath(os.environ['DATAHUB_DIRECTORY_PATH']))
 
     @classmethod
-    def store_or_overwrite_workflow_details_file(cls, workflow_details_file: SimpleUploadedFile, workflow_id: str):
-        # Change the name of the workflow details file
-        # to the workflow ID and store in DataHub.
-        with open(cls._get_workflow_details_file_path(workflow_id), 'wb+') as destination:
-            for chunk in workflow_details_file.chunks():
+    def _get_file_extension(cls, file_path: str):
+        file_name, file_extension = os.path.splitext(file_path)
+        return file_extension
+
+    @classmethod
+    def _store_or_overwrite_file_in_datahub(cls, file_path, file):
+        with open(file_path, 'wb+') as destination:
+            for chunk in file.chunks():
                 destination.write(chunk)
 
-        return pathlib.Path(cls._get_workflow_details_file_path(workflow_id))
-    
+        return pathlib.Path(file_path)
+
     @classmethod
-    def get_workflow_details_file(cls, workflow_id: str):
+    def _get_file_from_datahub(cls, file_path):
         try:
             # Get the details file by the workflow ID.
-            return open(cls._get_workflow_details_file_path(workflow_id), 'rb')
+            return open(file_path, 'rb')
         except FileNotFoundError:
-            logger.exception(f'Workflow {workflow_id}\'s details file was not found.')
+            logger.exception(f'Could not find a file in DataHub with path: {file_path}.')
         return None
 
     @classmethod
+    def _delete_file_from_datahub(cls, file_path):
+        file_to_remove = pathlib.Path(file_path)
+        return os.remove(file_to_remove)
+
+
+class WorkflowDataHubService(DataHubService):
+    @classmethod
+    def _get_workflow_details_file_path(cls, workflow_id):
+        return os.path.join(cls._get_datahub_directory_path(), 'workflows', f'{workflow_id}.pdf')
+
+    @classmethod
+    def store_or_overwrite_workflow_details_file(cls, file: SimpleUploadedFile, workflow_id: str):
+        # Change the name of the workflow details file
+        # to the workflow ID and store in DataHub.
+        return cls._store_or_overwrite_file_in_datahub(
+            cls._get_workflow_details_file_path(workflow_id),
+            file
+        )
+    
+    @classmethod
+    def get_workflow_details_file(cls, workflow_id: str):
+        # Get the details file by the workflow ID.
+        return cls._get_file_from_datahub(cls._get_workflow_details_file_path(workflow_id))
+
+    @classmethod
     def delete_workflow_details_file(cls, workflow_id: str):
-        workflow_details_file_to_remove = pathlib.Path(cls._get_workflow_details_file_path(workflow_id))
-        return os.remove(workflow_details_file_to_remove)
+        return cls._delete_file_from_datahub(cls._get_workflow_details_file_path(workflow_id))
+
+class CatalogueDataSubsetDataHubService(DataHubService):
+    @classmethod
+    def _get_catalogue_data_subset_directory_path_and_create_if_not_exists(cls, catalogue_data_subset_id):
+        catalogue_data_subset_path = os.path.join(cls._get_datahub_directory_path(), 'catalogues', catalogue_data_subset_id)
+        # Create a directory for the catalogue
+        # data subset if it does not exist. An
+        # error should be raised if the parent
+        # folder does not exist.
+        pathlib.Path(catalogue_data_subset_path).mkdir(parents=False, exist_ok=True)
+        return catalogue_data_subset_path
+
+    @classmethod
+    def _get_catalogue_data_subset_resource_file_path_from_name_with_no_extension(
+            cls,
+            catalogue_data_subset_id: str,
+            resource_name_with_no_extension: str):
+        catalogue_data_subset_resource_file_names = pathlib.Path(cls._get_catalogue_data_subset_directory_path_and_create_if_not_exists(catalogue_data_subset_id)).glob(f'{resource_name_with_no_extension}.*')
+        file_name = None
+        for rfn in catalogue_data_subset_resource_file_names:
+            file_name = rfn
+        return file_name
+
+    @classmethod
+    def store_or_overwrite_catalogue_data_subset_resource_file(
+            cls,
+            file: SimpleUploadedFile,
+            new_file_name: str,
+            catalogue_data_subset_id: str):
+        file_extension = cls._get_file_extension(file.name)
+        return cls._store_or_overwrite_file_in_datahub(
+            os.path.join(
+                cls._get_catalogue_data_subset_directory_path_and_create_if_not_exists(catalogue_data_subset_id),
+                f'{new_file_name}{file_extension}'
+            ),
+            file
+        )
+    
+    @classmethod
+    def get_catalogue_data_subset_file(cls, catalogue_data_subset_id: str, file_name_with_no_extension: str):
+        # Get the resource file from the catalogue data
+        # subset directory. The file extension is not
+        # known.
+        file_name = cls._get_catalogue_data_subset_resource_file_path_from_name_with_no_extension(
+            catalogue_data_subset_id,
+            file_name_with_no_extension
+        )
+        return cls._get_file_from_datahub(file_name)
+
+    @classmethod
+    def rename_catalogue_data_subset_resource_file(
+            cls,
+            catalogue_data_subset_id: str,
+            current_file_name: str,
+            new_file_name: str):
+        old_file_path = cls._get_catalogue_data_subset_resource_file_path_from_name_with_no_extension(
+            catalogue_data_subset_id,
+            current_file_name
+        )
+        old_file_extension = cls._get_file_extension(old_file_path)
+        catalogue_data_subset_directory = cls._get_catalogue_data_subset_directory_path_and_create_if_not_exists(catalogue_data_subset_id)
+        new_file_path = os.path.join(catalogue_data_subset_directory, f'{new_file_name}{old_file_extension}')
+        return os.rename(old_file_path, new_file_path)
+
+    @classmethod
+    def get_files_for_catalogue_data_subset(cls, catalogue_data_subset_id: str):
+        catalogue_data_subset_directory_path = cls._get_catalogue_data_subset_directory_path_and_create_if_not_exists(catalogue_data_subset_id)
+        directory_items = os.listdir(catalogue_data_subset_directory_path)
+        files = [
+            f
+            for f in directory_items
+            if os.path.isfile(os.path.join(catalogue_data_subset_directory_path, f))
+        ]
+        return files
+
+    @classmethod
+    def delete_catalogue_data_subset_resource_file(cls, catalogue_data_subset_id: str, file_name_with_no_extension: str):
+        return cls._delete_file_from_datahub(
+            cls._get_catalogue_data_subset_directory_path_and_create_if_not_exists(catalogue_data_subset_id),
+            file_name_with_no_extension
+        )
+
+    @classmethod
+    def delete_catalogue_data_subset_directory(cls, catalogue_data_subset_id: str):
+        try:
+            return shutil.rmtree(cls._get_catalogue_data_subset_directory_path_and_create_if_not_exists(catalogue_data_subset_id))
+        except OSError as err:
+            logger.exception(err)
