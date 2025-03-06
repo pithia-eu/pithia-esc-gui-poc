58b2a8fc77aa00b843de30939d1288a6289b6f3c
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-03-06 22:49:42 +0000

    Add IRI copy btns to ontology browser - Update indentation lines to align better with parent term - Update responsive identation spacing

--- ontology/static/ontology/ontology.js
+++ ontology/static/ontology/ontology.js
@@ -1,5 +1,8 @@
 const SEARCH_BOX_INPUT_FILTER_CLASS = "search-no-match";
 const noSearchMatchesFound = document.getElementById("no-search-results-msg");
+const COPY_BUTTON_TOOLTIP_DEFAULT_TEXT = 'Copy Ontology IRI';
+const COPY_BUTTON_TOOLTIP_COPIED_TEXT = 'Copied!';
+let ontologyIriCopyButtonTimeout;
 
 
 // Utility functions
@@ -146,6 +149,30 @@ export async function setupSearchFormComponent(html, treeContainerId, callback) 
     setTimeout(async () => {
         document.querySelector(`#${treeContainerId} .tree-search-terms`).innerHTML = html;
         setupInputsForTreeContainerId(treeContainerId);
+        const ontologyIriCopyButtons = document.querySelectorAll('.btn-copy-ontology-iri');
+        for await (const button of ontologyIriCopyButtons) {
+            const tooltip = new bootstrap.Tooltip(button, {});
+            button.addEventListener('hidden.bs.tooltip', () => {
+                tooltip.setContent({
+                    '.tooltip-inner': COPY_BUTTON_TOOLTIP_DEFAULT_TEXT
+                });
+            });
+            button.addEventListener("click", async () => {
+                const ontologyIri = button.dataset.ontologyIri;
+                await navigator.clipboard.writeText(ontologyIri);
+                if (ontologyIriCopyButtonTimeout) {
+                    window.clearTimeout(ontologyIriCopyButtonTimeout);
+                }
+                ontologyIriCopyButtonTimeout = window.setTimeout(() => {
+                    tooltip.setContent({
+                        '.tooltip-inner': COPY_BUTTON_TOOLTIP_DEFAULT_TEXT
+                    });
+                }, 1500);
+                tooltip.setContent({
+                    '.tooltip-inner': COPY_BUTTON_TOOLTIP_COPIED_TEXT
+                });
+            });
+        }
         document.querySelector(`#${treeContainerId} .tree-search-terms`).style.opacity = 1;
         if (callback) {
             callback();
