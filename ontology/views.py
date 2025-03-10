4dfe666c30469e60d9b75d4eac2ed0cc3e94a06d
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-03-10 14:34:59 +0000

    Implement updated ontology browser detail page - Update layout of ontology browser detail page

--- ontology/views.py
+++ ontology/views.py
@@ -1,19 +1,29 @@
 import os
 from django.http import (
     FileResponse,
+    Http404,
     HttpResponseServerError,
     HttpResponseNotFound,
 )
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from rdflib import URIRef
 from rdflib.resource import Resource
 from rdflib.namespace._SKOS import SKOS
 
-from common.constants import SPACE_PHYSICS_ONTOLOGY_SERVER_HTTPS_URL_BASE
-from ontology.services import (
+from .services import (
     create_dictionary_from_pithia_ontology_component,
     get_graph_of_pithia_ontology_component,
+    get_ontology_category_terms_in_xml_format,
 )
+from .utils import (
+    LicenceOntologyTermMetadata,
+    ObservedPropertyOntologyTermMetadata,
+    OntologyCategoryMetadata,
+    OntologyTermMetadata,
+)
+
+from common.constants import SPACE_PHYSICS_ONTOLOGY_SERVER_HTTPS_URL_BASE
 from pithiaesc.settings import BASE_DIR
 from search.services import (
     get_parents_of_registered_ontology_terms,
@@ -28,7 +38,9 @@ from utils.string_helpers import split_camel_case
 from utils.url_helpers import create_ontology_term_detail_url_from_ontology_term_server_url
 from utils.html_helpers import create_anchor_tag_html_from_ontology_term_details
 
+
 _ONTOLOGY_INDEX_PAGE_TITLE = 'Space Physics Ontology'
+
 
 # Create your views here.
 def ontology(request):
@@ -168,3 +180,58 @@ def ontology_term_detail(request, category, term_id):
         'ontology_index_page_breadcrumb_text': _ONTOLOGY_INDEX_PAGE_TITLE,
         'ontology_category_term_list_page_breadcrumb_text': _get_ontology_category_term_list_page_title_from_category(category),
     })
+ 
+class OntologyTermDetailView(TemplateView):
+    template_name = 'ontology/detail/base.html'
+
+    def apply_wrapper_to_ontology_term_metadata(self, xml_string_for_ontology_term: str):
+        return OntologyTermMetadata(xml_string_for_ontology_term)
+
+    def get(self, request, *args, **kwargs):
+        if not hasattr(self, 'category'):
+            self.category = self.kwargs['category']
+        self.term_id = self.kwargs['term_id']
+        xml_string_for_ontology_category_terms = get_ontology_category_terms_in_xml_format(self.category)
+        self.ontology_category_metadata = OntologyCategoryMetadata(xml_string_for_ontology_category_terms)
+        xml_string_for_ontology_term = self.ontology_category_metadata.get_term_with_id(self.term_id)
+        if not xml_string_for_ontology_term:
+            raise Http404(f'An ontology term with ID, <b>"{self.term_id}"</b>, was not found.')
+        self.ontology_term_metadata = self.apply_wrapper_to_ontology_term_metadata(xml_string_for_ontology_term)
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'title': self.ontology_term_metadata.pref_label,
+            'resource_ontology_url': f'{SPACE_PHYSICS_ONTOLOGY_SERVER_HTTPS_URL_BASE}/{self.category}/{self.term_id}',
+            'ontology_term_metadata': self.ontology_term_metadata,
+            # Get names and ontology browser URLs for
+            # ontology server URLs.
+            'names_and_ontology_browser_urls': self.ontology_term_metadata.get_names_and_ontology_browser_urls_of_ontology_term_urls(
+                fetched_ontology_categories={
+                    self.category: self.ontology_category_metadata,
+                }
+            ),
+            'category': self.category,
+            'ontology_index_page_breadcrumb_text': _ONTOLOGY_INDEX_PAGE_TITLE,
+            'ontology_category_term_list_page_breadcrumb_text': _get_ontology_category_term_list_page_title_from_category(self.category),
+        })
+        return context
+
+
+class LicenceTermDetailView(OntologyTermDetailView):
+    def apply_wrapper_to_ontology_term_metadata(self, xml_string_for_ontology_term: str):
+        return LicenceOntologyTermMetadata(xml_string_for_ontology_term)
+
+    def get(self, request, *args, **kwargs):
+        self.category = 'licence'
+        return super().get(request, *args, **kwargs)
+
+
+class ObservedPropertyTermDetailView(OntologyTermDetailView):
+    def apply_wrapper_to_ontology_term_metadata(self, xml_string_for_ontology_term: str):
+        return ObservedPropertyOntologyTermMetadata(xml_string_for_ontology_term)
+
+    def get(self, request, *args, **kwargs):
+        self.category = 'observedProperty'
+        return super().get(request, *args, **kwargs)
