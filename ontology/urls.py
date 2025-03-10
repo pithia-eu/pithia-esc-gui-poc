3d76f1155f4247b2b21766a4f1afadc41aa59ed5
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-03-10 15:55:25 +0000

    Update layout of ontology browser detail pages - Move broader/narrower terms into main section - Update section layout for OP terms - Move link to licence deed to aside section for licence terms - Remove view and URL for old ontology browser detail page

--- ontology/urls.py
+++ ontology/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('guide/', views.ontology_guide, name='guide'),
     path('categories/<category>/', views.ontology_category_terms_list, name='ontology_category_terms_list'),
     path('categories/<category>/terms/', views.ontology_category_terms_list_only, name='ontology_category_terms_list_only'),
-    # path('categories/<category>/terms/<term_id>/', views.ontology_term_detail, name='ontology_term_detail'),
     path('categories/observedProperty/terms/<term_id>/', views.ObservedPropertyTermDetailView.as_view(), name='observed_property_term_detail'),
     path('categories/licence/terms/<term_id>/', views.LicenceTermDetailView.as_view(), name='licence_term_detail'),
     path('categories/<category>/terms/<term_id>/', views.OntologyTermDetailView.as_view(), name='ontology_term_detail'),
--- ontology/views.py
+++ ontology/views.py
@@ -103,86 +103,9 @@ def ontology_category_terms_list_only(request, category):
         'registered_ontology_terms': registered_ontology_terms,
         'parents_of_registered_ontology_terms': parents_of_registered_ontology_terms,
     })
-
-def _remove_namespace_prefix_from_predicate(p):
-    p_identifier_no_prefix = p.identifier.split('#')[-1]
-    if len(p.identifier.split('#')) == 1:
-        p_identifier_no_prefix = p.identifier.split('/')[-1]
-    return p_identifier_no_prefix
-
-def ontology_term_detail(request, category, term_id):
-    g = get_graph_of_pithia_ontology_component(category)
-    resource = None
-    resource_uriref = URIRef(f'{SPACE_PHYSICS_ONTOLOGY_SERVER_HTTPS_URL_BASE}/{category}/{term_id}')
-    triples = list(g.triples((None, SKOS.member, resource_uriref)))
-    if len(triples) > 0:
-        resource = Resource(g, triples[0][2])
-    if resource is None:
-        return HttpResponseNotFound(f'Details for the ontology term ID <b>"{term_id}"</b> were not found.')
-    resource_predicates_no_prefix = list(set(map(_remove_namespace_prefix_from_predicate, resource.predicates())))
-    # Turn resource into a dictionary for easier usage
-    resource_dictionary = {}
-    resource_predicates_readable = {}
-    for p in resource.predicates():
-        p_identifier_no_prefix = p.identifier.split('#')[-1]
-        if len(p.identifier.split('#')) == 1:
-            p_identifier_no_prefix = p.identifier.split('/')[-1]
-        if p_identifier_no_prefix not in resource_dictionary and (p_identifier_no_prefix == 'observedProperty' or p_identifier_no_prefix == 'phenomenon' or p_identifier_no_prefix == 'measurand' or p_identifier_no_prefix == 'featureOfInterest' or p_identifier_no_prefix == 'propagationMode' or p_identifier_no_prefix == 'interaction' or p_identifier_no_prefix == 'qualifier' or p_identifier_no_prefix == 'narrower' or p_identifier_no_prefix == 'broader'):
-            # Other ontology terms referenced by the resource
-            if p_identifier_no_prefix == 'narrower' or p_identifier_no_prefix == 'broader':
-                g_other_ontology_term = g
-            else:
-                g_other_ontology_term = get_graph_of_pithia_ontology_component(p_identifier_no_prefix)
-            urls_of_referenced_terms = []
-            urls_of_referenced_terms_with_preflabels = []
-            for s2, p2, o2 in g.triples((resource_uriref, p.identifier, None)):
-                urls_of_referenced_terms.append(str(o2))
-            for u in urls_of_referenced_terms:
-                ontology_term_detail_url_for_referenced_term = create_ontology_term_detail_url_from_ontology_term_server_url(u)
-                term_pref_label = u.split('/')[-1]
-                anchor_tag_of_term = create_anchor_tag_html_from_ontology_term_details(term_pref_label, u, ontology_term_detail_url_for_referenced_term, g_other_ontology_term)
-                urls_of_referenced_terms_with_preflabels.append(anchor_tag_of_term)
-            resource_dictionary[p_identifier_no_prefix]= urls_of_referenced_terms_with_preflabels
-        if p_identifier_no_prefix not in resource_dictionary:
-            if len(list(g.triples((resource_uriref, p.identifier, None)))) > 1:
-                resource_dictionary[p_identifier_no_prefix] = []
-                for s2, p2, o2 in g.triples((resource_uriref, p.identifier, None)):
-                    resource_dictionary[p_identifier_no_prefix].append(str(o2))
-            else:
-                resource_dictionary[p_identifier_no_prefix] = str(list(g.triples((resource_uriref, p.identifier, None)))[0][2])
-    # Have a human-readable version of the predicates to display in the front-end.
-    for p in resource_predicates_no_prefix:
-        if p == 'broader':
-            resource_predicates_readable[p] = 'Broader Terms' 
-        elif p == 'narrower':
-            resource_predicates_readable[p] = 'Narrower Terms' 
-        elif p == 'observedProperty':
-            resource_predicates_readable[p] = 'Observed Properties' 
-        elif p == 'phenomenon':
-            resource_predicates_readable[p] = 'Phenomenons' 
-        elif p == 'measurand':
-            resource_predicates_readable[p] = 'Measurands' 
-        elif p == 'featureOfInterest':
-            resource_predicates_readable[p] = 'Features of Interest' 
-        elif p == 'interaction':
-            resource_predicates_readable[p] = 'Interactions' 
-        elif p == 'qualifier':
-            resource_predicates_readable[p] = 'Qualifiers'
-        else:
-            resource_predicates_readable[p] = ' '.join(split_camel_case(p)).title() 
-    return render(request, 'ontology/ontology_term_detail.html', {
-        'title': resource_dictionary['prefLabel'],
-        'resource_ontology_url': f'{SPACE_PHYSICS_ONTOLOGY_SERVER_HTTPS_URL_BASE}/{category}/{term_id}',
-        'resource_dictionary': resource_dictionary,
-        'resource_predicates_no_prefix': resource_predicates_no_prefix,
-        'resource_predicates_readable': resource_predicates_readable,
-        'category': category,
-        'ontology_index_page_breadcrumb_text': _ONTOLOGY_INDEX_PAGE_TITLE,
-        'ontology_category_term_list_page_breadcrumb_text': _get_ontology_category_term_list_page_title_from_category(category),
-    })
  
 class OntologyTermDetailView(TemplateView):
-    template_name = 'ontology/detail/base.html'
+    template_name = 'ontology/detail/bases/base.html'
 
     def apply_wrapper_to_ontology_term_metadata(self, xml_string_for_ontology_term: str):
         return OntologyTermMetadata(xml_string_for_ontology_term)
@@ -220,6 +143,8 @@ class OntologyTermDetailView(TemplateView):
 
 
 class LicenceTermDetailView(OntologyTermDetailView):
+    template_name = 'ontology/detail/bases/licence.html'
+
     def apply_wrapper_to_ontology_term_metadata(self, xml_string_for_ontology_term: str):
         return LicenceOntologyTermMetadata(xml_string_for_ontology_term)
 
@@ -229,6 +154,8 @@ class LicenceTermDetailView(OntologyTermDetailView):
 
 
 class ObservedPropertyTermDetailView(OntologyTermDetailView):
+    template_name = 'ontology/detail/bases/observed_property.html'
+
     def apply_wrapper_to_ontology_term_metadata(self, xml_string_for_ontology_term: str):
         return ObservedPropertyOntologyTermMetadata(xml_string_for_ontology_term)
 
