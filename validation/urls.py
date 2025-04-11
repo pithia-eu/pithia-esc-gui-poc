6b7020763fe986900913d58db30910dd6ba8a69d
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-04-11 17:33:35 +0100

    Add endpoints for acquisition capabilities validation

--- validation/urls.py
+++ validation/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('api-specification-url/', views.api_specification_url, name='api_specification_url'),
     # Inline validation URLs
     path('new-registration/', views.QuickInlineRegistrationValidationFormView.as_view(), name='new_registration'),
+    path('new-acquisition-capabilities-registration/', views.QuickInlineAcquisitionCapabilitiesRegistrationValidationFormView.as_view(), name='new_acquisition_capabilities_registration'),
     path('update/', views.QuickInlineUpdateValidationFormView.as_view(), name='update'),
+    path('acquisition-capabilities-update/', views.QuickInlineAcquisitionCapabilitiesUpdateValidationFormView.as_view(), name='acquisition_capabilities_update'),
     path('instrument-update/', views.QuickInlineInstrumentUpdateValidationFormView.as_view(), name='instrument_update'),
     path('xsd/', views.InlineXSDValidationFormView.as_view(), name='xsd'),
     path('new-localid/', views.localid, name='new_localid'),
