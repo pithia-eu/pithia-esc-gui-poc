b8bda99b188ba78f824418b95383ce572e0a88f5
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-04-16 17:41:16 +0100

    Update static dataset in-page search - Hide unmatching search results instead of greying out - Increase font weight of search results - Improve styling of search results

--- browse/static/browse/resource_filter_by_name_static_dataset.js
+++ browse/static/browse/resource_filter_by_name_static_dataset.js
@@ -1,30 +1,36 @@
 const searchInput = document.querySelector("#resource-name-search");
 const categorySections = Array.from(document.querySelectorAll(".category-section"));
-const linksByCategorySection = categorySections.reduce((accumulator, section) => {
-    accumulator[section.id] = Array.from(section.querySelectorAll("a"));
+const listItemsByCategorySection = categorySections.reduce((accumulator, section) => {
+    accumulator[section.id] = Array.from(section.querySelectorAll("li"));
     return accumulator;
 }, {});
 
 
 function searchCategorySection(input) {
     const inputLowerCase = input.toLowerCase();
-    for (const sectionId in linksByCategorySection) {
+    for (const sectionId in listItemsByCategorySection) {
         const section = document.querySelector(`#${sectionId}`);
+        const sectionListItems = listItemsByCategorySection[sectionId];
         if (input.trim() === "") {
             section.classList.remove("d-none");
+            sectionListItems.forEach(link => {
+                link.classList.remove("fw-semibold");
+                return link.classList.remove("d-none");
+            });
             continue;
         }
-        const sectionLinks = linksByCategorySection[sectionId];
-        if (!sectionLinks.some(link => link.textContent.toLowerCase().includes(inputLowerCase))) {
+        if (!sectionListItems.some(link => link.textContent.toLowerCase().includes(inputLowerCase))) {
             section.classList.add("d-none");
             continue;
         }
         section.classList.remove("d-none");
-        sectionLinks.forEach(link => {
+        sectionListItems.forEach(link => {
             if (link.textContent.toLowerCase().includes(inputLowerCase)) {
-                return link.classList.remove("text-body-secondary");
+                link.classList.add("fw-semibold");
+                return link.classList.remove("d-none");
             }
-            return link.classList.add("text-body-secondary");
+            link.classList.remove("fw-semibold");
+            return link.classList.add("d-none");
         });
     }
 }
