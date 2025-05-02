21c6e70443f5ef62683d541a3542d0fd55a7b684
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-05-02 10:16:31 +0100

    Add search endpoint for searching w/ one FOI

--- search/urls.py
+++ search/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'search'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('by-feature-of-interest/', views.results_from_foi, name='results_from_foi'),
     path('results/', views.results, name='results'),
     path('templates/form/component/<ontology_component>/', views.get_tree_form_for_ontology_component)
 ]
