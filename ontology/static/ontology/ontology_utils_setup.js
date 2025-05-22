88519b6a9e878fe31575eaf2cfc3c0a464ba584e
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-05-22 18:06:16 +0100

    Update ontology IRI copy button styling

--- ontology/static/ontology/ontology_utils_setup.js
+++ ontology/static/ontology/ontology_utils_setup.js
@@ -1,17 +1,12 @@
-const COPY_BUTTON_TOOLTIP_DEFAULT_TEXT = 'Copy Ontology IRI';
-const COPY_BUTTON_TOOLTIP_COPIED_TEXT = 'Copied!';
+const COPY_BUTTON_TOOLTIP_COPIED_TEXT = 'Copied';
 let ontologyIriCopyButtonTimeout;
 
 
 export async function setupOntologyIriCopyButtons() {
     const ontologyIriCopyButtons = document.querySelectorAll('.btn-copy-ontology-iri');
     for await (const button of ontologyIriCopyButtons) {
-        const tooltip = new bootstrap.Tooltip(button, {});
-        button.addEventListener('hidden.bs.tooltip', () => {
-            tooltip.setContent({
-                '.tooltip-inner': COPY_BUTTON_TOOLTIP_DEFAULT_TEXT
-            });
-        });
+        const buttonSpan = button.querySelector("span");
+        const buttonDefaultText = buttonSpan.textContent;
         button.addEventListener("click", async () => {
             const ontologyIri = button.dataset.ontologyIri;
             await navigator.clipboard.writeText(ontologyIri);
@@ -19,13 +14,9 @@ export async function setupOntologyIriCopyButtons() {
                 window.clearTimeout(ontologyIriCopyButtonTimeout);
             }
             ontologyIriCopyButtonTimeout = window.setTimeout(() => {
-                tooltip.setContent({
-                    '.tooltip-inner': COPY_BUTTON_TOOLTIP_DEFAULT_TEXT
-                });
+                buttonSpan.textContent = buttonDefaultText;
             }, 1500);
-            tooltip.setContent({
-                '.tooltip-inner': COPY_BUTTON_TOOLTIP_COPIED_TEXT
-            });
+            buttonSpan.textContent = COPY_BUTTON_TOOLTIP_COPIED_TEXT;
         });
     }
 }
