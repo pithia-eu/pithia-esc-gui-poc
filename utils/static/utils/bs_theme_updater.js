315496c6af1a60673c54d5084081cab5c7cffca6
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-06-04 11:52:57 +0100

    Use JS to update HTML meta theme-color

--- utils/static/utils/bs_theme_updater.js
+++ utils/static/utils/bs_theme_updater.js
@@ -3,6 +3,8 @@ const SITE_THEME_LOCAL_STORAGE_KEY = "theme";
 const LIGHT_SITE_THEME = "light";
 const DARK_SITE_THEME = "dark";
 const AUTO_SITE_THEME = "auto";
+const META_THEME_COLOUR_LIGHT_HEX = "#e9ecef";
+const META_THEME_COLOUR_DARK_HEX = "#343a40";
 
 // Query selectors
 const htmlElement = document.querySelector("html");
@@ -20,8 +22,10 @@ const autoSiteThemeIconSelector = "#circle-half";
 // Utils
 function switchBootstrapTheme(isDarkMode) {
     if (isDarkMode) {
+        metaThemeColourElement.setAttribute("content", META_THEME_COLOUR_DARK_HEX);
         return htmlElement.setAttribute("data-bs-theme", "dark");
     }
+    metaThemeColourElement.setAttribute("content", META_THEME_COLOUR_LIGHT_HEX);
     return htmlElement.setAttribute("data-bs-theme", "light");
 }
 
