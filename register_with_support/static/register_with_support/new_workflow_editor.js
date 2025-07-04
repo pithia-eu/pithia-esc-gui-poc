1edcc7f96b3634d2c59ec23c61d70723627f5f37
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-07-04 13:44:09 +0100

    Integrate new JS script for editing new workflows w/ wizard - Remove old script for setting up new workflow wizard

--- register_with_support/static/register_with_support/new_workflow_editor.js
+++ register_with_support/static/register_with_support/new_workflow_editor.js
@@ -1,8 +1,29 @@
 import {
     WorkflowEditor,
 } from "/static/metadata_editor/workflow_editor.js";
+import {
+    apiSpecificationUrlInput,
+    badApiInteractionMethodModifiedEvent,
+    validateOpenApiSpecificationUrl,
+} from "/static/validation/api_specification_validation.js";
 
 
-window.addEventListener("load", () => {
-    const editor = new WorkflowEditor();
+class NewWorkflowEditor extends WorkflowEditor {
+    setupEventListeners() {
+        super.setupEventListeners();
+
+        apiSpecificationUrlInput.addEventListener("input", async () => {
+            const url = apiSpecificationUrlInput.value;
+            if (url.trim().length === 0) {
+                return document.dispatchEvent(badApiInteractionMethodModifiedEvent);
+            }
+            await validateOpenApiSpecificationUrl();
+        });
+    }
+}
+
+
+window.addEventListener("load", async () => {
+    const editor = new NewWorkflowEditor();
+    await validateOpenApiSpecificationUrl();
 });
