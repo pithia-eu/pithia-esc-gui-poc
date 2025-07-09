79509eb6893f00345b69bc2756bf9c94ab3fdebb
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-07-09 11:38:43 +0100

    Wizard update - fix IDs of dynamically added HTML elements

--- metadata_editor/static/metadata_editor/components/utils.js
+++ metadata_editor/static/metadata_editor/components/utils.js
@@ -10,15 +10,33 @@ export function generateUniqueElemIdFromCurrentElemId(currentElemId) {
 
 export function updateDuplicatedElemsWithIdsInContainer(elems, containerElement) {
     elems.forEach(elem => {
+        if (elem.classList.contains("field-wrapper") || elem.classList.contains("invalid-feedback")) {
+            return;
+        }
         const newId = generateUniqueElemIdFromCurrentElemId(elem.id);
+        const invalidFeedbackElements = containerElement.querySelectorAll(`.invalid-feedback[data-field="${elem.id}"]`);
+        const fieldWrappers = containerElement.querySelectorAll(`.field-wrapper[data-field="${elem.id}"]`);
         const correspondingLabels = containerElement.querySelectorAll(`label[for="${elem.id}"]`);
         const correspondingAriaDescBys = containerElement.querySelectorAll(`[aria-describedby="${elem.id}"]`);
         elem.id = newId;
         correspondingLabels.forEach(label => {
             label.htmlFor = newId;
         });
-        correspondingAriaDescBys.forEach(elem => {
-            elem.setAttribute("aria-describedby", newId);
+        correspondingAriaDescBys.forEach(correspondingAriaDescBy => {
+            correspondingAriaDescBy.setAttribute("aria-describedby", newId);
+        });
+        invalidFeedbackElements.forEach(invalidFeedbackElem => {
+            const newInvalidFeedbackElemId = `invalid-feedback-${newId}`;
+            if (elem.getAttribute("aria-describedby") === invalidFeedbackElem.id) {
+                elem.setAttribute("aria-describedby", newInvalidFeedbackElemId);
+            }
+            invalidFeedbackElem.setAttribute("data-field", newId);
+            invalidFeedbackElem.setAttribute("id", newInvalidFeedbackElemId);
+        });
+        fieldWrappers.forEach(fieldWrapper => {
+            const newFieldWrapperId = `field-wrapper-${newId}`;
+            fieldWrapper.setAttribute("data-field", newId);
+            fieldWrapper.setAttribute("id", newFieldWrapperId);
         });
     });
 }
